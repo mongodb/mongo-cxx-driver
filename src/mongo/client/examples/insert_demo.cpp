bd9138a9d527e1e75f015ca95465afbc2e6b7e2c
Tyler Brock <tyler.brock@gmail.com>
2014-03-18 11:55:53 -0400

    CXX-141 general cleanup of insert_demo

--- src/mongo/client/examples/insert_demo.cpp
+++ src/mongo/client/examples/insert_demo.cpp
@@ -45,24 +45,24 @@ int main(int argc, char* argv[]) {
 
         bo o = BSON( "hello" << "world" );
 
-    cout << "dropping collection..." << endl;
-    c.dropCollection("test.foo");
+        cout << "dropping collection..." << endl;
+        c.dropCollection("test.foo");
 
-	cout << "inserting..." << endl;
+        cout << "inserting..." << endl;
 
-	time_t start = time(0);
-	for( unsigned i = 0; i < 1000000; i++ ) {
-	  c.insert("test.foo", o);
-	}
+        time_t start = time(0);
+        for( unsigned i = 0; i < 1000000; i++ ) {
+            c.insert("test.foo", o);
+        }
 
-	// wait until all operations applied
-	cout << "getlasterror returns: \"" << c.getLastError() << '"' << endl;
+        // wait until all operations applied
+        cout << "getlasterror returns: \"" << c.getLastError() << '"' << endl;
 
-	time_t done = time(0);
-	time_t dt = done-start;
-	cout << dt << " seconds " << 1000000/dt << " per second" << endl;
-    } 
-    catch(DBException& e) { 
+        time_t done = time(0);
+        time_t dt = done-start;
+        cout << dt << " seconds " << 1000000/dt << " per second" << endl;
+    }
+    catch(DBException& e) {
         cout << "caught DBException " << e.toString() << endl;
         return EXIT_FAILURE;
     }
