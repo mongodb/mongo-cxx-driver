1785cb5696e331e125be7236cf4fe5b7540aeb03
Tyler Brock <tyler.brock@gmail.com>
2014-04-03 12:02:45 -0400

    CXX-159 Add SASL client auth integration test

--- src/mongo/clienttest_main.cpp
+++ src/mongo/clienttest_main.cpp
@@ -2,15 +2,6 @@
 #include <mongo/client/init.h>
 
 int main(int argc, char **argv) {
-    const char *port = "27017";
-    if ( argc != 1 ) {
-        if ( argc != 3 ) {
-            std::cout << "need to pass port as second param" << endl;
-            return EXIT_FAILURE;
-        }
-        port = argv[ 2 ];
-    }
-
     ::testing::InitGoogleTest(&argc, argv);
 
     const mongo::Status status = mongo::client::initialize();
