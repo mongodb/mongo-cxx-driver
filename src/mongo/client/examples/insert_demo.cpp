7e83e8f2d62fab371df6a3369156f12634f0900f
Tyler Brock <tyler.brock@gmail.com>
2014-05-01 16:47:18 -0400

    minor: 100k is probably enough, boiling oceans, etc

--- src/mongo/client/examples/insert_demo.cpp
+++ src/mongo/client/examples/insert_demo.cpp
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
         cout << "inserting..." << endl;
 
         time_t start = time(0);
-        for( unsigned i = 0; i < 1000000; i++ ) {
+        for( unsigned i = 0; i < 100000; i++ ) {
             c.insert("test.foo", o);
         }
 
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
 
         time_t done = time(0);
         time_t dt = done-start;
-        cout << dt << " seconds " << 1000000/dt << " per second" << endl;
+        cout << dt << " seconds " << 100000/dt << " per second" << endl;
     }
     catch(DBException& e) {
         cout << "caught DBException " << e.toString() << endl;
