bf98e8af20dc456e9576d2bae9541464e0021f3e
Tyler Brock <tyler.brock@gmail.com>
2014-07-14 11:57:29 -0400

    CXX-281 Merge upstream changes for SERVER-5668
    SERVER-14482 Add add missing appendX methods to BSONArrayBuilder
    
    Conflicts:
    	src/mongo/bson/bsonobjbuilder.h
    	src/mongo/bson/mutable/document.cpp
    	src/mongo/dbtests/jsobjtests.cpp

--- src/mongo/dbtests/jsobjtests.cpp
+++ src/mongo/dbtests/jsobjtests.cpp
@@ -777,21 +777,6 @@ namespace JsobjTests {
             }
         }; DBTEST_SHIM_TEST(AppendAs);
 
-        class ArrayAppendAs {
-        public:
-            void run() {
-                BSONArrayBuilder b;
-                {
-                    BSONObj foo = BSON( "foo" << 1 );
-                    b.appendAs( foo.firstElement(), "3" );
-                }
-                BSONArray a = b.arr();
-                BSONObj expected = BSON( "3" << 1 );
-                ASSERT_EQUALS( expected.firstElement(), a[ 3 ] );
-                ASSERT_EQUALS( 4, a.nFields() );
-            }
-        }; DBTEST_SHIM_TEST(ArrayAppendAs);
-
         class GetField {
         public:
             void run(){
@@ -1718,6 +1703,23 @@ namespace JsobjTests {
 
             objb.appendRegex(objb.numStr(i++), "test", "imx");
             arrb.appendRegex("test", "imx");
+
+            objb.appendBinData(objb.numStr(i++), 4, BinDataGeneral, "wow");
+            arrb.appendBinData(4, BinDataGeneral, "wow");
+
+            objb.appendCode(objb.numStr(i++), "function(){ return 1; }");
+            arrb.appendCode("function(){ return 1; }");
+
+            objb.appendCodeWScope(objb.numStr(i++), "function(){ return a; }", BSON("a" << 1));
+            arrb.appendCodeWScope("function(){ return a; }", BSON("a" << 1));
+
+            time_t dt(0);
+            objb.appendTimeT(objb.numStr(i++), dt);
+            arrb.appendTimeT(dt);
+
+            Date_t date(0);
+            objb.appendDate(objb.numStr(i++), date);
+            arrb.appendDate(date);
 
             objb.append(objb.numStr(i++), BSONRegEx("test2", "s"));
             arrb.append(BSONRegEx("test2", "s"));
