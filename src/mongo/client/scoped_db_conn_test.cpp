220f122a09c5fa63d6839bf93d26b553b2b3b556
Tyler Brock <tyler.brock@gmail.com>
2014-09-12 16:17:00 -0400

    Revert "minor: use INADDR_LOOPBACK instead of INADDR_ANY"
    This reverts commit 3f21cf6d2c892aedcf43f409b30f2f5a85c67775.

--- src/mongo/client/scoped_db_conn_test.cpp
+++ src/mongo/client/scoped_db_conn_test.cpp
@@ -92,7 +92,7 @@ namespace mongo {
             memset(&address, 0, size);
             address.sin_family = AF_INET;
             address.sin_port = htons(port);
-            address.sin_addr.s_addr = INADDR_LOOPBACK;
+            address.sin_addr.s_addr = INADDR_ANY;
         }
 
         sockaddr* get_address() {
