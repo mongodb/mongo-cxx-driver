6e4d72c247369fb9e7d47c060b8a38be4411b52c
Tyler Brock <tyler.brock@gmail.com>
2015-01-29 13:54:18 -0500

    Add private headers

---
+++ src/driver/private/libmongoc.cpp
@@ -0,0 +1,29 @@
+// Copyright 2014 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "libmongoc.hpp"
+
+namespace mongo {
+namespace driver {
+namespace libmongoc {
+
+#ifdef MONGOCXX_TESTING
+#define MONGOCXX_LIBMONGOC_SYMBOL(name) mock::mock<decltype(&mongoc_##name)> name(mongoc_##name);
+#include "libmongoc_symbols.hpp"
+#undef MONGOCXX_LIBMONGOC_SYMBOL
+#endif  // MONGOCXX_TESTING
+
+}  // namespace libmongoc
+}  // namespace driver
+}  // namespace mongo
---
+++ src/driver/private/libmongoc_symbols.hpp
@@ -0,0 +1,76 @@
+// Copyright 2014 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_new)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_execute)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_set_database)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_set_collection)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_set_client)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_set_write_concern)
+//MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_get_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_insert)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_update)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_update_one)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_remove)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_remove_one)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_replace_one)
+MONGOCXX_LIBMONGOC_SYMBOL(bulk_operation_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(client_new_from_uri)
+MONGOCXX_LIBMONGOC_SYMBOL(client_set_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(client_get_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(client_set_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(client_get_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(client_get_database)
+MONGOCXX_LIBMONGOC_SYMBOL(client_get_collection)
+MONGOCXX_LIBMONGOC_SYMBOL(client_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_aggregate)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_count)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_set_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_get_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_set_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_get_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_find)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_find_and_modify)
+MONGOCXX_LIBMONGOC_SYMBOL(collection_drop)
+MONGOCXX_LIBMONGOC_SYMBOL(cursor_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(cursor_next)
+MONGOCXX_LIBMONGOC_SYMBOL(database_set_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(database_get_read_prefs)
+MONGOCXX_LIBMONGOC_SYMBOL(database_set_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(database_get_write_concern)
+MONGOCXX_LIBMONGOC_SYMBOL(database_get_collection)
+MONGOCXX_LIBMONGOC_SYMBOL(database_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_new)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_copy)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_destroy)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_fsync)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_journal)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_w)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_wtimeout)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_wtag)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_set_wmajority)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_fsync)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_journal)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_w)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_wtimeout)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_wtag)
+MONGOCXX_LIBMONGOC_SYMBOL(write_concern_get_wmajority)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_new)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_copy)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_set_mode)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_get_mode)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_set_tags)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_get_tags)
+MONGOCXX_LIBMONGOC_SYMBOL(read_prefs_destroy)
