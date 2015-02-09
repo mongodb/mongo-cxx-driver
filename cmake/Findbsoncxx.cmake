dfe5f02c9c0d3cb632891128cb2facc47b160c42
Adam Midvidy <amidvidy@gmail.com>
2015-02-09 10:43:38 -0500

    add library_dirs to find modules

--- cmake/Findbsoncxx.cmake
+++ cmake/Findbsoncxx.cmake
@@ -6,8 +6,9 @@
 # These vars will be defined:
 # BSONCXX_FOUND - System has bsoncxx
 # BSONCXX_VERSION - The version of bsoncxx we found
-# BSONCXX_INCLUDE_DIRS - The bsoncxx include directories
-# BSONCXX_LIBRARIES - The libraries needed to use bsoncxx
+# BSONCXX_INCLUDE_DIRS - The bsoncxx include directories - pass this to include_directories
+# BSONCXX_LIBRARY_DIRS - The bsoncxx library directories - pass this to link_directories
+# BSONCXX_LIBRARIES - The libraries needed to use bsoncxx - pass this to target_link_libraries
 # BSONCXX_DEFINITIONS - The compiler switches required for using bsoncxx
 
 find_package(PkgConfig)
@@ -19,6 +20,7 @@ find_package_handle_standard_args(BSONCXX
     REQUIRED_VARS
       BSONCXX_FOUND
       BSONCXX_INCLUDE_DIRS
+      BSONCXX_LIBRARY_DIRS
       BSONCXX_LIBRARIES
     VERSION_VAR
       BSONCXX_VERSION
--- cmake/Findmongocxx.cmake
+++ cmake/Findmongocxx.cmake
@@ -6,8 +6,9 @@
 # These vars will be defined:
 # MONGOCXX_FOUND - System has mongocxx
 # MONGOCXX_VERSION - The version of mongocxx we found
-# MONGOCXX_INCLUDE_DIRS - The mongocxx include directories
-# MONGOCXX_LIBRARIES - The libraries needed to use mongocxx
+# MONGOCXX_INCLUDE_DIRS - The mongocxx include directories - pass this to include_directories
+# MONGOCXX_LIBRARY_DIRS - The mongocxx library directories - pass this to link_directories
+# MONGOCXX_LIBRARIES - The libraries needed to use mongocxx - pass this to target_link_libraries
 # MONGOCXX_DEFINITIONS - The compiler switches required for using mongocxx
 
 find_package(PkgConfig)
@@ -19,6 +20,7 @@ find_package_handle_standard_args(MONGOCXX
     REQUIRED_VARS
       MONGOCXX_FOUND
       MONGOCXX_INCLUDE_DIRS
+      MONGOCXX_LIBRARY_DIRS
       MONGOCXX_LIBRARIES
     VERSION_VAR
       MONGOCXX_VERSION
