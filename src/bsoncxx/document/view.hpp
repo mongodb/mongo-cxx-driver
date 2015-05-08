0bb620e1ebdab3ac45f055aeb688f1937aa3d6f1
Yuval Hager <yhager@yhager.com>
2015-05-08 09:43:56 -0700

    CXX-593: added bsoncxx::document::view::empty() helper

--- src/bsoncxx/document/view.hpp
+++ src/bsoncxx/document/view.hpp
@@ -121,6 +121,13 @@ class BSONCXX_API view {
     ///
     std::size_t length() const;
 
+    ///
+    /// Checks if the underlying document is empty
+    ///
+    /// @return true if the underlying document is empty
+    ///
+    bool empty() const;
+
     friend BSONCXX_API bool operator==(view, view);
     friend BSONCXX_API bool operator!=(view, view);
 
