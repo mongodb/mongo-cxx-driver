94343a6cc42587e00f2455574ac2ee6920bd2481
Matt Cotter <matt@mattwc.com>
2015-11-17 18:12:16 -0500

    add comments to exception base

--- src/mongocxx/exception/base.hpp
+++ src/mongocxx/exception/base.hpp
@@ -27,13 +27,36 @@ namespace mongocxx {
 MONGOCXX_INLINE_NAMESPACE_BEGIN
 namespace exception {
 
+///
+/// A class to be used as the base class for all mongocxx exceptions.
+///
 class MONGOCXX_API base : virtual public std::exception {
    public:
+    ///
+    /// Constructs a new base exception.
+    ///
+    /// @param ec
+    ///   The error code associated with this exception.
+    /// @param what_arg
+    ///   An optional message to be returned by `what`.
+    ///
     explicit base(std::error_code ec, std::string what_arg = "");
 
+    ///
+    /// Constructs a new base exception.
+    ///
+    /// @param ec
+    ///   The error code associated with this exception.
+    /// @param raw_server_error
+    ///   The optional raw bson error document to be associated with this exception.
+    /// @param what_arg
+    ///   An optional message to be returned by `what`.
+    ///
     explicit base(std::error_code ec, bsoncxx::document::value raw_server_error,
                   std::string what_arg = "");
 
+    ///
+    /// The optional raw bson error document from the server.
     ///
     /// @returns The raw server error, if it is available.
     ///
