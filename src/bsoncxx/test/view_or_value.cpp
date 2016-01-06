17db0bfd0a23c00ddb30d0e328322fca2ba27e58
Samantha Ritter <samantha.ritter@10gen.com>
2016-01-06 10:59:26 -0500

    CXX-739 Tests for string::view_or_value and new comparison operators

--- src/bsoncxx/test/view_or_value.cpp
+++ src/bsoncxx/test/view_or_value.cpp
@@ -20,54 +20,55 @@
 #include <bsoncxx/document/view.hpp>
 #include <bsoncxx/document/view_or_value.hpp>
 #include <bsoncxx/json.hpp>
+#include <bsoncxx/string/view_or_value.hpp>
+
+using namespace bsoncxx;
 
 using bsoncxx::to_json;
-using bsoncxx::builder::stream::document;
 using bsoncxx::builder::stream::finalize;
-using bsoncxx::document::view_or_value;
 
-TEST_CASE("view_or_value", "[bsoncxx::document::view_or_value]") {
-    auto empty = document{} << finalize;
-    auto doc = document{} << "a" << 1 << finalize;
+TEST_CASE("document::view_or_value", "[bsoncxx::document::view_or_value]") {
+    auto empty = builder::stream::document{} << finalize;
+    auto doc = builder::stream::document{} << "a" << 1 << finalize;
     auto json = to_json(doc.view());
 
     SECTION("can be default-constructed") {
-        view_or_value variant{};
+        document::view_or_value variant{};
         REQUIRE(to_json(variant) == to_json(empty));
     }
 
     SECTION("can be constructed with a view") {
-        view_or_value variant{doc.view()};
+        document::view_or_value variant{doc.view()};
 
         SECTION("can be used as a view") {
             REQUIRE(to_json(variant) == json);
         }
 
         SECTION("can be copy constructed") {
-            view_or_value copied{variant};
+            document::view_or_value copied{variant};
             REQUIRE(to_json(copied) == json);
         }
 
         SECTION("can be copy assigned") {
-            view_or_value copied{empty.view()};
+            document::view_or_value copied{empty.view()};
             {
-                view_or_value temp{doc.view()};
+                document::view_or_value temp{doc.view()};
                 copied = temp;
             }
             REQUIRE(to_json(copied) == json);
         }
 
         SECTION("can be move constructed") {
-            view_or_value temp{doc.view()};
-            view_or_value moved{std::move(temp)};
+            document::view_or_value temp{doc.view()};
+            document::view_or_value moved{std::move(temp)};
             REQUIRE(to_json(moved) == json);
             REQUIRE(to_json(temp) == to_json(empty));
         }
 
         SECTION("can be move assigned") {
-            view_or_value moved{variant.view()};
+            document::view_or_value moved{variant.view()};
             {
-                view_or_value temp{doc.view()};
+                document::view_or_value temp{doc.view()};
                 moved = std::move(temp);
                 REQUIRE(to_json(temp) == to_json(empty));
             }
@@ -77,43 +78,152 @@ TEST_CASE("view_or_value", "[bsoncxx::document::view_or_value]") {
 
     SECTION("can be constructed with a value") {
         auto move_doc = doc;
-        view_or_value variant{std::move(move_doc)};
+        document::view_or_value variant{std::move(move_doc)};
 
         SECTION("can be used as a view") {
             REQUIRE(to_json(variant) == json);
         }
 
         SECTION("can be copy constructed") {
-            view_or_value copied{variant};
+            document::view_or_value copied{variant};
             REQUIRE(to_json(copied) == json);
         }
 
         SECTION("can be copy assigned") {
-            view_or_value copied{empty};
+            document::view_or_value copied{empty};
             {
                 auto temp_doc = doc;
-                view_or_value temp{std::move(temp_doc)};
+                document::view_or_value temp{std::move(temp_doc)};
                 copied = temp;
             }
             REQUIRE(to_json(copied) == json);
         }
 
         SECTION("can be move constructed") {
-            view_or_value temp{doc.view()};
-            view_or_value moved{std::move(temp)};
+            document::view_or_value temp{doc.view()};
+            document::view_or_value moved{std::move(temp)};
             REQUIRE(to_json(moved) == json);
             REQUIRE(to_json(temp) == to_json(empty));
         }
 
         SECTION("can be move assigned") {
-            view_or_value moved{empty};
+            document::view_or_value moved{empty};
             {
                 auto temp_doc = doc;
-                view_or_value temp{std::move(temp_doc)};
+                document::view_or_value temp{std::move(temp_doc)};
                 moved = std::move(temp);
                 REQUIRE(to_json(temp) == to_json(empty));
             }
             REQUIRE(to_json(moved) == json);
         }
+    }
+
+    SECTION("Can be compared to another view_or_value") {
+        SECTION("Compares equal with equal views, regardless of ownership") {
+            document::value temp{doc};
+            document::view_or_value a{std::move(temp)};
+            document::view_or_value b{doc.view()};
+
+            REQUIRE(b == a);
+        }
+
+        SECTION("Compares inequal with different views") {
+            auto temp_a = builder::stream::document{} << "a" << 1 << finalize;
+            auto temp_b = builder::stream::document{} << "b" << 1 << finalize;
+            document::view_or_value a{std::move(temp_a)};
+            document::view_or_value b{std::move(temp_b)};
+
+            REQUIRE(a != b);
+        }
+    }
+
+    SECTION("Can be compared to a plain View") {
+        auto bad_doc = builder::stream::document{} << "blah" << 1 << finalize;
+        document::view_or_value variant(doc.view());
+        REQUIRE(variant == doc.view());
+        REQUIRE(doc.view() == variant);
+        REQUIRE(variant != bad_doc.view());
+        REQUIRE(bad_doc.view() != variant);
+    }
+
+    SECTION("Can be compared to a plain Value") {
+        auto bad_doc = builder::stream::document{} << "blah" << 1 << finalize;
+        document::view_or_value variant(doc.view());
+        REQUIRE(variant == doc);
+        REQUIRE(doc == variant);
+        REQUIRE(variant != bad_doc);
+        REQUIRE(bad_doc != variant);
+    }
+}
+
+TEST_CASE("string::document::view_or_value", "[bsoncxx::string::view_or_value]") {
+    SECTION("can be constructed with a moved-in std::string") {
+        std::string name{"Sally"};
+        string::view_or_value sally{std::move(name)};
+    }
+
+    SECTION("can be constructed with a const std::string&") {
+        std::string name{"Jonny"};
+        string::view_or_value jonny{name};
+
+        SECTION("is non-owning") {
+            REQUIRE(jonny == "Jonny");
+            name[1] = 'e';
+            REQUIRE(jonny == "Jenny");
+        }
+    }
+
+    SECTION("can be constructed with a const char*") {
+        std::string name = "Julia";
+        string::view_or_value julia{name.c_str()};
+
+        SECTION("is non-owning") {
+            REQUIRE(julia == "Julia");
+            name[4] = 'o';
+            REQUIRE(julia == "Julio");
+        }
+    }
+
+    SECTION("can be constructed with a stdx::string_view") {
+        std::string name{"Mike"};
+        stdx::string_view name_view{name};
+        string::view_or_value mike{name_view};
+
+        SECTION("is non-owning") {
+            REQUIRE(mike == "Mike");
+            name[3] = 'a';
+            REQUIRE(mike == "Mika");
+        }
+    }
+
+    SECTION("can be compared to a std::string") {
+        std::string name{"Theo"};
+        std::string other_name{"Tad"};
+        string::view_or_value theo{name};
+
+        REQUIRE(theo == name);
+        REQUIRE(name == theo);
+        REQUIRE(theo != other_name);
+        REQUIRE(other_name != theo);
+    }
+
+    SECTION("can be compared to a const char*") {
+        string::view_or_value bess{"Bess"};
+
+        REQUIRE(bess == "Bess");
+        REQUIRE("Bess" == bess);
+        REQUIRE(bess != "Betty");
+        REQUIRE("Betty" != bess);
+    }
+
+    SECTION("can be compared to a stdx::string_view") {
+        stdx::string_view name{"Carlin"};
+        stdx::string_view other_name{"Cailin"};
+        string::view_or_value carlin{name};
+
+        REQUIRE(carlin == name);
+        REQUIRE(name == carlin);
+        REQUIRE(carlin != other_name);
+        REQUIRE(other_name != carlin);
     }
 }
