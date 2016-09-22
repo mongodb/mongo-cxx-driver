255b6e1b71109e41694da944ccadb26014144065
J. Rassi <rassi@10gen.com>
2016-09-22 13:09:44 -0700

    CXX-971 Add collation to options classes for find_one_and_*

--- src/mongocxx/test/options/find_one_and_delete.cpp
+++ src/mongocxx/test/options/find_one_and_delete.cpp
@@ -29,10 +29,13 @@ TEST_CASE("find_one_and_delete", "[find_one_and_delete][option]") {
 
     options::find_one_and_delete opts{};
 
+    auto collation = document{} << "locale"
+                                << "en_US" << finalize;
     std::chrono::milliseconds ms{400};
     auto projection = document{} << "_id" << false << finalize;
     auto sort = document{} << "x" << -1 << finalize;
 
+    CHECK_OPTIONAL_ARGUMENT(opts, collation, collation.view());
     CHECK_OPTIONAL_ARGUMENT(opts, max_time, ms);
     CHECK_OPTIONAL_ARGUMENT(opts, projection, projection.view());
     CHECK_OPTIONAL_ARGUMENT(opts, sort, sort.view());
--- src/mongocxx/test/options/find_one_and_replace.cpp
+++ src/mongocxx/test/options/find_one_and_replace.cpp
@@ -29,11 +29,14 @@ TEST_CASE("find_one_and_replace", "[find_one_and_replace][option]") {
 
     options::find_one_and_replace opts{};
 
+    auto collation = document{} << "locale"
+                                << "en_US" << finalize;
     std::chrono::milliseconds ms{400};
     auto projection = document{} << "_id" << false << finalize;
     auto sort = document{} << "x" << -1 << finalize;
 
     CHECK_OPTIONAL_ARGUMENT(opts, bypass_document_validation, true);
+    CHECK_OPTIONAL_ARGUMENT(opts, collation, collation.view());
     CHECK_OPTIONAL_ARGUMENT(opts, max_time, ms);
     CHECK_OPTIONAL_ARGUMENT(opts, projection, projection.view());
     CHECK_OPTIONAL_ARGUMENT(opts, return_document, options::return_document::k_before);
