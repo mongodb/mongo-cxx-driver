efdae8214cb1620ba390e3321c758d4f8791ccdb
David Golden <xdg@xdg.me>
2016-11-29 15:04:15 -0500

    Update doxygen files

---
+++ api/mongocxx-3.1.0-rc0/search/all_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['journal',['journal',['../classmongocxx_1_1write__concern.html#a31f2704d70ded409fd8cad5bad29aabd',1,'mongocxx::write_concern::journal(bool journal)'],['../classmongocxx_1_1write__concern.html#a2cfd0c5ece9b978ac02840861907b0c7',1,'mongocxx::write_concern::journal() const ']]]
+];
---
+++ api/mongocxx-3.1.0-rc0/search/classes_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['aggregate',['aggregate',['../classmongocxx_1_1options_1_1aggregate.html',1,'mongocxx::options']]],
+  ['array',['array',['../classbsoncxx_1_1builder_1_1basic_1_1array.html',1,'bsoncxx::builder::basic']]],
+  ['array',['array',['../classbsoncxx_1_1builder_1_1stream_1_1array.html',1,'bsoncxx::builder::stream']]],
+  ['array_5fcontext',['array_context',['../classbsoncxx_1_1builder_1_1stream_1_1array__context.html',1,'bsoncxx::builder::stream']]],
+  ['array_5fcontext_3c_3e',['array_context&lt;&gt;',['../classbsoncxx_1_1builder_1_1stream_1_1array__context.html',1,'bsoncxx::builder::stream']]],
+  ['authentication_5fexception',['authentication_exception',['../classmongocxx_1_1authentication__exception.html',1,'mongocxx']]]
+];
