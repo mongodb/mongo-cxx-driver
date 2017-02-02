54203766f9efcd978cb3c6baf0a41240a5d701b1
J. Rassi <rassi@10gen.com>
2017-02-01 16:09:35 -0800

    CXX-1216 .evergreen/install.sh: build both libs from single archive

--- .evergreen/install.sh
+++ .evergreen/install.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 # Usage examples:
-# ./install.sh libbson master
-# ./install.sh mongoc 1.3.5
-# PREFIX=/tmp/installdir CONFIGURE_ARGS="--enable-debug" ./install.sh mongoc 1.3.5
-
+# ./install.sh master
+# ./install.sh 1.3.5
+# PREFIX=/tmp/installdir ./install.sh 1.3.5
 
 set -o errexit
 set -o xtrace
 
-
 usage() {
-    echo "Please either say mongoc, or libbson, optionally which version"
-    echo "$0 <mongoc|libbson> [master|x.y.z]"
+    echo "Please say which version"
+    echo "$0 <master|x.y.z>"
 }
 [ $# -lt 1 ] && { usage; exit 2; }
 
-VERSION=${2:-master}
+VERSION=${1}
 PREFIX=${PREFIX:-$(pwd)"/deps-install"}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
@@ -39,25 +37,9 @@ case "$OS" in
         ;;
 esac
 
-case "$1" in
-    libbson)
-        LIB="libbson"
-        CONFIGURE_ARGS=${CONFIGURE_ARGS:-$CONFIGURE_ARGS_BSON $CONFIGURE_ARGS_EXTRA}
-    ;;
+echo "About to install C driver ($VERSION) into $PREFIX"
 
-    mongoc)
-        LIB="mongo-c-driver"
-        CONFIGURE_ARGS=${CONFIGURE_ARGS:=$CONFIGURE_ARGS_MONGOC $CONFIGURE_ARGS_EXTRA}
-    ;;
-    *)
-        echo "Got '$2'"
-        usage;
-        exit 2
-    ;;
-esac
-
-echo "About to install $LIB ($VERSION) into $PREFIX configured with '$CONFIGURE_ARGS'"
-
+LIB=mongo-c-driver
 if [ "${VERSION}" = "master" ]; then
     rm -rf $LIB
     # Must be http as rhel55 has https issues
@@ -74,26 +56,38 @@ fi
 
 cd $DIR
 
-case "$OS" in
-    darwin)
-        PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure $CONFIGURE_ARGS
-        make "-j$(sysctl -n hw.logicalcpu)"
-        make install
-        ;;
+compile_and_install() {
+    if [ -f /proc/cpuinfo ]; then
+        CONCURRENCY=$(grep -c ^processor /proc/cpuinfo)
+    elif which sysctl; then
+        CONCURRENCY=$(sysctl -n hw.logicalcpu)
+    else
+        echo "$0: can't figure out what value of -j to pass to 'make'" >&2
+        exit 1
+    fi
 
-    linux)
-        PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure $CONFIGURE_ARGS
-        make "-j$(grep -c ^processor /proc/cpuinfo)"
-        make install
-        ;;
+    case "$OS" in
+        darwin|linux)
+            PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure "$@"
+            make "-j$CONCURRENCY"
+            make install
+            ;;
 
-    cygwin*)
-        /cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" $CONFIGURE_ARGS
-        "/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
-        ;;
+        cygwin*)
+            /cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" "$@"
+            "/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
+            ;;
 
-    *)
-        echo "$0: unsupported platform '$OS'" >&2
-        exit 2
-        ;;
-esac
+        *)
+            echo "$0: unsupported platform '$OS'" >&2
+            exit 2
+            ;;
+    esac
+}
+
+# Compile and install libbson.
+( cd src/libbson && compile_and_install ${CONFIGURE_ARGS_BSON} ${CONFIGURE_ARGS_EXTRA} )
+
+# Compile and install libmongoc.
+compile_and_install ${CONFIGURE_ARGS_MONGOC} ${CONFIGURE_ARGS_EXTRA}
+
