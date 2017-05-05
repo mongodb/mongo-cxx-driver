f072758813df7b88a724e4af51b08715f1fafc9b
J. Rassi <rassi@10gen.com>
2017-05-05 10:44:25 -0700

    CXX-1257 Stop using special vars for MSBuild.exe/cmake in CI scripts
    MSBuild.exe/cmake are now accessed via $PATH as usual, which
    simplifies the environment variable management in these scripts.

--- .evergreen/compile.sh
+++ .evergreen/compile.sh
@@ -7,8 +7,6 @@
 # the default build directory './build'.  The following environment variables will change the
 # behavior of this script:
 # - BUILD_TYPE: must be set to "Release" or "Debug"
-# - PATH_TO_CMAKE: full path to cmake (defaults to searching $PATH)
-# - PATH_TO_BUILD_TOOL: full path to make / msbuild.exe (defaults to searching $PATH)
 
 set -o xtrace
 set -o errexit
@@ -21,10 +19,8 @@ fi
 
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-PATH_TO_CMAKE="${PATH_TO_CMAKE:-cmake}"
-
 cd build
-"$PATH_TO_CMAKE" "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}" "$@" ..
+cmake "-DCMAKE_BUILD_TYPE=${BUILD_TYPE}" "$@" ..
 
 case "$OS" in
     darwin|linux)
@@ -36,16 +32,14 @@ case "$OS" in
             echo "$0: can't figure out what value of -j to pass to 'make'" >&2
             exit 1
         fi
-        PATH_TO_BUILD_TOOL="${PATH_TO_BUILD_TOOL:-make}"
-        "$PATH_TO_BUILD_TOOL" "-j$CONCURRENCY" all
-        "$PATH_TO_BUILD_TOOL" install
-        "$PATH_TO_BUILD_TOOL" "-j$CONCURRENCY" examples
+        make "-j$CONCURRENCY" all VERBOSE=1
+        make install VERBOSE=1
+        make "-j$CONCURRENCY" examples VERBOSE=1
         ;;
     cygwin*)
-        PATH_TO_BUILD_TOOL="${PATH_TO_BUILD_TOOL:-msbuild.exe}"
-        "$PATH_TO_BUILD_TOOL" /p:Configuration=${BUILD_TYPE} /m ALL_BUILD.vcxproj
-        "$PATH_TO_BUILD_TOOL" /p:Configuration=${BUILD_TYPE} INSTALL.vcxproj
-        "$PATH_TO_BUILD_TOOL" /p:Configuration=${BUILD_TYPE} /m examples/examples.vcxproj
+        MSBuild.exe /p:Configuration=${BUILD_TYPE} /m ALL_BUILD.vcxproj
+        MSBuild.exe /p:Configuration=${BUILD_TYPE} INSTALL.vcxproj
+        MSBuild.exe /p:Configuration=${BUILD_TYPE} /m examples/examples.vcxproj
         ;;
     *)
         echo "$0: unsupported platform '$OS'" >&2
