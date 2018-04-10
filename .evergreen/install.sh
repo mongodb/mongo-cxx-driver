9e155e5298442a2be22719ff6c3b00546a5784b5
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-04-09 21:56:28 -0400

    CXX-1498 Update libmongoc from 1.9.2 to master
    Build libmongoc with CMake. Also solves CXX-1204 and CXX-1550.

--- .evergreen/install.sh
+++ .evergreen/install.sh
@@ -15,28 +15,13 @@ usage() {
 [ $# -lt 1 ] && { usage; exit 2; }
 
 VERSION=${1}
-PREFIX=${PREFIX:-$(pwd)"/deps-install"}
+PREFIX=${PREFIX:-$(pwd)"/../mongoc/"}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-# TODO CXX-1204: improve/tweak the flags below.
-case "$OS" in
-    darwin|linux)
-        CONFIGURE_ARGS_MONGOC="--disable-automatic-init-and-cleanup --disable-shm-counters --with-libbson=system"
-        CONFIGURE_ARGS_BSON="--disable-extra-align"
-        CONFIGURE_ARGS_EXTRA="--enable-tests=no --enable-examples=no --enable-debug --enable-optimizations --enable-static --disable-dependency-tracking --with-pic --prefix=$PREFIX"
-        ;;
-
-    cygwin*)
-        CONFIGURE_ARGS_MONGOC=
-        CONFIGURE_ARGS_BSON=
-        CONFIGURE_ARGS_EXTRA="-DCMAKE_INSTALL_PREFIX=$(cygpath -m "$PREFIX")"
-        ;;
-
-    *)
-        echo "$0: unsupported platform '$OS'" >&2
-        exit 2
-        ;;
-esac
+CMAKE_ARGS="
+  -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_SHM_COUNTERS=OFF -DENABLE_BSON=BUNDLED
+  -DENABLE_TESTS=OFF -DENABLE_EXAMPLES=OFF -DENABLE_STATIC=ON -DENABLE_EXTRA_ALIGNMENT=OFF
+  -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_PREFIX=$PREFIX"
 
 echo "About to install C driver ($VERSION) into $PREFIX"
 
@@ -55,40 +40,46 @@ else
     tar --extract --file $LIB.tgz
 fi
 
+if [ -f "/Applications/cmake-3.2.2-Darwin-x86_64/CMake.app/Contents/bin/cmake" ]; then
+    CMAKE="/Applications/cmake-3.2.2-Darwin-x86_64/CMake.app/Contents/bin/cmake"
+elif [ -f "/Applications/Cmake.app/Contents/bin/cmake" ]; then
+    CMAKE="/Applications/Cmake.app/Contents/bin/cmake"
+elif [ -f "/opt/cmake/bin/cmake" ]; then
+    CMAKE="/opt/cmake/bin/cmake"
+elif command -v cmake 2>/dev/null; then
+    CMAKE=cmake
+fi
+
 cd $DIR
 
-compile_and_install() {
-    if [ -f /proc/cpuinfo ]; then
-        CONCURRENCY=$(grep -c ^processor /proc/cpuinfo)
-    elif which sysctl; then
-        CONCURRENCY=$(sysctl -n hw.logicalcpu)
-    else
-        echo "$0: can't figure out what value of -j to pass to 'make'" >&2
-        exit 1
-    fi
+if [ -f /proc/cpuinfo ]; then
+    CONCURRENCY=$(grep -c ^processor /proc/cpuinfo)
+elif which sysctl; then
+    CONCURRENCY=$(sysctl -n hw.logicalcpu)
+else
+    echo "$0: can't figure out what value of -j to pass to 'make'" >&2
+    exit 1
+fi
 
-    case "$OS" in
-        darwin|linux)
-            PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure "$@"
-            make "-j$CONCURRENCY"
-            make install
-            ;;
+case "$OS" in
+    darwin|linux)
+        PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" $CMAKE $CMAKE_ARGS .
+        make "-j$CONCURRENCY"
+        make install
+        ;;
 
-        cygwin*)
-            /cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" "$@"
-            "/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
-            ;;
+    cygwin*)
+        /cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" $CMAKE_ARGS .
+        "/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
+        ;;
 
-        *)
-            echo "$0: unsupported platform '$OS'" >&2
-            exit 2
-            ;;
-    esac
-}
+    *)
+        echo "$0: unsupported platform '$OS'" >&2
+        exit 2
+        ;;
+esac
 
-# Compile and install libbson.
-( cd src/libbson && compile_and_install ${CONFIGURE_ARGS_BSON} ${CONFIGURE_ARGS_EXTRA} )
+echo "Done installing"
 
-# Compile and install libmongoc.
-compile_and_install ${CONFIGURE_ARGS_MONGOC} ${CONFIGURE_ARGS_EXTRA}
-
+cd ..
+ls -l ..
