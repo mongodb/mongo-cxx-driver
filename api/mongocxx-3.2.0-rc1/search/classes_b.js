d9dd96dc5166b1313424cbe0a14f5218dc089763
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-05-31 14:34:43 -0400

    Update doxygen files

--- api/mongocxx-3.2.0-rc1/search/classes_b.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['md5_5fstate_5fs',['md5_state_s',['../structmd5__state__s.html',1,'']]],
-  ['modify_5fcollection',['modify_collection',['../classmongocxx_1_1options_1_1modify__collection.html',1,'mongocxx::options']]]
-];
---
+++ api/mongocxx-3.3.0-rc0/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "abcdefghijklmnopqrstuvw~",
+  1: "abcdefghiklmopqrsuvw",
+  2: "bm",
+  3: "abcdefghijklmnoprstuvw~",
+  4: "belrstvw",
+  5: "bk",
+  6: "o",
+  7: "dmt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "enums",
+  5: "enumvalues",
+  6: "related",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Enumerations",
+  5: "Enumerator",
+  6: "Friends",
+  7: "Pages"
+};
+
