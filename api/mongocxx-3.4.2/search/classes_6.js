b22f831e13c571eea0cbf7c1e21dac1f818b4945
Kevin Albertson <kevin.albertson@mongodb.com>
2020-06-03 17:24:48 -0400

    Update doxygen files

---
+++ api/mongocxx-3.4.2/search/classes_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['gridfs_5fexception',['gridfs_exception',['../classmongocxx_1_1gridfs__exception.html',1,'mongocxx']]]
+];
---
+++ api/mongocxx-3.5.1/search/namespaces_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['bsoncxx',['bsoncxx',['../namespacebsoncxx.html',1,'']]]
+];
