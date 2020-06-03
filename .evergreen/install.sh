32535030739935f859edd924f889175ca513f440
Kevin Albertson <kevin.albertson@mongodb.com>
2020-06-03 07:40:04 -0400

    install c driver

--- .evergreen/install.sh
+++ .evergreen/install.sh
@@ -15,28 +15,13 @@ usage() {
 [ $# -lt 1 ] && { usage; exit 2; }
 
 VERSION=${1}
-PREFIX=${PREFIX:-$(pwd)"/deps-install"}
+PREFIX=${PREFIX:-$(pwd)"/../mongoc/"}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 
-# TODO CXX-1204: improve/tweak the flags below.
-case "$OS" in
-    darwin|linux)
-        CONFIGURE_ARGS_MONGOC="--disable-automatic-init-and-cleanup --disable-shm-counters --with-libbson=system"
-        CONFIGURE_ARGS_BSON="--disable-extra-align"
-        CONFIGURE_ARGS_EXTRA="--enable-tests=no --enable-examples=no --enable-debug --enable-optimizations --enable-static --disable-dependency-tracking --with-pic --prefix=$PREFIX"
-        ;;
-
-    cygwin*)
-        CONFIGURE_ARGS_MONGOC=
-        CONFIGURE_ARGS_BSON=
-        CONFIGURE_ARGS_EXTRA="-DCMAKE_INSTALL_PREFIX=$(cygpath -m "$PREFIX")"
-        ;;
-
-    *)
-        echo "$0: unsupported platform '$OS'" >&2
-        exit 2
-        ;;
-esac
+CMAKE_ARGS="
+  -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_SHM_COUNTERS=OFF
+  -DENABLE_TESTS=OFF -DENABLE_EXAMPLES=OFF -DENABLE_STATIC=ON -DENABLE_EXTRA_ALIGNMENT=OFF
+  -DCMAKE_MACOSX_RPATH=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_PREFIX_PATH=$PREFIX"
 
 echo "About to install C driver ($VERSION) into $PREFIX"
 
@@ -55,40 +40,79 @@ else
     tar --extract --file $LIB.tgz
 fi
 
+. .evergreen/find_cmake.sh
+
 cd $DIR
 
-compile_and_install() {
-    if [ -f /proc/cpuinfo ]; then
-        CONCURRENCY=$(grep -c ^processor /proc/cpuinfo)
-    elif which sysctl; then
-        CONCURRENCY=$(sysctl -n hw.logicalcpu)
-    else
-        echo "$0: can't figure out what value of -j to pass to 'make'" >&2
-        exit 1
-    fi
+if [ -f /proc/cpuinfo ]; then
+    CONCURRENCY=$(grep -c ^processor /proc/cpuinfo)
+elif which sysctl; then
+    CONCURRENCY=$(sysctl -n hw.logicalcpu)
+else
+    echo "$0: can't figure out what value of -j to pass to 'make'" >&2
+    exit 1
+fi
 
-    case "$OS" in
-        darwin|linux)
-            PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig" ./configure "$@"
-            make "-j$CONCURRENCY"
-            make install
-            ;;
+export CFLAGS="-fPIC"
 
-        cygwin*)
-            /cygdrive/c/cmake/bin/cmake -G "Visual Studio 14 2015 Win64" "$@"
-            "/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin/MSBuild.exe" /m INSTALL.vcxproj
-            ;;
+case "$OS" in
+    darwin|linux)
+	# build libbson
+	mkdir cmake_build
+	cd cmake_build
+	$CMAKE -DENABLE_MONGOC=OFF $CMAKE_ARGS ..
+	make "-j$CONCURRENCY"
+	make install
+	cd ../../
 
-        *)
-            echo "$0: unsupported platform '$OS'" >&2
-            exit 2
-            ;;
-    esac
-}
+	# fetch and build libmongocrypt
+	git clone https://github.com/mongodb/libmongocrypt
+	mkdir libmongocrypt/cmake_build
+	cd libmongocrypt/cmake_build
+	$CMAKE -DENABLE_SHARED_BSON=ON -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_PREFIX_PATH="$PREFIX" -DCMAKE_BUILD_TYPE="Debug" -DENABLE_CLIENT_SIDE_ENCRYPTION=OFF ..
+	make install
+	cd ../../$DIR
 
-# Compile and install libbson.
-( cd src/libbson && compile_and_install ${CONFIGURE_ARGS_BSON} ${CONFIGURE_ARGS_EXTRA} )
+	# build libmongoc
+	cd cmake_build
+        $CMAKE -DENABLE_MONGOC=ON -DENABLE_CLIENT_SIDE_ENCRYPTION=ON $CMAKE_ARGS ..
+        make "-j$CONCURRENCY"
+        make install
+	cd ../
+        ;;
 
-# Compile and install libmongoc.
-compile_and_install ${CONFIGURE_ARGS_MONGOC} ${CONFIGURE_ARGS_EXTRA}
+    cygwin*)
+        GENERATOR=${GENERATOR:-"Visual Studio 14 2015 Win64"}
 
+	# build libbson
+	mkdir cmake_build
+	cd cmake_build
+	"$CMAKE" -G "$GENERATOR" -DENABLE_MONGOC=OFF $CMAKE_ARGS ..
+	"$CMAKE" --build . --target INSTALL --config "Debug" -- /m
+	cd ../../
+
+	# fetch and build libmongocrypt
+	git clone https://github.com/mongodb/libmongocrypt
+	mkdir libmongocrypt/cmake_build
+	cd libmongocrypt/cmake_build
+	"$CMAKE" -G "$GENERATOR" -DENABLE_SHARED_BSON=ON -DCMAKE_BUILD_TYPE="Debug" $CMAKE_ARGS ..
+	"$CMAKE" --build . --target INSTALL --config "Debug" -- /m
+	cd ../../$DIR
+
+	# build libmongoc
+	cd cmake_build
+        "$CMAKE" -G "$GENERATOR" -DENABLE_MONGOC=ON -DENABLE_CLIENT_SIDE_ENCRYPTION=ON $CMAKE_ARGS ..
+	"$CMAKE" --build . --target INSTALL --config "Debug" -- /m
+	cd ../
+        ;;
+
+    *)
+        echo "$0: unsupported platform '$OS'" >&2
+        exit 2
+        ;;
+esac
+
+echo "Done installing"
+
+cd ..
+ls -l ..
