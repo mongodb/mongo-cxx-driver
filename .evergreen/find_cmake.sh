cda89b0843926f2d3c776b602e544c77fa965a89
Kevin Albertson <kevin.albertson@mongodb.com>
2020-06-03 13:43:18 -0400

    Remove unused bash var from find_cmake.sh

--- .evergreen/find_cmake.sh
+++ .evergreen/find_cmake.sh
@@ -2,8 +2,6 @@
 set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-
 find_cmake ()
 {
   if [ ! -z "$CMAKE" ]; then
