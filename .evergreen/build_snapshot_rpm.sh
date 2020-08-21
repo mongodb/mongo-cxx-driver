738b1ec1c6014b12bb96661cbe97e873fb272fda
Roberto C. Sánchez <roberto@connexer.com>
2020-08-21 13:46:30 -0400

    CXX-2093 fix rpm-package-build task

--- .evergreen/build_snapshot_rpm.sh
+++ .evergreen/build_snapshot_rpm.sh
@@ -41,7 +41,7 @@ done
 package=mongo-cxx-driver
 spec_file=../mongo-cxx-driver.spec
 spec_url=https://src.fedoraproject.org/rpms/mongo-cxx-driver/raw/master/f/mongo-cxx-driver.spec
-config=${MOCK_TARGET_CONFIG:=fedora-33-x86_64}
+config=${MOCK_TARGET_CONFIG:=fedora-32-x86_64}
 
 if [ ! -x /usr/bin/rpmbuild -o ! -x /usr/bin/rpmspec ]; then
   echo "Missing the rpmbuild or rpmspec utility from the rpm-build package"
