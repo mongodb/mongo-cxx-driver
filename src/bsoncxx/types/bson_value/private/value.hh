9beffd312ff58b9660241f14a36807f15f4106b3
Clyde Bazile <clyde.bazile@10gen.com>
2020-09-14 13:20:18 -0400

    114201 Uninitialized scalar field

--- src/bsoncxx/types/bson_value/private/value.hh
+++ src/bsoncxx/types/bson_value/private/value.hh
@@ -33,7 +33,7 @@ class value::impl {
 
     impl() {
         // Initialize the value to null for safe destruction.
-        _value.value_type = BSON_TYPE_NULL;
+        _value = {.value_type = BSON_TYPE_NULL};
     }
 
     ~impl() {
