96ce19fb21aefaa00872fafd8c6436cd1eadfacf
Roberto C. Sánchez <roberto@connexer.com>
2020-10-27 19:08:29 -0400

    (Debian packaging) CXX-1747 allow building Debian packages with MNMLSTC

--- .evergreen/debian_package_build.sh
+++ .evergreen/debian_package_build.sh
@@ -17,6 +17,10 @@ on_exit () {
 }
 trap on_exit EXIT
 
+if [ ! -z "${DEB_BUILD_PROFILES}" ]; then
+   echo "DEB_BUILD_PROFILES was set; building with profiles: ${DEB_BUILD_PROFILES}"
+fi
+
 if [ "${IS_PATCH}" = "true" ]; then
   git diff HEAD -- . ':!debian' > ../upstream.patch
   git diff HEAD -- debian > ../debian.patch
@@ -36,6 +40,10 @@ if [ "${IS_PATCH}" = "true" ]; then
     git log -n1 -p
   fi
 fi
+if [ "${DEB_BUILD_PROFILES#*pkg.mongo-cxx-driver.mnmlstc}" != "${DEB_BUILD_PROFILES}" ]; then
+   MNMLSTC_DEPS="git ca-certificates"
+fi
+
 
 cd ..
 
@@ -43,8 +51,8 @@ git clone https://salsa.debian.org/installer-team/debootstrap.git debootstrap.gi
 export DEBOOTSTRAP_DIR=`pwd`/debootstrap.git
 sudo -E ./debootstrap.git/debootstrap unstable ./unstable-chroot/ http://cdn-aws.deb.debian.org/debian
 cp -a mongo-cxx-driver ./unstable-chroot/tmp/
-sudo chroot ./unstable-chroot /bin/bash -c "(set -o xtrace && \
-  apt-get install -y build-essential git-buildpackage fakeroot debhelper cmake curl ca-certificates libboost-dev libsasl2-dev libicu-dev doxygen && \
+sudo DEB_BUILD_PROFILES="${DEB_BUILD_PROFILES}" chroot ./unstable-chroot /bin/bash -c "(set -o xtrace && \
+  apt-get install -y build-essential git-buildpackage fakeroot debhelper cmake curl ca-certificates libboost-dev libsasl2-dev libicu-dev doxygen ${MNMLSTC_DEPS} && \
   mkdir /tmp/mongo-c-driver && \
   curl -o deb.tar.gz -L https://s3.amazonaws.com/mciuploads/mongo-c-driver/master/mongo-c-driver-debian-packages-latest.tar.gz && \
   tar zxvf deb.tar.gz && \
@@ -55,7 +63,7 @@ sudo chroot ./unstable-chroot /bin/bash -c "(set -o xtrace && \
   python3 etc/calc_release_version.py > build/VERSION_CURRENT && \
   git add --force build/VERSION_CURRENT && \
   git commit build/VERSION_CURRENT -m 'Set current version' && \
-  LANG=C /bin/bash ./debian/build_snapshot.sh && \
+  LANG=C /bin/bash -x ./debian/build_snapshot.sh && \
   debc ../*.changes && \
   dpkg -i ../*.deb && \
   /usr/bin/g++ -I/usr/include/bsoncxx/v_noabi -I/usr/include/mongocxx/v_noabi -o runcommand_examples examples/mongocxx/mongodb.com/runcommand_examples.cpp -lmongocxx -lbsoncxx && \
