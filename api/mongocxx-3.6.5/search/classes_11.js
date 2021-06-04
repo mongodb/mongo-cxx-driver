109cad4b7432dde448c336fb563cb30b2098f074
Kevin Albertson <kevin.albertson@mongodb.com>
2021-06-04 16:40:30 -0400

    Update doxygen files

---
+++ api/mongocxx-3.6.5/search/classes_11.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['update_723',['update',['../classmongocxx_1_1result_1_1update.html',1,'mongocxx::result::update'],['../classmongocxx_1_1options_1_1update.html',1,'mongocxx::options::update']]],
+  ['update_5fmany_724',['update_many',['../classmongocxx_1_1model_1_1update__many.html',1,'mongocxx::model']]],
+  ['update_5fone_725',['update_one',['../classmongocxx_1_1model_1_1update__one.html',1,'mongocxx::model']]],
+  ['upload_726',['upload',['../classmongocxx_1_1result_1_1gridfs_1_1upload.html',1,'mongocxx::result::gridfs::upload'],['../classmongocxx_1_1options_1_1gridfs_1_1upload.html',1,'mongocxx::options::gridfs::upload']]],
+  ['uploader_727',['uploader',['../classmongocxx_1_1gridfs_1_1uploader.html',1,'mongocxx::gridfs']]],
+  ['uri_728',['uri',['../classmongocxx_1_1uri.html',1,'mongocxx']]]
+];
---
+++ api/mongocxx-3.6.5/search/classes_c.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pipeline_702',['pipeline',['../classmongocxx_1_1pipeline.html',1,'mongocxx']]],
+  ['pool_703',['pool',['../classmongocxx_1_1pool.html',1,'mongocxx::pool'],['../classmongocxx_1_1options_1_1pool.html',1,'mongocxx::options::pool']]]
+];
