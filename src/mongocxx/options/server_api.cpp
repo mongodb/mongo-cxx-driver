7bbf767c2a1949fabc6a5de615cd9f8cd90e04d7
Jesse Williamson <jesse.williamson@mongodb.com>
2021-07-27 14:27:45 -0700

    Fix CXX-2329; change type of thrown exception to match documentation.
    Modify exceptions throw to use mongocxx::logic_error.
    
    Adds std::error_code/std::errc plumbing, as mongocxx::exception
    derives from std::system_exception, and mongocxx::logic_error
    derives in turn from that.
    
    See: https://jira.mongodb.org/browse/CXX-2329
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/mongocxx/options/server_api.cpp
+++ src/mongocxx/options/server_api.cpp
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <system_error>
+
 #include <bsoncxx/stdx/string_view.hpp>
 #include <mongocxx/exception/logic_error.hpp>
 #include <mongocxx/options/server_api.hpp>
@@ -29,7 +31,7 @@ std::string server_api::version_to_string(server_api::version version) {
         case server_api::version::k_version_1:
             return "1";
         default:
-            throw std::logic_error{"invalid server API version"};
+            throw mongocxx::logic_error{std::make_error_code(std::errc::invalid_argument), "invalid server API version"};
     }
 }
 
@@ -37,7 +39,7 @@ server_api::version server_api::version_from_string(stdx::string_view version) {
     if (!version.compare("1")) {
         return server_api::version::k_version_1;
     }
-    throw std::logic_error{"invalid server API version"};
+    throw mongocxx::logic_error{std::make_error_code(std::errc::invalid_argument), "invalid server API version"};
 }
 
 server_api::server_api(server_api::version version) : _version(std::move(version)) {}
