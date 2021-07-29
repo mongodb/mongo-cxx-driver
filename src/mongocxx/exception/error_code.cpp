55b93c84d198dc974d96e57f9128711928073f64
Jesse Williamson <jesse.williamson@mongodb.com>
2021-07-29 15:57:50 -0700

    Added new mongocxx::error_code for failing to create a resource.
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/mongocxx/exception/error_code.cpp
+++ src/mongocxx/exception/error_code.cpp
@@ -1,4 +1,4 @@
-// Copyright 2015 MongoDB Inc.
+// Copyright 2015-present MongoDB Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -84,6 +84,8 @@ class error_category final : public std::error_category {
                 return "an invalid client session was provided";
             case error_code::k_invalid_transaction_options_object:
                 return "an invalid transactions options object was provided";
+	    case error_code::k_create_resource_fail:
+		return "could not create resource";
             default:
                 return "unknown mongocxx error";
         }
