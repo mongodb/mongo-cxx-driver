fd84da871c35cbc4beee0b39afa188b84c698fc1
Jesse Williamson <jesse.williamson@mongodb.com>
2021-09-17 11:44:54 -0700

    CXX-2342 add serviceid to events (#823)
    * Set up bsoncxx spuport for catch, etc..
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Update event commands.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Import rewritten mongoc_symbols
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Add unit test for serviceId availability.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Various cleanup; fix nullopt_t usage; remove is_load_balanced()
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Tests for mocked load-balanced or non-load-balanced service_id.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Apply clang-format.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Cleanups in response to review comments.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Small fixups in response to review.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>
    
    * Update test, move function object to lambda.
    
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/bsoncxx/private/helpers.hh
+++ src/bsoncxx/private/helpers.hh
@@ -1,4 +1,4 @@
-// Copyright 2015 MongoDB Inc.
+// Copyright 2015-present MongoDB Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 #include <bsoncxx/document/value.hpp>
 #include <bsoncxx/document/view.hpp>
+#include <bsoncxx/oid.hpp>
 #include <bsoncxx/private/libbson.hh>
 
 #include <bsoncxx/config/private/prelude.hh>
@@ -31,6 +32,20 @@ inline document::view view_from_bson_t(const bson_t* bson) {
 
 inline document::value value_from_bson_t(const bson_t* bson) {
     return document::value{view_from_bson_t(bson)};
+}
+
+/*
+Construct an oid from a bson_oid_t (which is a C API type that we don't want to
+expose to the world).
+
+Note: passing a nullptr is unguarded
+Note: Deduction guides aren't yet available to us, so a factory it is! This is
+something that can be improved as part of CXX-2350 (migration to more recent C++
+standards).
+*/
+inline bsoncxx::oid make_oid(const bson_oid_t* bson_oid) {
+    return bsoncxx::oid(reinterpret_cast<const char*>(bson_oid),
+                        bsoncxx::oid::size());
 }
 
 }  // namespace helpers
