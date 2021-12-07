d7e781ec09b25195c5ab1b3bd8f7f83315ffbf10
Jesse Williamson <jesse.williamson@mongodb.com>
2021-12-07 15:54:00 -0800

    Rework throw_exception<> and bsoncxx/mongocxx error code conversions.
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/mongocxx/exception/private/mongoc_error.hh
+++ src/mongocxx/exception/private/mongoc_error.hh
@@ -24,46 +24,16 @@
 namespace mongocxx {
 MONGOCXX_INLINE_NAMESPACE_BEGIN
 
-inline std::error_code make_error_code(int code, int) {
-    // Domain is ignored. We simply issue the code.
-    return {code, server_error_category()};
-}
-
-inline std::error_code make_error_code(const ::bson_error_t& error) {
-    return make_error_code(static_cast<int>(error.code), static_cast<int>(error.domain));
-}
-
-inline void set_bson_error_message(bson_error_t* error, const char* msg) {
-    bson_strncpy(error->message,
-                 msg,
-                 std::min(strlen(msg) + 1, static_cast<size_t>(BSON_ERROR_BUFFER_SIZE)));
-}
-
-inline void make_bson_error(bson_error_t* error, const operation_exception& e) {
+inline void make_bson_error(bson_error_t* error, const mongocxx::operation_exception& e) {
     // No way to get the domain back out of the exception, so zero out.
     error->code = static_cast<uint32_t>(e.code().value());
     error->domain = 0;
     set_bson_error_message(error, e.what());
 }
 
-inline void make_generic_bson_error(bson_error_t* error) {
-    // CDRIVER-3524 Zero these out since we don't have them.
-    error->code = 0;
-    error->domain = 0;
-    set_bson_error_message(error, "unknown error");
-}
-
-template <typename exception_type>
-void throw_exception(const ::bson_error_t& error) {
-    throw exception_type{make_error_code(error), error.message};
-}
-
-template <typename exception_type>
-void throw_exception(bsoncxx::document::value raw_server_error, const ::bson_error_t& error) {
-    throw exception_type{make_error_code(error), std::move(raw_server_error), error.message};
-}
-
 MONGOCXX_INLINE_NAMESPACE_END
 }  // namespace mongocxx
 
 #include <mongocxx/config/private/postlude.hh>
+
+
