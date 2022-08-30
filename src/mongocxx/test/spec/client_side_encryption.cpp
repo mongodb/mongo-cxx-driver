0e6b37e507a2a550ac0d09fe2a8aadbe79fa2fb9
Ezra Chung <ezra.chung@mongodb.com>
2022-08-30 12:46:53 -0500

    CXX-2488 Sync legacy CSE spec tests with 6a7158d5

--- src/mongocxx/test/spec/client_side_encryption.cpp
+++ src/mongocxx/test/spec/client_side_encryption.cpp
@@ -284,8 +284,9 @@ void run_encryption_tests_in_file(const std::string& test_path) {
 
 TEST_CASE("Client side encryption spec automated tests", "[client_side_encryption_spec]") {
     instance::current();
-    /* Tests that use operations that the C++ driver does not have. */
-    std::set<std::string> unsupported_tests = {"count.json", "unsupportedCommand.json"};
+
+    std::set<std::string> unsupported_tests = {
+        "badQueries.json", "count.json", "unsupportedCommand.json"};
 
     char* encryption_tests_path = std::getenv("ENCRYPTION_TESTS_PATH");
     REQUIRE(encryption_tests_path);
@@ -306,7 +307,7 @@ TEST_CASE("Client side encryption spec automated tests", "[client_side_encryptio
     while (std::getline(test_files, test_file)) {
         if (std::find(unsupported_tests.begin(), unsupported_tests.end(), test_file) !=
             unsupported_tests.end()) {
-            WARN("skipping " << test_file << " due to unsupported operation");
+            WARN("skipping " << test_file);
             continue;
         }
         run_encryption_tests_in_file(path + "/" + test_file);
