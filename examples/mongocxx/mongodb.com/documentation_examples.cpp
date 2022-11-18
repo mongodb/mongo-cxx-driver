a919fb527f7ea4a1d74cc61d27e90a61f6c35295
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2022-11-18 20:44:14 +0000

    try 1 second sleep?

--- examples/mongocxx/mongodb.com/documentation_examples.cpp
+++ examples/mongocxx/mongodb.com/documentation_examples.cpp
@@ -1215,10 +1215,12 @@ static void snapshot_examples(mongocxx::client& client) {
     db["dogs"].insert_one(make_document(kvp("adoptable", false)), write_options);
 
     // Sleep to avoid this error:
+    //
     // Unable to read from a snapshot due to pending collection catalog changes;
     // please retry the operation.
+    //
     // See: https://jira.mongodb.org/browse/SERVER-41532
-    // std::this_thread::sleep_for(std::chrono::seconds(120));
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
     int64_t adoptable_pets_count = 0;
 
