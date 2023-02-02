61bacf3097fc38f067ad3423cd8b5dffe4e4a5e1
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-02-02 09:47:49 -0600

    removed unnecessary headers

--- src/mongocxx/test/spec/uri.cpp
+++ src/mongocxx/test/spec/uri.cpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <algorithm>
-#include <cctype>
 #include <fstream>
 #include <mutex>
 #include <thread>
