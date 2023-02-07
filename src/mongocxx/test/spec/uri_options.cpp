616803ea312305f4c07ea797be860003d35aabbd
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-02-07 15:37:57 -0600

    fix windows build

--- src/mongocxx/test/spec/uri_options.cpp
+++ src/mongocxx/test/spec/uri_options.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cctype>
 #include <fstream>
 
 #include <bsoncxx/test_util/catch.hh>
