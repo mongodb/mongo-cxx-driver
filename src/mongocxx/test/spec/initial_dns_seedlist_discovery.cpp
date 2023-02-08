6265fa040ed522e23da0bb7b97d32057d1d874ae
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-02-08 11:27:24 -0600

    fmt

--- src/mongocxx/test/spec/initial_dns_seedlist_discovery.cpp
+++ src/mongocxx/test/spec/initial_dns_seedlist_discovery.cpp
@@ -227,7 +227,7 @@ static void iterate_srv_max_hosts_tests(std::string dir, std::vector<std::string
     for (const auto& file : files) {
         auto test_doc = _doc_from_file("/" + dir + "/" + file);
         auto test = InitialDNSSeedlistTest::parse(test_doc);
-        SECTION (file) {
+        SECTION(file) {
             run_srv_max_hosts_test_file(test);
         }
     }
