7d33e750238c89a097bd2781945687dd00a42f7a
Roberto C. Sánchez <roberto@connexer.com>
2023-03-08 15:12:31 -0500

    CXX-2577 compile and test with c++20

--- .evergreen/build_example_projects.sh
+++ .evergreen/build_example_projects.sh
@@ -14,18 +14,18 @@ for project in bsoncxx mongocxx; do
 (
     cd $project
 
-    if ! ( cd cmake/$DIR && ./build.sh >|output.txt 2>&1); then
+    if ! ( cd cmake/$DIR && ./build.sh "$@" >|output.txt 2>&1); then
         cat cmake/$DIR/output.txt 1>&2
         exit 1
     fi
 
-    if ! ( cd cmake-deprecated/$DIR && ./build.sh >|output.txt 2>&1); then
+    if ! ( cd cmake-deprecated/$DIR && ./build.sh "$@" >|output.txt 2>&1); then
         cat cmake-deprecated/$DIR/output.txt 1>&2
         exit 1
     fi
 
     if [[ ! ( "$OSTYPE" =~ cygwin ) ]]; then
-        if ! ( cd pkg-config/$DIR && ./build.sh >|output.txt 2>&1); then
+        if ! ( cd pkg-config/$DIR && ./build.sh "$@" >|output.txt 2>&1); then
             cat pkg-config/$DIR/output.txt 1>&2
             exit 1
         fi
