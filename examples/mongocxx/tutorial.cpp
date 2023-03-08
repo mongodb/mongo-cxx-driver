d9265d52d995fb471e63e81a09ffedb9a6bdcb3b
Kevin Albertson <kevin.albertson@mongodb.com>
2023-03-08 16:08:06 -0500

    replace stream builder with basic builder
    Use of the stream builder is documented as discouraged.
    The basic builder syntax is more succinct.

--- examples/mongocxx/tutorial.cpp
+++ examples/mongocxx/tutorial.cpp
@@ -18,21 +18,16 @@
 #include <iostream>
 #include <vector>
 
-#include <bsoncxx/builder/stream/array.hpp>
-#include <bsoncxx/builder/stream/document.hpp>
-#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/basic/document.hpp>
 #include <bsoncxx/json.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
 #include <mongocxx/stdx.hpp>
 #include <mongocxx/uri.hpp>
 
-using bsoncxx::builder::stream::close_array;
-using bsoncxx::builder::stream::close_document;
-using bsoncxx::builder::stream::document;
-using bsoncxx::builder::stream::finalize;
-using bsoncxx::builder::stream::open_array;
-using bsoncxx::builder::stream::open_document;
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
 
 int main() {
     mongocxx::instance instance{};  // This should be done only once.
@@ -44,17 +39,11 @@ int main() {
 
     // Create a Document
     {
-        auto builder = bsoncxx::builder::stream::document{};
-        auto doc_value =
-            builder << "name"
-                    << "MongoDB"
-                    << "type"
-                    << "database"
-                    << "count" << 1 << "versions" << bsoncxx::builder::stream::open_array << "v3.2"
-                    << "v3.0"
-                    << "v2.6" << close_array << "info" << bsoncxx::builder::stream::open_document
-                    << "x" << 203 << "y" << 102 << bsoncxx::builder::stream::close_document
-                    << bsoncxx::builder::stream::finalize;
+        auto doc_value = make_document(kvp("name", "MongoDB"),
+                                       kvp("type", "database"),
+                                       kvp("count", 1),
+                                       kvp("versions", make_array("v3.2", "v3.0", "v2.6")),
+                                       kvp("info", make_document(kvp("x", 203), kvp("y", 102))));
 
         auto view = doc_value.view();
 
@@ -67,8 +56,7 @@ int main() {
 
     // Insert One Document
     {
-        auto insert_one_result = collection.insert_one(document{} << "hello"
-                                                                  << "world" << finalize);
+        auto insert_one_result = collection.insert_one(make_document(kvp("hello", "world")));
         // Acknowledged writes return a result.
         assert(insert_one_result);
         auto doc_id = insert_one_result->inserted_id();
@@ -78,7 +66,7 @@ int main() {
     {
         std::vector<bsoncxx::document::value> documents;
         for (int i = 0; i < 100; i++) {
-            documents.push_back(bsoncxx::builder::stream::document{} << "i" << i << finalize);
+            documents.push_back(make_document(kvp("i", i)));
         }
 
         auto insert_many_result = collection.insert_many(documents);
@@ -105,7 +93,7 @@ int main() {
 
     // Get A Single Document That Matches a Filter
     {
-        auto find_one_filtered_result = collection.find_one(document{} << "i" << 71 << finalize);
+        auto find_one_filtered_result = collection.find_one(make_document(kvp("i", 71)));
         if (find_one_filtered_result) {
             std::cout << bsoncxx::to_json(*find_one_filtered_result) << "\n";
         }
@@ -113,9 +101,8 @@ int main() {
 
     // Get All Documents That Match a Filter
     {
-        auto cursor_filtered =
-            collection.find(document{} << "i" << open_document << "$gt" << 50 << "$lte" << 100
-                                       << close_document << finalize);
+        auto cursor_filtered = collection.find(
+            make_document(kvp("i", make_document(kvp("$gt", 50), kvp("$lte", 100)))));
         for (auto doc : cursor_filtered) {
             std::cout << bsoncxx::to_json(doc) << "\n";
         }
@@ -123,16 +110,15 @@ int main() {
 
     // Update a Single Document
     {
-        collection.update_one(
-            document{} << "i" << 10 << finalize,
-            document{} << "$set" << open_document << "i" << 110 << close_document << finalize);
+        collection.update_one(make_document(kvp("i", 10)),
+                              make_document(kvp("$set", make_document(kvp("i", 110)))));
     }
 
     // Update Multiple Documents
     {
-        auto update_many_result = collection.update_many(
-            document{} << "i" << open_document << "$lt" << 100 << close_document << finalize,
-            document{} << "$inc" << open_document << "i" << 100 << close_document << finalize);
+        auto update_many_result =
+            collection.update_many(make_document(kvp("i", make_document(kvp("$lt", 100)))),
+                                   make_document(kvp("$inc", make_document(kvp("i", 100)))));
 
         if (update_many_result) {
             std::cout << update_many_result->modified_count() << "\n";
@@ -140,12 +126,12 @@ int main() {
     }
 
     // Delete a Single Document
-    { collection.delete_one(document{} << "i" << 110 << finalize); }
+    { collection.delete_one(make_document(kvp("i", 110))); }
 
     // Delete All Documents That Match a Filter
     {
-        auto delete_many_result = collection.delete_many(
-            document{} << "i" << open_document << "$gte" << 100 << close_document << finalize);
+        auto delete_many_result =
+            collection.delete_many(make_document(kvp("i", make_document(kvp("$gte", 100)))));
 
         if (delete_many_result) {
             std::cout << delete_many_result->deleted_count() << "\n";
@@ -154,7 +140,7 @@ int main() {
 
     // Create Indexes
     {
-        auto index_specification = document{} << "i" << 1 << finalize;
+        auto index_specification = make_document(kvp("i", 1));
         collection.create_index(std::move(index_specification));
     }
 }
