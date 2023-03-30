c0dc56ae9cb5e7ddcd8c2b7adddb500812859602
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-03-30 13:22:28 -0500

    Update src/mongocxx/test/client_side_encryption.cpp
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- src/mongocxx/test/client_side_encryption.cpp
+++ src/mongocxx/test/client_side_encryption.cpp
@@ -2401,7 +2401,7 @@ TEST_CASE("Unique Index on keyAltNames", "[client_side_encryption]") {
     }
 
     if (!test_util::newer_than(uri{}, "4.2")) {
-        std::cerr << "Unique Index on keyAltNames tests require MongoDB server 4.2+." << std::endl;
+        WARN ("Skipping - requires MongoDB server 4.2+");
         return;
     }
 
