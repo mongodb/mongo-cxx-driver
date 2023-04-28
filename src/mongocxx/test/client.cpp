cd041b5363fbe2d869bd05ed2d243c3de683ef4e
Kevin Albertson <kevin.albertson@mongodb.com>
2023-04-28 08:50:01 -0400

    remove check for server version 3.5.8
    C driver 1.21.0 requires server version 3.6 (minWireVersion=6)
    C++ driver 3.7.x requirees C driver 1.22.1 or higher.

--- src/mongocxx/test/client.cpp
+++ src/mongocxx/test/client.cpp
@@ -389,29 +389,24 @@ TEST_CASE("integration tests for client metadata handshake feature") {
 
             std::string server_version = test_util::get_server_version(client);
 
-            // clientMetadata not returned until 3.5.8.
-            if (test_util::compare_versions(server_version, "3.5.8") >= 0) {
-                REQUIRE(op_view["clientMetadata"]);
-                auto metadata = op_view["clientMetadata"].get_document();
-                auto metadata_view = metadata.view();
+            REQUIRE(op_view["clientMetadata"]);
+            auto metadata = op_view["clientMetadata"].get_document();
+            auto metadata_view = metadata.view();
 
-                REQUIRE(metadata_view["application"]);
-                auto application = metadata_view["application"].get_document();
-                REQUIRE(application.view()["name"].get_string().value ==
-                        stdx::string_view(app_name));
+            REQUIRE(metadata_view["application"]);
+            auto application = metadata_view["application"].get_document();
+            REQUIRE(application.view()["name"].get_string().value == stdx::string_view(app_name));
 
-                REQUIRE(metadata_view["driver"]);
-                auto driver = metadata_view["driver"].get_document();
-                auto driver_view = driver.view();
-                REQUIRE(driver_view["name"].get_string().value ==
-                        stdx::string_view{"mongoc / mongocxx"});
-                auto version =
-                    bsoncxx::string::to_string(driver_view["version"].get_string().value);
-                REQUIRE(version.find(MONGOCXX_VERSION_STRING) != std::string::npos);
+            REQUIRE(metadata_view["driver"]);
+            auto driver = metadata_view["driver"].get_document();
+            auto driver_view = driver.view();
+            REQUIRE(driver_view["name"].get_string().value ==
+                    stdx::string_view{"mongoc / mongocxx"});
+            auto version = bsoncxx::string::to_string(driver_view["version"].get_string().value);
+            REQUIRE(version.find(MONGOCXX_VERSION_STRING) != std::string::npos);
 
-                REQUIRE(metadata_view["os"]);
-                REQUIRE(metadata_view["os"].get_document().view()["type"]);
-            }
+            REQUIRE(metadata_view["os"]);
+            REQUIRE(metadata_view["os"].get_document().view()["type"]);
 
             break;
         }
