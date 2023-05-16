e9db5b20bac476a88fdbf496c4719c160610f3a6
Kevin Albertson <kevin.albertson@mongodb.com>
2023-05-16 16:56:26 +0000

    CXX-2642 update C driver and QE tests for QEv2 collection creation (#964)
    * update C driver dependency to commit cf45741cef8a26b77ae8dda557e49af311a6d66d
    
    To include CDRIVER-4563
    
    * sync `fle2v2-*` to specifications commit aa28f787718eb4306ce7ff8e5a87bd46bb0a2c05
    
    * sync `encryptedFields.json` to specifications commit aa28f787718eb4306ce7ff8e5a87bd46bb0a2c05

--- etc/make_release.py
+++ etc/make_release.py
@@ -84,7 +84,7 @@ ISSUE_TYPE_ID = {'Backport': '10300',
               show_default=True,
               help='The remote reference which points to the mongodb/mongo-cxx-driver repo')
 @click.option('--c-driver-build-ref',
-              default='11e31e3e',
+              default='cf45741cef8a26b77ae8dda557e49af311a6d66d',
               show_default=True,
               help='When building the C driver, build at this Git reference')
 @click.option('--with-c-driver',
