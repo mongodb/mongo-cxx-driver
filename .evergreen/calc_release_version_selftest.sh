ea3804cf8ab7dbc391bae2c2b084b29a0c8e6d7d
Roberto C. Sánchez <roberto@connexer.com>
2023-06-10 16:28:47 -0400

    add self-test for calc_release_version.py

---
+++ .evergreen/calc_release_version_selftest.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# calc_release_version_selftest.sh is used to test output of calc_release_version.py.
+# run with:
+# cd etc
+# ./calc_release_version_selftest.sh
+
+set -o errexit
+set -o pipefail
+
+function assert_eq () {
+    a="$1"
+    b="$2"
+    if [[ "$a" != "$b" ]]; then
+        echo "Assertion failed: $a != $b"
+        # Print caller
+        caller
+        exit 1
+    fi
+}
+
+SAVED_REF=$(git rev-parse HEAD)
+
+function cleanup () {
+    rm calc_release_version_test.py
+    git checkout $SAVED_REF --quiet
+}
+
+trap cleanup EXIT
+
+# copy calc_release_version.py to a separate file not tracked by git so it does not change on `git checkout`
+cp calc_release_version.py calc_release_version_test.py
+
+echo "Test a tagged commit ... begin"
+{
+    git checkout r3.7.2 --quiet
+    got=$(python calc_release_version_test.py)
+    assert_eq "$got" "3.7.2"
+    git checkout - --quiet
+}
+echo "Test a tagged commit ... end"
+
+DATE=$(date +%Y%m%d)
+echo "Test an untagged commit ... begin"
+{
+    # 15756bff8640435b8647fe2eccf8d9c1f6d78816 is commit before r3.7.2
+    git checkout 15756bff8640435b8647fe2eccf8d9c1f6d78816 --quiet
+    got=$(python calc_release_version_test.py)
+    assert_eq "$got" "3.7.2-$DATE+git15756bff86"
+    git checkout - --quiet
+}
+echo "Test an untagged commit ... end"
+
+echo "Test next minor version ... begin"
+{
+    CURRENT_SHORTREF=$(git rev-parse --revs-only --short=10 HEAD)
+    got=$(python calc_release_version_test.py --next-minor)
+    # The expected output may need to be updated after a release.
+    assert_eq "$got" "3.8.0-$DATE+git$CURRENT_SHORTREF"
+}
+echo "Test next minor version ... end"
+
+echo "All tests passed"
