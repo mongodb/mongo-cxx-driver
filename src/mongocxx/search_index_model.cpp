27773c4c9e1090b3fdddd2e9074a2474906cd82a
Josh Siegel <josh.siegel@mongodb.com>
2023-07-03 18:35:10 +0000

    search_index_model

---
+++ src/mongocxx/search_index_model.cpp
@@ -0,0 +1,31 @@
+#include <mongocxx/search_index_model.hpp>
+
+#include <mongocxx/config/private/prelude.hh>
+
+namespace mongocxx {
+MONGOCXX_INLINE_NAMESPACE_BEGIN
+
+search_index_model::search_index_model(const bsoncxx::document::view_or_value& document)
+    : search_index_model("default", document) {}
+search_index_model::search_index_model(const std::string name,
+                                       const bsoncxx::document::view_or_value& document)
+    : _name(name), _document(document) {}
+
+search_index_model::search_index_model(search_index_model&&) noexcept = default;
+
+search_index_model& search_index_model::operator=(search_index_model&&) noexcept = default;
+
+search_index_model::search_index_model(const search_index_model&) = default;
+
+search_index_model::~search_index_model() = default;
+
+std::string search_index_model::get_name() const {
+    return _name;
+}
+
+bsoncxx::document::view search_index_model::get_document() const {
+    return _document.view();
+}
+
+MONGOCXX_INLINE_NAMESPACE_END
+}  // namespace mongocxx
---
+++ src/mongocxx/search_index_model.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <string>
+
+#include <bsoncxx/document/value.hpp>
+#include <bsoncxx/document/view_or_value.hpp>
+#include <stdx/optional.hpp>
+
+#include <mongocxx/config/prelude.hpp>
+
+namespace mongocxx {
+MONGOCXX_INLINE_NAMESPACE_BEGIN
+
+///
+/// Class representing an index on a MongoDB server.
+///
+class MONGOCXX_API search_index_model {
+   public:
+    ///
+    /// Initializes a new search_index_model over a mongocxx::collection.
+    ///
+    search_index_model(const bsoncxx::document::view_or_value& document);
+    search_index_model(const std::string name, const bsoncxx::document::view_or_value& document);
+
+    search_index_model() = delete;
+
+    ///
+    /// Move constructs a search_index_model.
+    ///
+    search_index_model(search_index_model&&) noexcept;
+
+    ///
+    /// Move assigns a search_index_model.
+    ///
+    search_index_model& operator=(search_index_model&&) noexcept;
+
+    ///
+    /// Copy constructs a search_index_model.
+    ///
+    search_index_model(const search_index_model&);
+
+    search_index_model& operator=(const search_index_model&) = delete;
+
+    ///
+    /// Destroys a search_index_model.
+    ///
+    ~search_index_model();
+
+    ///
+    /// Retrieves name of a search_index_model.
+    ///
+    std::string get_name() const;
+
+    ///
+    /// Retrieves document of a search_index_model.
+    ///
+    bsoncxx::document::view get_document() const;
+
+   private:
+    std::string _name;
+    bsoncxx::document::value _document;
+};
+
+MONGOCXX_INLINE_NAMESPACE_END
+}  // namespace mongocxx
+
+#include <mongocxx/config/postlude.hpp>
