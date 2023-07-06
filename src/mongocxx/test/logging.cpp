956a878ad543b38028db2488018d299c2aa33e00
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-07-06 09:35:00 -0500

    is this getting reset?

--- src/mongocxx/test/logging.cpp
+++ src/mongocxx/test/logging.cpp
@@ -50,6 +50,7 @@ class test_log_handler : public logger {
 class reset_log_handler_when_done {
    public:
     ~reset_log_handler_when_done() {
+        fprintf(stderr, "\n\nRESETTING LOG HANDLER\n\n");
         libmongoc::log_set_handler(::mongoc_log_default_handler, nullptr);
     }
 };
@@ -64,7 +65,7 @@ TEST_CASE("a user-provided log handler will be used for logging output", "[insta
 
     // The libmongoc namespace mocking system doesn't play well with varargs
     // functions, so we use a bare mongoc_log call here.
-    fprintf(stderr, "\n\n***NOW LOGGING ERROR: %d***\n\n", ::MONGOC_LOG_LEVEL_ERROR);
+    fprintf(stderr, "\n\nNOW LOGGING ERRO: %d\n\n", ::MONGOC_LOG_LEVEL_ERROR);
     mongoc_log(::MONGOC_LOG_LEVEL_ERROR, "foo", "bar");
 
     REQUIRE(events.size() == 1);
