b3d2fd14a0729f2d2c1d7f4ea623baa57f1282e3
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2023-07-06 13:41:52 -0500

    try not resetting the log handler

--- src/mongocxx/test/logging.cpp
+++ src/mongocxx/test/logging.cpp
@@ -47,16 +47,16 @@ class test_log_handler : public logger {
     std::vector<event>* _events;
 };
 
-class reset_log_handler_when_done {
-   public:
-    ~reset_log_handler_when_done() {
-        fprintf(stderr, "\n\nRESETTING LOG HANDLER\n\n");
-        libmongoc::log_set_handler(::mongoc_log_default_handler, nullptr);
-    }
-};
+// class reset_log_handler_when_done {
+//    public:
+//     ~reset_log_handler_when_done() {
+//         fprintf(stderr, "\n\nRESETTING LOG HANDLER\n\n");
+//         libmongoc::log_set_handler(::mongoc_log_default_handler, nullptr);
+//     }
+// };
 
 TEST_CASE("a user-provided log handler will be used for logging output", "[instance]") {
-    reset_log_handler_when_done rlhwd;
+    // reset_log_handler_when_done rlhwd;
 
     std::vector<test_log_handler::event> events;
     mongocxx::instance driver{stdx::make_unique<test_log_handler>(&events)};
