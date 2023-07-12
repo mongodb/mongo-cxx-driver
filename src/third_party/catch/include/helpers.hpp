5fe46305c25540a3bfbc295c5802b630fb341601
Josh Siegel <josh.siegel@mongodb.com>
2023-07-12 21:33:20 +0000

    revert more formatting

--- src/third_party/catch/include/helpers.hpp
+++ src/third_party/catch/include/helpers.hpp
@@ -92,12 +92,11 @@ MONGOCXX_INLINE_NAMESPACE_END
     client_set_concern->interpose([](mongoc_client_t*, const mongoc_write_concern_t*) {})       \
         .forever();                                                                             \
     auto client_reset = libmongoc::client_reset.create_instance();                              \
-    client_reset->interpose([](const mongoc_client_t*) {}).forever();                           \
+    client_reset->interpose([](const mongoc_client_t*){}).forever();                            \
     auto client_get_concern = libmongoc::client_get_write_concern.create_instance();            \
     client_get_concern->interpose([](const mongoc_client_t*) { return nullptr; }).forever();    \
     auto client_start_session = libmongoc::client_start_session.create_instance();              \
-    auto client_find_databases_with_opts =                                                      \
-        mongocxx::libmongoc::client_find_databases_with_opts.create_instance();
+    auto client_find_databases_with_opts = mongocxx::libmongoc::client_find_databases_with_opts.create_instance();
 
 #if defined(MONGOCXX_ENABLE_SSL) && defined(MONGOC_ENABLE_SSL)
 #define MOCK_CLIENT                                                              \
