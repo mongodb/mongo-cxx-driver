d7454fbd2f0b803512366a51d469d9227e4c50dd
Ezra Chung <ezra.chung@mongodb.com>
2023-08-01 11:23:35 -0500

    Move legacy transaction spec tests into subdirectory

--- src/mongocxx/test/spec/transactions.cpp
+++ src/mongocxx/test/spec/transactions.cpp
@@ -27,8 +27,9 @@ TEST_CASE("Transactions spec automated tests", "[transactions_spec]") {
     /* Tests that use operations that the C++ driver does not have. */
     std::set<std::string> unsupported_transaction_tests = {"count.json"};
 
-    run_tests_in_suite(
-        "TRANSACTIONS_TESTS_PATH", &run_transactions_tests_in_file, unsupported_transaction_tests);
+    run_tests_in_suite("TRANSACTIONS_LEGACY_TESTS_PATH",
+                       &run_transactions_tests_in_file,
+                       unsupported_transaction_tests);
 
     run_tests_in_suite("WITH_TRANSACTION_TESTS_PATH", &run_transactions_tests_in_file);
 }
