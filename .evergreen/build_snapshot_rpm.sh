18b7436071ca4e6f368ef04a173bbb8e9abb4d3a
Roberto C. Sánchez <roberto@connexer.com>
2023-08-17 17:01:02 -0400

    use Fedora 38 for RPM builds

--- .evergreen/build_snapshot_rpm.sh
+++ .evergreen/build_snapshot_rpm.sh
@@ -39,7 +39,7 @@ done
 
 package=mongo-cxx-driver
 spec_file=../mongo-cxx-driver.spec
-config=${MOCK_TARGET_CONFIG:=fedora-39-aarch64}
+config=${MOCK_TARGET_CONFIG:=fedora-38-aarch64}
 
 if [ ! -x /usr/bin/rpmbuild -o ! -x /usr/bin/rpmspec ]; then
   echo "Missing the rpmbuild or rpmspec utility from the rpm-build package"
