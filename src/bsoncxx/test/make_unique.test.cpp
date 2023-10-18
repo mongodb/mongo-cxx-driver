165a531ab337bbe75728a8b10b3d23b58c824cda
vector-of-bool <vectorofbool@gmail.com>
2023-10-18 14:34:22 -0600

    No checked_if

--- src/bsoncxx/test/make_unique.test.cpp
+++ src/bsoncxx/test/make_unique.test.cpp
@@ -14,26 +14,23 @@ struct something {
 
 TEST_CASE("Create a unique_ptr") {
     auto ptr = bsoncxx::stdx::make_unique<int>(12);
-    CHECKED_IF(ptr) {
-        CHECK(*ptr == 12);
-    }
+    REQUIRE(ptr);
+    CHECK(*ptr == 12);
 
     auto thing = bsoncxx::stdx::make_unique<something>(5);
-    CHECKED_IF(thing) {
-        CHECK(thing->value == 5);
-    }
+    REQUIRE(thing);
+    CHECK(thing->value == 5);
 }
 
 TEST_CASE("Create a unique_ptr<T[]>") {
     const unsigned length = 12;
     auto ptr = bsoncxx::stdx::make_unique<int[]>(length);
-    CHECKED_IF(ptr) {
-        // All elements are direct-initialized, which produces '0' for `int`
-        CHECK(ptr[0] == 0);
-        auto res = std::equal_range(ptr.get(), ptr.get() + length, 0);
-        CHECK(res.first == ptr.get());
-        CHECK(res.second == (ptr.get() + length));
-    }
+    REQUIRE(ptr);
+    // All elements are direct-initialized, which produces '0' for `int`
+    CHECK(ptr[0] == 0);
+    auto res = std::equal_range(ptr.get(), ptr.get() + length, 0);
+    CHECK(res.first == ptr.get());
+    CHECK(res.second == (ptr.get() + length));
 
     ptr = bsoncxx::stdx::make_unique_for_overwrite<int[]>(length);
     std::fill_n(ptr.get(), length, 42);
