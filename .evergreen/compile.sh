ce5e14556505afd78cbb65ca252afdc46fcfc614
Ezra Chung <ezra.chung@mongodb.com>
2023-10-30 11:00:32 -0500

    Move compile commands into .evergreen/compile.sh

--- .evergreen/compile.sh
+++ .evergreen/compile.sh
@@ -6,13 +6,47 @@
 # This script should be run from the root of the repository.  This script will run the build from
 # the default build directory './build'.  The following environment variables will change the
 # behavior of this script:
-# - BUILD_TYPE: must be set to "Release" or "Debug"
+# - build_type: must be set to "Release" or "Debug"
 
 set -o errexit
 set -o pipefail
 
-if [ "$BUILD_TYPE" != "Debug" -a "$BUILD_TYPE" != "Release" ]; then
-    echo "$0: expected BUILD_TYPE environment variable to be set to 'Debug' or 'Release'" >&2
+# Add MSBuild.exe to path.
+if [[ "${OSTYPE:?}" == "cygwin" ]]; then
+   case "${generator:-}" in
+   *2015*)
+      PATH="/cygdrive/c/cmake/bin:/cygdrive/c/Program Files (x86)/MSBuild/14.0/Bin:$PATH"
+      ;;
+   *2017*)
+      PATH="/cygdrive/c/cmake/bin:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/15.0/Bin:$PATH"
+      ;;
+   esac
+fi
+export PATH
+
+mongoc_prefix="$(pwd)/../mongoc"
+echo "mongoc_prefix=${mongoc_prefix:?}"
+
+if [[ "${OSTYPE:?}" =~ cygwin ]]; then
+      mongoc_prefix=$(cygpath -m "${mongoc_prefix:?}")
+fi
+
+. "${mongoc_prefix:?}/.evergreen/scripts/find-cmake-latest.sh"
+export cmake_binary
+cmake_binary="$(find_cmake_latest)"
+command -v "$cmake_binary"
+
+if [ ! -d ../drivers-evergreen-tools ]; then
+   git clone --depth 1 git@github.com:mongodb-labs/drivers-evergreen-tools.git ../drivers-evergreen-tools
+fi
+. ../drivers-evergreen-tools/.evergreen/find-python3.sh
+. ../drivers-evergreen-tools/.evergreen/venv-utils.sh
+
+venvcreate "$(find_python3)" venv
+python -m pip install GitPython
+
+if [ "$build_type" != "Debug" -a "$build_type" != "Release" ]; then
+    echo "$0: expected build_type environment variable to be set to 'Debug' or 'Release'" >&2
     exit 1
 fi
 
@@ -50,13 +84,12 @@ esac
 cd build
 
 cmake_flags=(
-   -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
-   -DCMAKE_PREFIX_PATH="${MONGOC_PREFIX:?}"
+   -DCMAKE_BUILD_TYPE=${build_type}
+   -DCMAKE_PREFIX_PATH="${mongoc_prefix:?}"
    -DBUILD_TESTING=ON
    -DMONGOCXX_ENABLE_SLOW_TESTS=ON
    -DCMAKE_INSTALL_PREFIX=install
    -DENABLE_UNINSTALL=ON
-   ${ADDL_OPTS:-}
 )
 
 case "${OSTYPE:?}" in
@@ -146,6 +179,23 @@ if [[ "${#cxx_flags[@]}" > 0 ]]; then
    cmake_flags+=("-DCMAKE_CXX_FLAGS=${cxx_flags[*]}")
 fi
 
+if [[ "${ENABLE_CODE_COVERAGE:-}" == "ON" ]]; then
+   cmake_flags+=("-DENABLE_CODE_COVERAGE=ON")
+fi
+
+if [ "${USE_STATIC_LIBS:-}" ]; then
+   cmake_flags+=("-DBUILD_SHARED_LIBS=OFF")
+fi
+
+if [ "${ENABLE_TESTS:-}" = "OFF" ]; then
+   cmake_flags+=("-DENABLE_TESTS=OFF")
+fi
+
+if [[ -n "${REQUIRED_CXX_STANDARD:-}" ]]; then
+   cmake_flags+=("-DCMAKE_CXX_STANDARD=${REQUIRED_CXX_STANDARD:?}")
+   cmake_flags+=("-DCMAKE_CXX_STANDARD_REQUIRED=ON")
+fi
+
 echo "Configuring with CMake flags: ${cmake_flags[*]}"
 
 "${cmake_binary}" "${cmake_flags[@]}" ..
@@ -153,15 +203,15 @@ echo "Configuring with CMake flags: ${cmake_flags[*]}"
 if [[ "${COMPILE_MACRO_GUARD_TESTS:-"OFF"}" == "ON" ]]; then
    # We only need to compile the macro guard tests.
    "${cmake_binary}" -DENABLE_MACRO_GUARD_TESTS=ON ..
-   "${cmake_binary}" --build . --config $BUILD_TYPE --target test_bsoncxx_macro_guards test_mongocxx_macro_guards -- $CMAKE_BUILD_OPTS
+   "${cmake_binary}" --build . --config $build_type --target test_bsoncxx_macro_guards test_mongocxx_macro_guards -- $CMAKE_BUILD_OPTS
    exit # Nothing else to be done.
 fi
 
 # Regular build and install routine.
-"${cmake_binary}" --build . --config $BUILD_TYPE -- $CMAKE_BUILD_OPTS
-"${cmake_binary}" --build . --config $BUILD_TYPE --target install -- $CMAKE_BUILD_OPTS
-"${cmake_binary}" --build . --config $BUILD_TYPE --target $CMAKE_EXAMPLES_TARGET -- $CMAKE_BUILD_OPTS
+"${cmake_binary}" --build . --config $build_type -- $CMAKE_BUILD_OPTS
+"${cmake_binary}" --build . --config $build_type --target install -- $CMAKE_BUILD_OPTS
+"${cmake_binary}" --build . --config $build_type --target $CMAKE_EXAMPLES_TARGET -- $CMAKE_BUILD_OPTS
 
 if [ "$_RUN_DISTCHECK" ]; then
-  "${cmake_binary}" --build . --config $BUILD_TYPE --target distcheck
+  "${cmake_binary}" --build . --config $build_type --target distcheck
 fi
