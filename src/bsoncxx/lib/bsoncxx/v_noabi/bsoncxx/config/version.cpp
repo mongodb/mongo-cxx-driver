abb128f1efea7e265809d47fec1c470ce9e659a7
Ezra Chung <ezra.chung@mongodb.com>
2023-11-02 16:03:34 -0500

    Migrate version.hpp from v_noabi to v1

---
+++ src/bsoncxx/lib/bsoncxx/v_noabi/bsoncxx/config/version.cpp
@@ -0,0 +1,31 @@
+// Copyright 2023 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <bsoncxx/config/version.hpp>
+
+#if !defined(BSONCXX_VERSION_MAJOR)
+#error "BSONCXX_VERSION_MAJOR is not defined!"
+#endif
+
+#if !defined(BSONCXX_VERSION_MINOR)
+#error "BSONCXX_VERSION_MINOR is not defined!"
+#endif
+
+#if !defined(BSONCXX_VERSION_PATCH)
+#error "BSONCXX_VERSION_PATCH is not defined!"
+#endif
+
+#if !defined(BSONCXX_VERSION_EXTRA)
+#error "BSONCXX_VERSION_EXTRA is not defined!"
+#endif
