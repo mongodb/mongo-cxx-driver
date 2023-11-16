fe9fa6278f3e2a92209469ecb3773bebc2f20c61
Ezra Chung <ezra.chung@mongodb.com>
2023-11-16 13:16:41 -0600

    Avoid redeclarations of mongocxx::search_index_view

--- src/mongocxx/lib/mongocxx/v_noabi/mongocxx/collection.cpp
+++ src/mongocxx/lib/mongocxx/v_noabi/mongocxx/collection.cpp
@@ -1382,7 +1382,7 @@ index_view collection::indexes() {
     return index_view{_get_impl().collection_t, _get_impl().client_impl->client_t};
 }
 
-class search_index_view collection::search_indexes() {
+search_index_view collection::search_indexes() {
     return search_index_view{_get_impl().collection_t, _get_impl().client_impl->client_t};
 }
 
