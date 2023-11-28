9a26c76d3f348c9e331e9bafbc79572a7b9d6709
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2023-11-28 12:59:39 -0600

    Update comment to more accurately reflect sharded cluster behavior (#1064)

--- src/mongocxx/test/database.cpp
+++ src/mongocxx/test/database.cpp
@@ -344,8 +344,8 @@ TEST_CASE("Database integration tests", "[database]") {
                 return;
             }
 
-            // SERVER-79306: $listLocalSessions does not behave as expect if the database does not
-            // already exist on sharded clusters.
+            // SERVER-79306: Ensure the database exists for consistent behavior with sharded
+            // clusters.
             database.create_collection("dummy");
 
             auto session1 = mongo_client.start_session();
