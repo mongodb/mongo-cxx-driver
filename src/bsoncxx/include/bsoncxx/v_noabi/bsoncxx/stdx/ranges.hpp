bff45a14704f70c786edd1e895cfc445ef33c4e6
vector-of-bool <vectorofbool@gmail.com>
2023-12-08 14:58:59 -0700

    Add/update copyright banners

--- src/bsoncxx/include/bsoncxx/v_noabi/bsoncxx/stdx/ranges.hpp
+++ src/bsoncxx/include/bsoncxx/v_noabi/bsoncxx/stdx/ranges.hpp
@@ -1,10 +1,16 @@
-/**
- * @file ranges.hpp
- * @brief A backport of a small amount of std::ranges from C++20
- * @date 2023-11-14
- *
- * @copyright Copyright (c) 2023
- */
+// Copyright 2023 MongoDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #pragma once
 
 #include <cstddef>
