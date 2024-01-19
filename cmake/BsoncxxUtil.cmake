0a4e5e47c0cbee74604c7ac1f4d6b4723f551239
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2024-01-19 12:48:42 -0600

    CXX-2803 Add ABI tag to library filenames on Windows (#1081)
    * Add ABI tag to library filenames on Windows
    
    * Update API/ABI documentation with Windows library filename description

--- cmake/BsoncxxUtil.cmake
+++ cmake/BsoncxxUtil.cmake
@@ -10,17 +10,133 @@ function(bsoncxx_add_library TARGET OUTPUT_NAME LINK_TYPE)
         ${bsoncxx_sources}
     )
 
+    # Full ABI tag string to append to library output name.
+    # The value is determined at generator-time when using a multi-config generator.
+    # Otherwise, the value is determined at configure-time.
+    set(abi_tag "")
+
+    # ABI tag and properties.
+    if(1)
+        # Enforce ABI compatibility in dependent targets.
+        set_property(TARGET ${TARGET} APPEND PROPERTY COMPATIBLE_INTERFACE_STRING
+            BSONCXX_ABI_TAG_MONGOC_LINK_TYPE
+            BSONCXX_ABI_TAG_POLYFILL_LIBRARY
+        )
+
+        # ABI version number. Only necessary for shared library targets.
+        if(LINK_TYPE STREQUAL "SHARED")
+            set(soversion _noabi)
+            set_target_properties(${TARGET} PROPERTIES SOVERSION ${soversion})
+            string(APPEND abi_tag "-v${soversion}")
+        endif()
+
+        # Build type:
+        # - 'd' for debug.
+        # - 'r' for release (including RelWithDebInfo and MinSizeRel).
+        # - 'u' for unknown (e.g. to allow user-defined configurations).
+        # Compatibility is handled via CMake's IMPORTED_CONFIGURATIONS rather than interface properties.
+        string(APPEND abi_tag "-$<IF:$<CONFIG:Debug>,d,$<IF:$<OR:$<CONFIG:Release>,$<CONFIG:RelWithDebInfo>,$<CONFIG:MinSizeRel>>,r,u>>")
+
+        # Link type with libmongoc.
+        # - 'h' for shared.
+        # - 't' for static.
+        if(1)
+            if(BSONCXX_LINK_WITH_STATIC_MONGOC)
+                set(mongoc_link_type "t")
+            else()
+                set(mongoc_link_type "h")
+            endif()
+
+            set_target_properties(${TARGET} PROPERTIES INTERFACE_BSONCXX_ABI_TAG_MONGOC_LINK_TYPE ${mongoc_link_type})
+
+            string(APPEND abi_tag "${mongoc_link_type}")
+        endif()
+
+        # Library used for C++17 polyfills:
+        # - 'm' for mnmlstc/core.
+        # - 'b' for Boost.
+        # - 'x' for experimental standard library.
+        # - 'i' for bsoncxx implementations.
+        # - 's' for standard library (no polyfill).
+        if(1)
+            if(BSONCXX_POLY_USE_MNMLSTC)
+                set(polyfill "m")
+            elseif(BSONCXX_POLY_USE_BOOST)
+                set(polyfill "b")
+            elseif(BSONCXX_POLY_USE_STD_EXPERIMENTAL)
+                set(polyfill "x")
+            elseif(0) # CXX-2796: reserved for bsoncxx implementations as polyfill.
+                set(polyfill "i")
+            elseif(BSONCXX_POLY_USE_STD)
+                set(polyfill "s")
+            else()
+                message(FATAL_ERROR "could not determine polyfill library: must be one of [mbxis]")
+            endif()
+
+            set_target_properties(${TARGET} PROPERTIES INTERFACE_BSONCXX_ABI_TAG_POLYFILL_LIBRARY ${polyfill})
+
+            string(APPEND abi_tag "${polyfill}")
+        endif()
+
+        # MSVC-specific ABI tag suffixes.
+        if(MSVC)
+            set(vs_suffix "")
+
+            # Include the target architecture if applicable (Win32, x64, etc.).
+            if(CMAKE_VS_PLATFORM_NAME)
+                string(APPEND vs_suffix "-$<LOWER_CASE:${CMAKE_VS_PLATFORM_NAME}>")
+            endif()
+
+            # Include the platform toolset version if applicable (v140, v142, etc.).
+            if(CMAKE_VS_PLATFORM_TOOLSET)
+                string(APPEND vs_suffix "-$<LOWER_CASE:${CMAKE_VS_PLATFORM_TOOLSET}>")
+            endif()
+
+            # Include the C runtime if applicable.
+            if(1)
+                get_target_property(runtime ${TARGET} MSVC_RUNTIME_LIBRARY)
+
+                if(runtime)
+                    if(runtime STREQUAL "MultiThreaded")
+                        set(runtime_str "mt")
+                    elseif(runtime STREQUAL "MultiThreadedDebug")
+                        set(runtime_str "mtd")
+                    elseif(runtime STREQUAL "MultiThreadedDLL")
+                        set(runtime_str "md")
+                    elseif(runtime STREQUAL "MultiThreadedDebugDLL")
+                        set(runtime_str "mdd")
+                    endif()
+                else()
+                    # Per CMake documentation: if MSVC_RUNTIME_LIBRARY is not set, then
+                    # CMake uses the default value MultiThreaded$<$<CONFIG:Debug>:Debug>DLL
+                    # to select a MSVC runtime library.
+                    set(runtime_str "m$<$<CONFIG:Debug>:d>d")
+                endif()
+
+                string(APPEND vs_suffix "-${runtime_str}")
+            endif()
+
+            set_target_properties(${TARGET} PROPERTIES BSONCXX_ABI_TAG_VS_SUFFIX ${vs_suffix})
+
+            string(APPEND abi_tag "${vs_suffix}")
+        endif()
+    endif()
+
     set_target_properties(${TARGET} PROPERTIES
-        OUTPUT_NAME ${OUTPUT_NAME}
         VERSION ${BSONCXX_VERSION}
         DEFINE_SYMBOL BSONCXX_EXPORT
     )
+
+    if(ENABLE_ABI_TAG_IN_LIBRARY_FILENAMES)
+        set_target_properties(${TARGET} PROPERTIES OUTPUT_NAME ${OUTPUT_NAME}${abi_tag})
+    else()
+        set_target_properties(${TARGET} PROPERTIES OUTPUT_NAME ${OUTPUT_NAME})
+    endif()
 
     if(LINK_TYPE STREQUAL "SHARED")
         set_target_properties(${TARGET} PROPERTIES
             CXX_VISIBILITY_PRESET hidden
             VISIBILITY_INLINES_HIDDEN ON
-            SOVERSION _noabi
         )
     endif()
 
