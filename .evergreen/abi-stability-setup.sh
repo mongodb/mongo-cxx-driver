bba7f4f1174d3c2bf9ef2da0de71c4175ebb29b7
Ezra Chung <ezra.chung@mongodb.com>
2024-01-19 10:49:20 -0600

    Retry fix of patch vs. non-patch build handling of staged changes

--- .evergreen/abi-stability-setup.sh
+++ .evergreen/abi-stability-setup.sh
@@ -5,8 +5,6 @@ set -o pipefail
 
 : "${cxx_standard:?}" # Set by abi-stability-checks-* build variant definition.
 
-: "${is_patch:-}" # Set by EVG for patch builds.
-
 command -V git >/dev/null
 
 # Files prepared by EVG config.
@@ -73,10 +71,7 @@ CFLAGS="-g -Og"
 CXXFLAGS="-g -Og"
 
 # Build and install the base commit first.
-if [[ "${is_patch:-}" == "true" ]]; then
-  # Patch builds treat diffs relative to base commit as staged changes.
-  git -C mongo-cxx-driver stash push -u
-fi
+git -C mongo-cxx-driver stash push -u
 git -C mongo-cxx-driver reset --hard "${base:?}"
 
 # Install old (base) to install/old.
@@ -99,9 +94,7 @@ echo "Building old libraries... done."
 
 # Restore all pending changes.
 git -C mongo-cxx-driver reset --hard "HEAD@{1}"
-if [[ "${is_patch:-}" == "true" ]]; then
-  git -C mongo-cxx-driver stash pop
-fi
+git -C mongo-cxx-driver stash pop -q || true # Only patch builds have stashed changes.
 
 # Install new (current) to install/new.
 echo "Building new libraries..."
