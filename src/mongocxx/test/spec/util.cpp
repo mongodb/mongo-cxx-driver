c383b7e5ef75136201c42c9cd814f8dbe726e48b
Kevin Albertson <kevin.albertson@mongodb.com>
2024-01-22 13:05:11 -0500

    do not check output of `serverStatus` command
    `mongohoused` responds with `{ "ok" : 1, "host" : "localhost" }`.

--- src/mongocxx/test/spec/util.cpp
+++ src/mongocxx/test/spec/util.cpp
@@ -116,6 +116,13 @@ bool should_skip_spec_test(const client& client, document::view test) {
         return true;
     }
 
+    auto run_mongohouse_tests = std::getenv("RUN_MONGOHOUSE_TESTS");
+    if (run_mongohouse_tests && std::string(run_mongohouse_tests) == "ON") {
+        // mongohoused does not return `version` field in response to serverStatus.
+        // Exit early to run the test.
+        return false;
+    }
+
     std::string server_version;
     try {
         server_version = test_util::get_server_version(client);
