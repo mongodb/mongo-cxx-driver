39902f47681c96f1e56bbf00bca42a39066977a1
Kevin Albertson <kevin.albertson@mongodb.com>
2024-01-22 13:05:48 -0500

    remove no-longer-applicable try/catch

--- src/mongocxx/test/spec/util.cpp
+++ src/mongocxx/test/spec/util.cpp
@@ -123,19 +123,7 @@ bool should_skip_spec_test(const client& client, document::view test) {
         return false;
     }
 
-    std::string server_version;
-    try {
-        server_version = test_util::get_server_version(client);
-    } catch (const operation_exception& e) {
-        // Mongohouse does not support serverStatus, so if we get an error from
-        // serverStatus, exit this logic early and run the test.
-        std::string message = e.what();
-        if (message.find("command serverStatus is unsupported") != std::string::npos) {
-            return false;
-        }
-
-        throw e;
-    }
+    std::string server_version = test_util::get_server_version(client);
 
     std::string topology = test_util::get_topology(client);
 
