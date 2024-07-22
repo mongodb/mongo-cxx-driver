735968efaa68cad2d5b90d5db94e0927a1825194
Ezra Chung <ezra.chung@mongodb.com>
2024-07-22 14:47:45 -0500

    Revert 3.1.1 docs to their initial state (f2eecbf7)

--- api/mongocxx-3.1.1/search/classes_0.js
+++ api/mongocxx-3.1.1/search/classes_0.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['aggregate_355',['aggregate',['../classmongocxx_1_1options_1_1aggregate.html',1,'mongocxx::options']]],
-  ['array_356',['array',['../classbsoncxx_1_1builder_1_1basic_1_1array.html',1,'bsoncxx::builder::basic::array'],['../classbsoncxx_1_1builder_1_1stream_1_1array.html',1,'bsoncxx::builder::stream::array']]],
-  ['array_5fcontext_357',['array_context',['../classbsoncxx_1_1builder_1_1stream_1_1array__context.html',1,'bsoncxx::builder::stream']]],
-  ['authentication_5fexception_358',['authentication_exception',['../classmongocxx_1_1authentication__exception.html',1,'mongocxx']]]
+  ['aggregate',['aggregate',['../classmongocxx_1_1options_1_1aggregate.html',1,'mongocxx::options']]],
+  ['array',['array',['../classbsoncxx_1_1builder_1_1basic_1_1array.html',1,'bsoncxx::builder::basic']]],
+  ['array',['array',['../classbsoncxx_1_1builder_1_1stream_1_1array.html',1,'bsoncxx::builder::stream']]],
+  ['array_5fcontext',['array_context',['../classbsoncxx_1_1builder_1_1stream_1_1array__context.html',1,'bsoncxx::builder::stream']]],
+  ['array_5fcontext_3c_3e',['array_context&lt;&gt;',['../classbsoncxx_1_1builder_1_1stream_1_1array__context.html',1,'bsoncxx::builder::stream']]],
+  ['authentication_5fexception',['authentication_exception',['../classmongocxx_1_1authentication__exception.html',1,'mongocxx']]]
 ];
--- api/mongocxx-3.1.1/search/classes_6.js
+++ api/mongocxx-3.1.1/search/classes_6.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['hint_411',['hint',['../classmongocxx_1_1hint.html',1,'mongocxx']]],
-  ['host_412',['host',['../structmongocxx_1_1uri_1_1host.html',1,'mongocxx::uri']]]
+  ['hint',['hint',['../classmongocxx_1_1hint.html',1,'mongocxx']]],
+  ['host',['host',['../structmongocxx_1_1uri_1_1host.html',1,'mongocxx::uri']]]
 ];
