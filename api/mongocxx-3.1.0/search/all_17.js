cc2d178806bcd9a79a4d07b95bab8cc6bb355a8a
Ezra Chung <ezra.chung@mongodb.com>
2024-07-22 14:45:57 -0500

    Revert 3.1.0 docs to their initial state (1d610018)

--- api/mongocxx-3.1.0/search/all_17.js
+++ api/mongocxx-3.1.0/search/all_17.js
@@ -1,17 +1,17 @@
 var searchData=
 [
-  ['_7ebulk_5fwrite_328',['~bulk_write',['../classmongocxx_1_1bulk__write.html#a5e632780beff5e1bef52c0ac1a74eac5',1,'mongocxx::bulk_write']]],
-  ['_7eclient_329',['~client',['../classmongocxx_1_1client.html#a05a8d2e08fc5ef4523c526ddd693da3b',1,'mongocxx::client']]],
-  ['_7ecollection_330',['~collection',['../classmongocxx_1_1collection.html#a00261755d5d330a6a9bdef5f261573e1',1,'mongocxx::collection']]],
-  ['_7ecursor_331',['~cursor',['../classmongocxx_1_1cursor.html#a5005db0d3bb6f7ca5c476912362bed2e',1,'mongocxx::cursor']]],
-  ['_7edatabase_332',['~database',['../classmongocxx_1_1database.html#a97a9e0a0edf617cb878b8ea3a40c8ba1',1,'mongocxx::database']]],
-  ['_7einstance_333',['~instance',['../classmongocxx_1_1instance.html#afd1dc8caf8123c0f6902de801e7b73d6',1,'mongocxx::instance']]],
-  ['_7epipeline_334',['~pipeline',['../classmongocxx_1_1pipeline.html#a2570e47a0502be4872b60428fd91a964',1,'mongocxx::pipeline']]],
-  ['_7epool_335',['~pool',['../classmongocxx_1_1pool.html#a8aeb1d9a619a9f54e7da0651170eedc6',1,'mongocxx::pool']]],
-  ['_7eread_5fconcern_336',['~read_concern',['../classmongocxx_1_1read__concern.html#a01505c768de14bbcc8cb1800d3960c4a',1,'mongocxx::read_concern']]],
-  ['_7eread_5fpreference_337',['~read_preference',['../classmongocxx_1_1read__preference.html#a66bcdfc2b6a945d2d1f4e21900f29ec2',1,'mongocxx::read_preference']]],
-  ['_7euri_338',['~uri',['../classmongocxx_1_1uri.html#a39b1d2afeec7e90f107213317e40a817',1,'mongocxx::uri']]],
-  ['_7evalidator_339',['~validator',['../classbsoncxx_1_1validator.html#a28320624d228eaa5e195734cd317526b',1,'bsoncxx::validator']]],
-  ['_7ewrite_340',['~write',['../classmongocxx_1_1model_1_1write.html#aa14cb36c3cbd527acdc1f61cb209d8c9',1,'mongocxx::model::write']]],
-  ['_7ewrite_5fconcern_341',['~write_concern',['../classmongocxx_1_1write__concern.html#ab81f921c8949cdc8da41af0587c4b69f',1,'mongocxx::write_concern']]]
+  ['_7ebulk_5fwrite',['~bulk_write',['../classmongocxx_1_1bulk__write.html#a5e632780beff5e1bef52c0ac1a74eac5',1,'mongocxx::bulk_write']]],
+  ['_7eclient',['~client',['../classmongocxx_1_1client.html#a05a8d2e08fc5ef4523c526ddd693da3b',1,'mongocxx::client']]],
+  ['_7ecollection',['~collection',['../classmongocxx_1_1collection.html#a00261755d5d330a6a9bdef5f261573e1',1,'mongocxx::collection']]],
+  ['_7ecursor',['~cursor',['../classmongocxx_1_1cursor.html#a5005db0d3bb6f7ca5c476912362bed2e',1,'mongocxx::cursor']]],
+  ['_7edatabase',['~database',['../classmongocxx_1_1database.html#a97a9e0a0edf617cb878b8ea3a40c8ba1',1,'mongocxx::database']]],
+  ['_7einstance',['~instance',['../classmongocxx_1_1instance.html#afd1dc8caf8123c0f6902de801e7b73d6',1,'mongocxx::instance']]],
+  ['_7epipeline',['~pipeline',['../classmongocxx_1_1pipeline.html#a2570e47a0502be4872b60428fd91a964',1,'mongocxx::pipeline']]],
+  ['_7epool',['~pool',['../classmongocxx_1_1pool.html#a8aeb1d9a619a9f54e7da0651170eedc6',1,'mongocxx::pool']]],
+  ['_7eread_5fconcern',['~read_concern',['../classmongocxx_1_1read__concern.html#a01505c768de14bbcc8cb1800d3960c4a',1,'mongocxx::read_concern']]],
+  ['_7eread_5fpreference',['~read_preference',['../classmongocxx_1_1read__preference.html#a66bcdfc2b6a945d2d1f4e21900f29ec2',1,'mongocxx::read_preference']]],
+  ['_7euri',['~uri',['../classmongocxx_1_1uri.html#a39b1d2afeec7e90f107213317e40a817',1,'mongocxx::uri']]],
+  ['_7evalidator',['~validator',['../classbsoncxx_1_1validator.html#a28320624d228eaa5e195734cd317526b',1,'bsoncxx::validator']]],
+  ['_7ewrite',['~write',['../classmongocxx_1_1model_1_1write.html#aa14cb36c3cbd527acdc1f61cb209d8c9',1,'mongocxx::model::write']]],
+  ['_7ewrite_5fconcern',['~write_concern',['../classmongocxx_1_1write__concern.html#ab81f921c8949cdc8da41af0587c4b69f',1,'mongocxx::write_concern']]]
 ];
--- api/mongocxx-3.1.0/search/classes_7.js
+++ api/mongocxx-3.1.0/search/classes_7.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['index_400',['index',['../classmongocxx_1_1options_1_1index.html',1,'mongocxx::options']]],
-  ['init_5ftag_5ft_401',['init_tag_t',['../structbsoncxx_1_1oid_1_1init__tag__t.html',1,'bsoncxx::oid']]],
-  ['insert_402',['insert',['../classmongocxx_1_1options_1_1insert.html',1,'mongocxx::options']]],
-  ['insert_5fmany_403',['insert_many',['../classmongocxx_1_1result_1_1insert__many.html',1,'mongocxx::result']]],
-  ['insert_5fmany_5fbuilder_404',['insert_many_builder',['../classmongocxx_1_1insert__many__builder.html',1,'mongocxx']]],
-  ['insert_5fone_405',['insert_one',['../classmongocxx_1_1model_1_1insert__one.html',1,'mongocxx::model::insert_one'],['../classmongocxx_1_1result_1_1insert__one.html',1,'mongocxx::result::insert_one']]],
-  ['instance_406',['instance',['../classmongocxx_1_1instance.html',1,'mongocxx']]],
-  ['is_5ferror_5fcode_5fenum_3c_20bsoncxx_3a_3aerror_5fcode_20_3e_407',['is_error_code_enum&lt; bsoncxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01bsoncxx_1_1error__code_01_4.html',1,'std']]],
-  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aerror_5fcode_20_3e_408',['is_error_code_enum&lt; mongocxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1error__code_01_4.html',1,'std']]],
-  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aserver_5ferror_5fcode_20_3e_409',['is_error_code_enum&lt; mongocxx::server_error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1server__error__code_01_4.html',1,'std']]],
-  ['iterator_410',['iterator',['../classmongocxx_1_1cursor_1_1iterator.html',1,'mongocxx::cursor']]]
+  ['index',['index',['../classmongocxx_1_1options_1_1index.html',1,'mongocxx::options']]],
+  ['init_5ftag_5ft',['init_tag_t',['../structbsoncxx_1_1oid_1_1init__tag__t.html',1,'bsoncxx::oid']]],
+  ['insert',['insert',['../classmongocxx_1_1options_1_1insert.html',1,'mongocxx::options']]],
+  ['insert_5fmany',['insert_many',['../classmongocxx_1_1result_1_1insert__many.html',1,'mongocxx::result']]],
+  ['insert_5fmany_5fbuilder',['insert_many_builder',['../classmongocxx_1_1insert__many__builder.html',1,'mongocxx']]],
+  ['insert_5fone',['insert_one',['../classmongocxx_1_1model_1_1insert__one.html',1,'mongocxx::model']]],
+  ['insert_5fone',['insert_one',['../classmongocxx_1_1result_1_1insert__one.html',1,'mongocxx::result']]],
+  ['instance',['instance',['../classmongocxx_1_1instance.html',1,'mongocxx']]],
+  ['is_5ferror_5fcode_5fenum_3c_20bsoncxx_3a_3aerror_5fcode_20_3e',['is_error_code_enum&lt; bsoncxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01bsoncxx_1_1error__code_01_4.html',1,'std']]],
+  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aerror_5fcode_20_3e',['is_error_code_enum&lt; mongocxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1error__code_01_4.html',1,'std']]],
+  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aserver_5ferror_5fcode_20_3e',['is_error_code_enum&lt; mongocxx::server_error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1server__error__code_01_4.html',1,'std']]],
+  ['iterator',['iterator',['../classmongocxx_1_1cursor_1_1iterator.html',1,'mongocxx::cursor']]]
 ];
