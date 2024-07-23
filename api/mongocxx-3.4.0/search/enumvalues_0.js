1d7f4d29c3944ba22deb9224357ac4a78d7dd0cb
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:02:54 -0500

    Revert 3.4.0 docs to their initial state (620c0f58)

--- api/mongocxx-3.4.0/search/enumvalues_0.js
+++ api/mongocxx-3.4.0/search/enumvalues_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['bsoncxx_5fenum_1106',['BSONCXX_ENUM',['../namespacebsoncxx.html#a5f8d292ea2f1f1a9f9760acff8228773a6ac444ab073011811d9e4c11343e05de',1,'bsoncxx']]]
+  ['bsoncxx_5fenum',['BSONCXX_ENUM',['../namespacebsoncxx.html#a5f8d292ea2f1f1a9f9760acff8228773a6ac444ab073011811d9e4c11343e05de',1,'bsoncxx']]]
 ];
