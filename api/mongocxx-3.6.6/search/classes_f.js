de954f3b276cdb70d20dabfe398691d0c7d07358
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:06:25 -0500

    Revert 3.6.6 docs to their initial state (503dd458)

--- api/mongocxx-3.6.6/search/classes_f.js
+++ api/mongocxx-3.6.6/search/classes_f.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['server_5fchanged_5fevent_705',['server_changed_event',['../classmongocxx_1_1events_1_1server__changed__event.html',1,'mongocxx::events']]],
-  ['server_5fclosed_5fevent_706',['server_closed_event',['../classmongocxx_1_1events_1_1server__closed__event.html',1,'mongocxx::events']]],
-  ['server_5fdescription_707',['server_description',['../classmongocxx_1_1events_1_1server__description.html',1,'mongocxx::events']]],
-  ['server_5fdescriptions_708',['server_descriptions',['../classmongocxx_1_1events_1_1topology__description_1_1server__descriptions.html',1,'mongocxx::events::topology_description']]],
-  ['server_5fopening_5fevent_709',['server_opening_event',['../classmongocxx_1_1events_1_1server__opening__event.html',1,'mongocxx::events']]],
-  ['single_5fcontext_710',['single_context',['../classbsoncxx_1_1builder_1_1stream_1_1single__context.html',1,'bsoncxx::builder::stream']]],
-  ['sub_5farray_711',['sub_array',['../classbsoncxx_1_1builder_1_1basic_1_1sub__array.html',1,'bsoncxx::builder::basic']]],
-  ['sub_5fdocument_712',['sub_document',['../classbsoncxx_1_1builder_1_1basic_1_1sub__document.html',1,'bsoncxx::builder::basic']]]
+  ['server_5fchanged_5fevent_709',['server_changed_event',['../classmongocxx_1_1events_1_1server__changed__event.html',1,'mongocxx::events']]],
+  ['server_5fclosed_5fevent_710',['server_closed_event',['../classmongocxx_1_1events_1_1server__closed__event.html',1,'mongocxx::events']]],
+  ['server_5fdescription_711',['server_description',['../classmongocxx_1_1events_1_1server__description.html',1,'mongocxx::events']]],
+  ['server_5fdescriptions_712',['server_descriptions',['../classmongocxx_1_1events_1_1topology__description_1_1server__descriptions.html',1,'mongocxx::events::topology_description']]],
+  ['server_5fopening_5fevent_713',['server_opening_event',['../classmongocxx_1_1events_1_1server__opening__event.html',1,'mongocxx::events']]],
+  ['single_5fcontext_714',['single_context',['../classbsoncxx_1_1builder_1_1stream_1_1single__context.html',1,'bsoncxx::builder::stream']]],
+  ['sub_5farray_715',['sub_array',['../classbsoncxx_1_1builder_1_1basic_1_1sub__array.html',1,'bsoncxx::builder::basic']]],
+  ['sub_5fdocument_716',['sub_document',['../classbsoncxx_1_1builder_1_1basic_1_1sub__document.html',1,'bsoncxx::builder::basic']]]
 ];
