bae014d450e647cc3c4da07eff54560e3a4a8588
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:03:13 -0500

    Revert 3.4.1 docs to their initial state (2a4846c4)

--- api/mongocxx-3.4.1/search/enums_4.js
+++ api/mongocxx-3.4.1/search/enums_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['server_5ferror_5fcode_1101',['server_error_code',['../namespacemongocxx.html#a0256e82231a92f27a1fd4d3436099eff',1,'mongocxx']]]
+  ['server_5ferror_5fcode',['server_error_code',['../namespacemongocxx.html#a0256e82231a92f27a1fd4d3436099eff',1,'mongocxx']]]
 ];
--- api/mongocxx-3.4.1/search/functions_a.js
+++ api/mongocxx-3.4.1/search/functions_a.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['key_868',['key',['../classbsoncxx_1_1document_1_1element.html#a3f8fc6ce79fbc5aa50cd0a57c2d5f133',1,'bsoncxx::document::element']]],
-  ['key_5fcontext_869',['key_context',['../classbsoncxx_1_1builder_1_1stream_1_1key__context.html#a6131dde005dc09ca6b543e5ed437d39b',1,'bsoncxx::builder::stream::key_context']]],
-  ['key_5fowned_870',['key_owned',['../classbsoncxx_1_1builder_1_1core.html#a151703a20f1bffaf62177d28f4408449',1,'bsoncxx::builder::core']]],
-  ['key_5fview_871',['key_view',['../classbsoncxx_1_1builder_1_1core.html#a9dd62e33cb3e4b911d396669a2c64d59',1,'bsoncxx::builder::core']]],
-  ['keylen_872',['keylen',['../classbsoncxx_1_1document_1_1element.html#a96d5a83f9b4953b5a1c6207eb6a07302',1,'bsoncxx::document::element']]],
-  ['keys_873',['keys',['../classmongocxx_1_1index__model.html#a67f3245b4162cbea66b73c146e133a08',1,'mongocxx::index_model']]]
+  ['key',['key',['../classbsoncxx_1_1document_1_1element.html#a3f8fc6ce79fbc5aa50cd0a57c2d5f133',1,'bsoncxx::document::element']]],
+  ['key_5fcontext',['key_context',['../classbsoncxx_1_1builder_1_1stream_1_1key__context.html#a6131dde005dc09ca6b543e5ed437d39b',1,'bsoncxx::builder::stream::key_context']]],
+  ['key_5fowned',['key_owned',['../classbsoncxx_1_1builder_1_1core.html#a151703a20f1bffaf62177d28f4408449',1,'bsoncxx::builder::core']]],
+  ['key_5fview',['key_view',['../classbsoncxx_1_1builder_1_1core.html#a9dd62e33cb3e4b911d396669a2c64d59',1,'bsoncxx::builder::core']]],
+  ['keylen',['keylen',['../classbsoncxx_1_1document_1_1element.html#a96d5a83f9b4953b5a1c6207eb6a07302',1,'bsoncxx::document::element']]],
+  ['keys',['keys',['../classmongocxx_1_1index__model.html#a67f3245b4162cbea66b73c146e133a08',1,'mongocxx::index_model']]]
 ];
