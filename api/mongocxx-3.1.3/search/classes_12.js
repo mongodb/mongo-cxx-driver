b6c5471588a87910959d5b7ecac5a3fa49be4f23
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 10:55:24 -0500

    Revert 3.1.3 docs to their initial state (aeb7d511)

--- api/mongocxx-3.1.3/search/classes_12.js
+++ api/mongocxx-3.1.3/search/classes_12.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['wiredtiger_5fstorage_5foptions_455',['wiredtiger_storage_options',['../classmongocxx_1_1options_1_1index_1_1wiredtiger__storage__options.html',1,'mongocxx::options::index']]],
-  ['write_456',['write',['../classmongocxx_1_1model_1_1write.html',1,'mongocxx::model']]],
-  ['write_5fconcern_457',['write_concern',['../classmongocxx_1_1write__concern.html',1,'mongocxx']]],
-  ['write_5fexception_458',['write_exception',['../classmongocxx_1_1write__exception.html',1,'mongocxx']]]
+  ['wiredtiger_5fstorage_5foptions',['wiredtiger_storage_options',['../classmongocxx_1_1options_1_1index_1_1wiredtiger__storage__options.html',1,'mongocxx::options::index']]],
+  ['write',['write',['../classmongocxx_1_1model_1_1write.html',1,'mongocxx::model']]],
+  ['write_5fconcern',['write_concern',['../classmongocxx_1_1write__concern.html',1,'mongocxx']]],
+  ['write_5fexception',['write_exception',['../classmongocxx_1_1write__exception.html',1,'mongocxx']]]
 ];
