167838ffa7963b362d7fe560b013a638167114bd
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:05:06 -0500

    Revert 3.6.1 docs to their initial state (19df6fb9)

--- api/mongocxx-3.6.1/search/classes_10.js
+++ api/mongocxx-3.6.1/search/classes_10.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['tls_713',['tls',['../classmongocxx_1_1options_1_1tls.html',1,'mongocxx::options']]],
-  ['topology_5fchanged_5fevent_714',['topology_changed_event',['../classmongocxx_1_1events_1_1topology__changed__event.html',1,'mongocxx::events']]],
-  ['topology_5fclosed_5fevent_715',['topology_closed_event',['../classmongocxx_1_1events_1_1topology__closed__event.html',1,'mongocxx::events']]],
-  ['topology_5fdescription_716',['topology_description',['../classmongocxx_1_1events_1_1topology__description.html',1,'mongocxx::events']]],
-  ['topology_5fopening_5fevent_717',['topology_opening_event',['../classmongocxx_1_1events_1_1topology__opening__event.html',1,'mongocxx::events']]],
-  ['transaction_718',['transaction',['../classmongocxx_1_1options_1_1transaction.html',1,'mongocxx::options']]]
+  ['tls_717',['tls',['../classmongocxx_1_1options_1_1tls.html',1,'mongocxx::options']]],
+  ['topology_5fchanged_5fevent_718',['topology_changed_event',['../classmongocxx_1_1events_1_1topology__changed__event.html',1,'mongocxx::events']]],
+  ['topology_5fclosed_5fevent_719',['topology_closed_event',['../classmongocxx_1_1events_1_1topology__closed__event.html',1,'mongocxx::events']]],
+  ['topology_5fdescription_720',['topology_description',['../classmongocxx_1_1events_1_1topology__description.html',1,'mongocxx::events']]],
+  ['topology_5fopening_5fevent_721',['topology_opening_event',['../classmongocxx_1_1events_1_1topology__opening__event.html',1,'mongocxx::events']]],
+  ['transaction_722',['transaction',['../classmongocxx_1_1options_1_1transaction.html',1,'mongocxx::options']]]
 ];
