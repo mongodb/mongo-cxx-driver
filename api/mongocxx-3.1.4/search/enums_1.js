2e86bc301998ebedf1373b8b524ef0ed9ffc5621
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 10:56:14 -0500

    Revert 3.1.4 docs to their initial state (d9dd96dc)

--- api/mongocxx-3.1.4/search/enums_1.js
+++ api/mongocxx-3.1.4/search/enums_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['error_5fcode_752',['error_code',['../namespacebsoncxx.html#a5f8d292ea2f1f1a9f9760acff8228773',1,'bsoncxx::error_code()'],['../namespacemongocxx.html#a36960f75ff06296d1ecd18b9393174f1',1,'mongocxx::error_code()']]]
+  ['error_5fcode',['error_code',['../namespacebsoncxx.html#a5f8d292ea2f1f1a9f9760acff8228773',1,'bsoncxx::error_code()'],['../namespacemongocxx.html#a36960f75ff06296d1ecd18b9393174f1',1,'mongocxx::error_code()']]]
 ];
