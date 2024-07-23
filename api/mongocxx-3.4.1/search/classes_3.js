13866cacc049c060840fc4accc5f2d5f014afda8
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:03:13 -0500

    Revert 3.4.1 docs to their initial state (2a4846c4)

--- api/mongocxx-3.4.1/search/classes_3.js
+++ api/mongocxx-3.4.1/search/classes_3.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['database_595',['database',['../classmongocxx_1_1database.html',1,'mongocxx']]],
-  ['decimal128_596',['decimal128',['../classbsoncxx_1_1decimal128.html',1,'bsoncxx']]],
-  ['delete_5fmany_597',['delete_many',['../classmongocxx_1_1model_1_1delete__many.html',1,'mongocxx::model']]],
-  ['delete_5fone_598',['delete_one',['../classmongocxx_1_1model_1_1delete__one.html',1,'mongocxx::model']]],
-  ['delete_5foptions_599',['delete_options',['../classmongocxx_1_1options_1_1delete__options.html',1,'mongocxx::options']]],
-  ['delete_5fresult_600',['delete_result',['../classmongocxx_1_1result_1_1delete__result.html',1,'mongocxx::result']]],
-  ['deprecated_5ftag_601',['deprecated_tag',['../structmongocxx_1_1read__preference_1_1deprecated__tag.html',1,'mongocxx::read_preference']]],
-  ['distinct_602',['distinct',['../classmongocxx_1_1options_1_1distinct.html',1,'mongocxx::options']]],
-  ['document_603',['document',['../classbsoncxx_1_1builder_1_1basic_1_1document.html',1,'bsoncxx::builder::basic::document'],['../classbsoncxx_1_1builder_1_1stream_1_1document.html',1,'bsoncxx::builder::stream::document']]],
-  ['downloader_604',['downloader',['../classmongocxx_1_1gridfs_1_1downloader.html',1,'mongocxx::gridfs']]]
+  ['database',['database',['../classmongocxx_1_1database.html',1,'mongocxx']]],
+  ['decimal128',['decimal128',['../classbsoncxx_1_1decimal128.html',1,'bsoncxx']]],
+  ['delete_5fmany',['delete_many',['../classmongocxx_1_1model_1_1delete__many.html',1,'mongocxx::model']]],
+  ['delete_5fone',['delete_one',['../classmongocxx_1_1model_1_1delete__one.html',1,'mongocxx::model']]],
+  ['delete_5foptions',['delete_options',['../classmongocxx_1_1options_1_1delete__options.html',1,'mongocxx::options']]],
+  ['delete_5fresult',['delete_result',['../classmongocxx_1_1result_1_1delete__result.html',1,'mongocxx::result']]],
+  ['deprecated_5ftag',['deprecated_tag',['../structmongocxx_1_1read__preference_1_1deprecated__tag.html',1,'mongocxx::read_preference']]],
+  ['distinct',['distinct',['../classmongocxx_1_1options_1_1distinct.html',1,'mongocxx::options']]],
+  ['document',['document',['../classbsoncxx_1_1builder_1_1stream_1_1document.html',1,'bsoncxx::builder::stream::document'],['../classbsoncxx_1_1builder_1_1basic_1_1document.html',1,'bsoncxx::builder::basic::document']]],
+  ['downloader',['downloader',['../classmongocxx_1_1gridfs_1_1downloader.html',1,'mongocxx::gridfs']]]
 ];
--- api/mongocxx-3.4.1/search/classes_b.js
+++ api/mongocxx-3.4.1/search/classes_b.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['oid_636',['oid',['../classbsoncxx_1_1oid.html',1,'bsoncxx']]],
-  ['open_5farray_5ftype_637',['open_array_type',['../structbsoncxx_1_1builder_1_1stream_1_1open__array__type.html',1,'bsoncxx::builder::stream']]],
-  ['open_5fdocument_5ftype_638',['open_document_type',['../structbsoncxx_1_1builder_1_1stream_1_1open__document__type.html',1,'bsoncxx::builder::stream']]],
-  ['operation_5fexception_639',['operation_exception',['../classmongocxx_1_1operation__exception.html',1,'mongocxx']]]
+  ['oid',['oid',['../classbsoncxx_1_1oid.html',1,'bsoncxx']]],
+  ['open_5farray_5ftype',['open_array_type',['../structbsoncxx_1_1builder_1_1stream_1_1open__array__type.html',1,'bsoncxx::builder::stream']]],
+  ['open_5fdocument_5ftype',['open_document_type',['../structbsoncxx_1_1builder_1_1stream_1_1open__document__type.html',1,'bsoncxx::builder::stream']]],
+  ['operation_5fexception',['operation_exception',['../classmongocxx_1_1operation__exception.html',1,'mongocxx']]]
 ];
