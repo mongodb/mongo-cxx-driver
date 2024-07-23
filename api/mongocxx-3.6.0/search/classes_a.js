df1c1ea692761ce0e366e8594a9668d2a9104c8e
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:04:49 -0500

    Revert 3.6.0 docs to their initial state (5374b9f0)

--- api/mongocxx-3.6.0/search/classes_a.js
+++ api/mongocxx-3.6.0/search/classes_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['logger_692',['logger',['../classmongocxx_1_1logger.html',1,'mongocxx']]],
-  ['logic_5ferror_693',['logic_error',['../classmongocxx_1_1logic__error.html',1,'mongocxx']]]
+  ['logger_696',['logger',['../classmongocxx_1_1logger.html',1,'mongocxx']]],
+  ['logic_5ferror_697',['logic_error',['../classmongocxx_1_1logic__error.html',1,'mongocxx']]]
 ];
