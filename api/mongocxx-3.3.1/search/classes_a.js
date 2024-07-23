4c14d4346afb342ae8c5e4c076e9faa37312ac97
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:01:42 -0500

    Revert 3.3.1 docs to their initial state (1e85a303)

--- api/mongocxx-3.3.1/search/classes_a.js
+++ api/mongocxx-3.3.1/search/classes_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['logger_536',['logger',['../classmongocxx_1_1logger.html',1,'mongocxx']]],
-  ['logic_5ferror_537',['logic_error',['../classmongocxx_1_1logic__error.html',1,'mongocxx']]]
+  ['logger',['logger',['../classmongocxx_1_1logger.html',1,'mongocxx']]],
+  ['logic_5ferror',['logic_error',['../classmongocxx_1_1logic__error.html',1,'mongocxx']]]
 ];
