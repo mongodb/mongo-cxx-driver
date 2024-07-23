89c16c0d8fc2b24fd41185f33a15c4d052d1acb6
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:05:39 -0500

    Revert 3.6.3 docs to their initial state (8b87c318)

--- api/mongocxx-3.6.3/search/functions_16.js
+++ api/mongocxx-3.6.3/search/functions_16.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['zlib_5fcompression_5flevel_1165',['zlib_compression_level',['../classmongocxx_1_1uri.html#a82fd4cd8d19d86135aa28325ec40c187',1,'mongocxx::uri']]]
+  ['zlib_5fcompression_5flevel_1169',['zlib_compression_level',['../classmongocxx_1_1uri.html#a82fd4cd8d19d86135aa28325ec40c187',1,'mongocxx::uri']]]
 ];
