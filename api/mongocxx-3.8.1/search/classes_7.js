122c6e58e7bfa63145298faf91053c70ae8f4507
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:10:37 -0500

    Revert 3.8.1 docs to their initial state (e671c418)

--- api/mongocxx-3.8.1/search/classes_7.js
+++ api/mongocxx-3.8.1/search/classes_7.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['heartbeat_5ffailed_5fevent_724',['heartbeat_failed_event',['../classmongocxx_1_1events_1_1heartbeat__failed__event.html',1,'mongocxx::events']]],
-  ['heartbeat_5fstarted_5fevent_725',['heartbeat_started_event',['../classmongocxx_1_1events_1_1heartbeat__started__event.html',1,'mongocxx::events']]],
-  ['heartbeat_5fsucceeded_5fevent_726',['heartbeat_succeeded_event',['../classmongocxx_1_1events_1_1heartbeat__succeeded__event.html',1,'mongocxx::events']]],
-  ['hint_727',['hint',['../classmongocxx_1_1hint.html',1,'mongocxx']]],
-  ['host_728',['host',['../structmongocxx_1_1uri_1_1host.html',1,'mongocxx::uri']]]
+  ['heartbeat_5ffailed_5fevent_0',['heartbeat_failed_event',['../classmongocxx_1_1events_1_1heartbeat__failed__event.html',1,'mongocxx::events']]],
+  ['heartbeat_5fstarted_5fevent_1',['heartbeat_started_event',['../classmongocxx_1_1events_1_1heartbeat__started__event.html',1,'mongocxx::events']]],
+  ['heartbeat_5fsucceeded_5fevent_2',['heartbeat_succeeded_event',['../classmongocxx_1_1events_1_1heartbeat__succeeded__event.html',1,'mongocxx::events']]],
+  ['hint_3',['hint',['../classmongocxx_1_1hint.html',1,'mongocxx']]],
+  ['host_4',['host',['../structmongocxx_1_1uri_1_1host.html',1,'mongocxx::uri']]]
 ];
