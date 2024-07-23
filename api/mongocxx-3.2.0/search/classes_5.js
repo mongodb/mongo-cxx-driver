c67253017e50237c055da86f9db40401c99be373
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 10:56:45 -0500

    Revert 3.2.0 docs to their initial state (d9dd96dc)

--- api/mongocxx-3.2.0/search/classes_5.js
+++ api/mongocxx-3.2.0/search/classes_5.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['finalize_5ftype_494',['finalize_type',['../structbsoncxx_1_1builder_1_1stream_1_1finalize__type.html',1,'bsoncxx::builder::stream']]],
-  ['find_495',['find',['../classmongocxx_1_1options_1_1find.html',1,'mongocxx::options']]],
-  ['find_5fone_5fand_5fdelete_496',['find_one_and_delete',['../classmongocxx_1_1options_1_1find__one__and__delete.html',1,'mongocxx::options']]],
-  ['find_5fone_5fand_5freplace_497',['find_one_and_replace',['../classmongocxx_1_1options_1_1find__one__and__replace.html',1,'mongocxx::options']]],
-  ['find_5fone_5fand_5fupdate_498',['find_one_and_update',['../classmongocxx_1_1options_1_1find__one__and__update.html',1,'mongocxx::options']]]
+  ['finalize_5ftype',['finalize_type',['../structbsoncxx_1_1builder_1_1stream_1_1finalize__type.html',1,'bsoncxx::builder::stream']]],
+  ['find',['find',['../classmongocxx_1_1options_1_1find.html',1,'mongocxx::options']]],
+  ['find_5fone_5fand_5fdelete',['find_one_and_delete',['../classmongocxx_1_1options_1_1find__one__and__delete.html',1,'mongocxx::options']]],
+  ['find_5fone_5fand_5freplace',['find_one_and_replace',['../classmongocxx_1_1options_1_1find__one__and__replace.html',1,'mongocxx::options']]],
+  ['find_5fone_5fand_5fupdate',['find_one_and_update',['../classmongocxx_1_1options_1_1find__one__and__update.html',1,'mongocxx::options']]]
 ];
--- api/mongocxx-3.2.0/search/functions_5.js
+++ api/mongocxx-3.2.0/search/functions_5.js
@@ -1,13 +1,13 @@
 var searchData=
 [
-  ['facet_644',['facet',['../classmongocxx_1_1pipeline.html#aa00e2df7f169024c0ed6085c66b649da',1,'mongocxx::pipeline']]],
-  ['file_5flength_645',['file_length',['../classmongocxx_1_1gridfs_1_1downloader.html#a2e3af5496395d6b6b0d2f4ea5e1c00c2',1,'mongocxx::gridfs::downloader']]],
-  ['files_5fdocument_646',['files_document',['../classmongocxx_1_1gridfs_1_1downloader.html#a8a350117ff4952e8ee79f963ba137272',1,'mongocxx::gridfs::downloader']]],
-  ['filter_647',['filter',['../classmongocxx_1_1model_1_1delete__many.html#a8f25b4ef7ab70927786935b0652a92c6',1,'mongocxx::model::delete_many::filter()'],['../classmongocxx_1_1model_1_1delete__one.html#a8596967d6803d1fdf60f9b47bd78464c',1,'mongocxx::model::delete_one::filter()'],['../classmongocxx_1_1model_1_1replace__one.html#ab9e324e3666fe54ed1da52fbff99ee8f',1,'mongocxx::model::replace_one::filter()'],['../classmongocxx_1_1model_1_1update__many.html#acd9a7a6c14cfb27b8a70532bf7000470',1,'mongocxx::model::update_many::filter()'],['../classmongocxx_1_1model_1_1update__one.html#abec07cce25c17754d9ddc2bf892e3977',1,'mongocxx::model::update_one::filter()']]],
-  ['find_648',['find',['../classbsoncxx_1_1array_1_1view.html#a64554908ffe1cb907bde083d73891961',1,'bsoncxx::array::view::find()'],['../classmongocxx_1_1gridfs_1_1bucket.html#aea1a02a75eb98a67788b94402ff90ba9',1,'mongocxx::gridfs::bucket::find()'],['../classmongocxx_1_1collection.html#ae3748d404ee8affee0103e75a276abe4',1,'mongocxx::collection::find()'],['../classbsoncxx_1_1document_1_1view.html#a24b5f4e6db66bf2dd66b1699bd064ccd',1,'bsoncxx::document::view::find()']]],
-  ['find_5fone_649',['find_one',['../classmongocxx_1_1collection.html#a7bc5c6087f55bf9a26b11f85c5ffe2c4',1,'mongocxx::collection']]],
-  ['find_5fone_5fand_5fdelete_650',['find_one_and_delete',['../classmongocxx_1_1collection.html#a3db5124c26fff90fb5f06796c7fefa7d',1,'mongocxx::collection']]],
-  ['find_5fone_5fand_5freplace_651',['find_one_and_replace',['../classmongocxx_1_1collection.html#a1e1e5146b69faaf263a2003874213993',1,'mongocxx::collection']]],
-  ['find_5fone_5fand_5fupdate_652',['find_one_and_update',['../classmongocxx_1_1collection.html#a6b04b5265e044c08d1ecd1e4185fb699',1,'mongocxx::collection']]],
-  ['from_5fjson_653',['from_json',['../namespacebsoncxx.html#a80fcbc1a4efcd33bdce336b57e6f4949',1,'bsoncxx']]]
+  ['facet',['facet',['../classmongocxx_1_1pipeline.html#aa00e2df7f169024c0ed6085c66b649da',1,'mongocxx::pipeline']]],
+  ['file_5flength',['file_length',['../classmongocxx_1_1gridfs_1_1downloader.html#a2e3af5496395d6b6b0d2f4ea5e1c00c2',1,'mongocxx::gridfs::downloader']]],
+  ['files_5fdocument',['files_document',['../classmongocxx_1_1gridfs_1_1downloader.html#a8a350117ff4952e8ee79f963ba137272',1,'mongocxx::gridfs::downloader']]],
+  ['filter',['filter',['../classmongocxx_1_1model_1_1delete__many.html#a8f25b4ef7ab70927786935b0652a92c6',1,'mongocxx::model::delete_many::filter()'],['../classmongocxx_1_1model_1_1delete__one.html#a8596967d6803d1fdf60f9b47bd78464c',1,'mongocxx::model::delete_one::filter()'],['../classmongocxx_1_1model_1_1replace__one.html#ab9e324e3666fe54ed1da52fbff99ee8f',1,'mongocxx::model::replace_one::filter()'],['../classmongocxx_1_1model_1_1update__many.html#acd9a7a6c14cfb27b8a70532bf7000470',1,'mongocxx::model::update_many::filter()'],['../classmongocxx_1_1model_1_1update__one.html#abec07cce25c17754d9ddc2bf892e3977',1,'mongocxx::model::update_one::filter()']]],
+  ['find',['find',['../classbsoncxx_1_1array_1_1view.html#a64554908ffe1cb907bde083d73891961',1,'bsoncxx::array::view::find()'],['../classbsoncxx_1_1document_1_1view.html#a24b5f4e6db66bf2dd66b1699bd064ccd',1,'bsoncxx::document::view::find()'],['../classmongocxx_1_1collection.html#ae3748d404ee8affee0103e75a276abe4',1,'mongocxx::collection::find()'],['../classmongocxx_1_1gridfs_1_1bucket.html#aea1a02a75eb98a67788b94402ff90ba9',1,'mongocxx::gridfs::bucket::find()']]],
+  ['find_5fone',['find_one',['../classmongocxx_1_1collection.html#a7bc5c6087f55bf9a26b11f85c5ffe2c4',1,'mongocxx::collection']]],
+  ['find_5fone_5fand_5fdelete',['find_one_and_delete',['../classmongocxx_1_1collection.html#a3db5124c26fff90fb5f06796c7fefa7d',1,'mongocxx::collection']]],
+  ['find_5fone_5fand_5freplace',['find_one_and_replace',['../classmongocxx_1_1collection.html#a1e1e5146b69faaf263a2003874213993',1,'mongocxx::collection']]],
+  ['find_5fone_5fand_5fupdate',['find_one_and_update',['../classmongocxx_1_1collection.html#a6b04b5265e044c08d1ecd1e4185fb699',1,'mongocxx::collection']]],
+  ['from_5fjson',['from_json',['../namespacebsoncxx.html#a80fcbc1a4efcd33bdce336b57e6f4949',1,'bsoncxx']]]
 ];
