4d4aae66c2b0e951697997e295be240ed163474d
Ezra Chung <ezra.chung@mongodb.com>
2024-07-23 11:01:57 -0500

    Revert 3.3.2 docs to their initial state (b22f831e)

--- api/mongocxx-3.3.2/search/classes_8.js
+++ api/mongocxx-3.3.2/search/classes_8.js
@@ -1,16 +1,16 @@
 var searchData=
 [
-  ['index_522',['index',['../classmongocxx_1_1options_1_1index.html',1,'mongocxx::options']]],
-  ['index_5fmodel_523',['index_model',['../classmongocxx_1_1index__model.html',1,'mongocxx']]],
-  ['index_5fview_524',['index_view',['../classmongocxx_1_1index__view.html',1,'mongocxx::index_view'],['../classmongocxx_1_1options_1_1index__view.html',1,'mongocxx::options::index_view']]],
-  ['init_5ftag_5ft_525',['init_tag_t',['../structbsoncxx_1_1oid_1_1init__tag__t.html',1,'bsoncxx::oid']]],
-  ['init_5ftag_5ft_5fdeprecated_526',['init_tag_t_deprecated',['../structbsoncxx_1_1oid_1_1init__tag__t__deprecated.html',1,'bsoncxx::oid']]],
-  ['insert_527',['insert',['../classmongocxx_1_1options_1_1insert.html',1,'mongocxx::options']]],
-  ['insert_5fmany_528',['insert_many',['../classmongocxx_1_1result_1_1insert__many.html',1,'mongocxx::result']]],
-  ['insert_5fone_529',['insert_one',['../classmongocxx_1_1model_1_1insert__one.html',1,'mongocxx::model::insert_one'],['../classmongocxx_1_1result_1_1insert__one.html',1,'mongocxx::result::insert_one']]],
-  ['instance_530',['instance',['../classmongocxx_1_1instance.html',1,'mongocxx']]],
-  ['is_5ferror_5fcode_5fenum_3c_20bsoncxx_3a_3aerror_5fcode_20_3e_531',['is_error_code_enum&lt; bsoncxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01bsoncxx_1_1error__code_01_4.html',1,'std']]],
-  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aerror_5fcode_20_3e_532',['is_error_code_enum&lt; mongocxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1error__code_01_4.html',1,'std']]],
-  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aserver_5ferror_5fcode_20_3e_533',['is_error_code_enum&lt; mongocxx::server_error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1server__error__code_01_4.html',1,'std']]],
-  ['iterator_534',['iterator',['../classmongocxx_1_1change__stream_1_1iterator.html',1,'mongocxx::change_stream::iterator'],['../classmongocxx_1_1cursor_1_1iterator.html',1,'mongocxx::cursor::iterator']]]
+  ['index',['index',['../classmongocxx_1_1options_1_1index.html',1,'mongocxx::options']]],
+  ['index_5fmodel',['index_model',['../classmongocxx_1_1index__model.html',1,'mongocxx']]],
+  ['index_5fview',['index_view',['../classmongocxx_1_1options_1_1index__view.html',1,'mongocxx::options::index_view'],['../classmongocxx_1_1index__view.html',1,'mongocxx::index_view']]],
+  ['init_5ftag_5ft',['init_tag_t',['../structbsoncxx_1_1oid_1_1init__tag__t.html',1,'bsoncxx::oid']]],
+  ['init_5ftag_5ft_5fdeprecated',['init_tag_t_deprecated',['../structbsoncxx_1_1oid_1_1init__tag__t__deprecated.html',1,'bsoncxx::oid']]],
+  ['insert',['insert',['../classmongocxx_1_1options_1_1insert.html',1,'mongocxx::options']]],
+  ['insert_5fmany',['insert_many',['../classmongocxx_1_1result_1_1insert__many.html',1,'mongocxx::result']]],
+  ['insert_5fone',['insert_one',['../classmongocxx_1_1model_1_1insert__one.html',1,'mongocxx::model::insert_one'],['../classmongocxx_1_1result_1_1insert__one.html',1,'mongocxx::result::insert_one']]],
+  ['instance',['instance',['../classmongocxx_1_1instance.html',1,'mongocxx']]],
+  ['is_5ferror_5fcode_5fenum_3c_20bsoncxx_3a_3aerror_5fcode_20_3e',['is_error_code_enum&lt; bsoncxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01bsoncxx_1_1error__code_01_4.html',1,'std']]],
+  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aerror_5fcode_20_3e',['is_error_code_enum&lt; mongocxx::error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1error__code_01_4.html',1,'std']]],
+  ['is_5ferror_5fcode_5fenum_3c_20mongocxx_3a_3aserver_5ferror_5fcode_20_3e',['is_error_code_enum&lt; mongocxx::server_error_code &gt;',['../structstd_1_1is__error__code__enum_3_01mongocxx_1_1server__error__code_01_4.html',1,'std']]],
+  ['iterator',['iterator',['../classmongocxx_1_1cursor_1_1iterator.html',1,'mongocxx::cursor::iterator'],['../classmongocxx_1_1change__stream_1_1iterator.html',1,'mongocxx::change_stream::iterator']]]
 ];
