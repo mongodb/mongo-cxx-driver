3c90e39f5509e7c30f4dab2ade07f45e8a77e352
Ezra Chung <ezra.chung@mongodb.com>
2024-10-29 15:28:24 -0500

    functions: setup

---
+++ .evergreen/config_generator/components/funcs/setup.py
@@ -0,0 +1,44 @@
+from config_generator.etc.function import Function
+from config_generator.etc.utils import bash_exec
+
+from shrub.v3.evg_command import EvgCommandType, git_get_project
+
+
+class Setup(Function):
+    name = 'setup'
+    commands = [
+        bash_exec(
+            command_type=EvgCommandType.SETUP,
+            script='''\
+                set -o errexit
+                set -o pipefail
+                rm -rf "mongo-cxx-driver"
+                rm -fr "mongo-c-driver"
+                rm -fr mongod
+                rm -fr drivers-evergreen-tools
+            '''
+        ),
+        git_get_project(directory='mongo-cxx-driver'),
+        bash_exec(
+            command_type=EvgCommandType.SETUP,
+            script='''\
+                set -o errexit
+                set -o pipefail
+                cc --version || true
+                c++ --version || true
+                gcc --version || true
+                g++ --version || true
+                clang --version || true
+                cmake --version || true
+                openssl version || true
+            '''
+        ),
+    ]
+
+    @classmethod
+    def call(cls, **kwargs):
+        return cls.default_call(**kwargs)
+
+
+def functions():
+    return Setup.defn()
