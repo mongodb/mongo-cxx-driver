35c0e2993800b4bdbcbc845d8930b33a7edf6601
Ezra Chung <ezra.chung@mongodb.com>
2024-12-18 09:13:44 -0600

    Address discrepancy between GNU and BSD sed

--- etc/generate-latest-apidocs.sh
+++ etc/generate-latest-apidocs.sh
@@ -66,7 +66,7 @@ else
   # Update the Doxyfile configuration file:
   #  - set OUTPUT_DIRECTORY to `build/docs/api/mongocxx-<version>`.
   #  - set PROJECT_NUMBER to `<version>`.
-  sed -i \
+  sed -i '' \
     -e "s|^OUTPUT_DIRECTORY\s*=\s*.*$|OUTPUT_DIRECTORY = ${output_directory:?}|g" \
     -e "s|^PROJECT_NUMBER\s*=\s*.*$|PROJECT_NUMBER = ${LATEST_VERSION:?}|g" \
     "${scratch_dir:?}/Doxyfile"
