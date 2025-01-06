b4c13cdf43f65355352c75c72a27c480d3ab4f05
Ezra Chung <ezra.chung@mongodb.com>
2025-01-06 14:32:45 -0600

    Fix initial casing

--- src/mongocxx/include/mongocxx/v_noabi/mongocxx/options/data_key.hpp
+++ src/mongocxx/include/mongocxx/v_noabi/mongocxx/options/data_key.hpp
@@ -30,7 +30,7 @@ namespace v_noabi {
 namespace options {
 
 ///
-/// used by @ref mongocxx::v_noabi::client_encryption::create_data_key.
+/// Used by @ref mongocxx::v_noabi::client_encryption::create_data_key.
 ///
 class data_key {
    public:
