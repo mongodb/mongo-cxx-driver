4f0d701e61ce5ed3e9f75bcf0d278a3b58af343c
Ezra Chung <ezra.chung@mongodb.com>
2025-02-11 16:26:20 -0600

    examples: extend use of /Z7 to building example projects

--- examples/projects/mongocxx/cmake/static/build.sh
+++ examples/projects/mongocxx/cmake/static/build.sh
@@ -9,6 +9,6 @@ if [ -z "$MSVC" ]; then
   "${cmake_binary:?}" -DCMAKE_BUILD_TYPE="${build_type}" -DCMAKE_CXX_STANDARD="${CXX_STANDARD:?}" ..
   "${cmake_binary:?}" --build . --target run
 else
-  "${cmake_binary:?}" -G "Visual Studio 15 2017" -A "x64" -DCMAKE_CXX_STANDARD="${CXX_STANDARD:?}" ..
+  "${cmake_binary:?}" -G "Visual Studio 15 2017" -A "x64" -DCMAKE_CXX_STANDARD="${CXX_STANDARD:?}" -DCMAKE_POLICY_DEFAULT_CMP0141=NEW -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT=Embedded ..
   "${cmake_binary:?}" --build . --target run --config "${build_type:?}" -- /verbosity:minimal
 fi
