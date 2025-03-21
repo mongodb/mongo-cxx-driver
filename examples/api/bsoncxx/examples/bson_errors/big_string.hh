d1f59dd89f7dbe9c8e3e4ee7a25f20f6536f71b3
Micah Scott <micah.scott@mongodb.com>
2025-03-21 11:46:21 -0700

    Revert "remove obsolete workaround for CDRIVER-5732"
    This reverts commit 2b5c35a21340d55afc74bf3acd2add543684082b.

--- examples/api/bsoncxx/examples/bson_errors/big_string.hh
+++ examples/api/bsoncxx/examples/bson_errors/big_string.hh
@@ -27,7 +27,8 @@ namespace examples {
 // Used to trigger builder append failure.
 struct big_string {
     // BSON_SIZE_MAX == 0x7FFFFFFF
-    std::size_t length{static_cast<std::size_t>(std::numeric_limits<std::int32_t>::max())};
+    // Leave some room for CDRIVER-5732.
+    std::size_t length{static_cast<std::size_t>(std::numeric_limits<std::int32_t>::max() - 32u)};
 
     // Allocate an UNINITIALIZED blob of data that will not be accessed due to length checks.
     // Leaving memory unitialized (rather than zero-init) should hopefully avoid slow and expensive
