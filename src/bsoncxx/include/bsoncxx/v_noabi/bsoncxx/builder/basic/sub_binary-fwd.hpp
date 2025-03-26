dccb2fdcf87297077cb19bcc29e37b855eae649c
mdbmes <micah.scott@mongodb.com>
2025-03-26 14:31:40 -0700

    Update src/bsoncxx/include/bsoncxx/v_noabi/bsoncxx/builder/basic/sub_binary-fwd.hpp
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- src/bsoncxx/include/bsoncxx/v_noabi/bsoncxx/builder/basic/sub_binary-fwd.hpp
+++ src/bsoncxx/include/bsoncxx/v_noabi/bsoncxx/builder/basic/sub_binary-fwd.hpp
@@ -38,5 +38,5 @@ using ::bsoncxx::v_noabi::builder::basic::sub_binary;
 
 ///
 /// @file
-/// Declares @ref bsoncxx::builder::basic::sub_binary
+/// Declares @ref bsoncxx::v_noabi::builder::basic::sub_binary
 ///
