7ffdb172649580847fa1d02d10714d32d82981d5
mdbmes <micah.scott@mongodb.com>
2025-03-26 15:02:36 -0700

    Update src/bsoncxx/lib/bsoncxx/v_noabi/bsoncxx/vector.cpp
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- src/bsoncxx/lib/bsoncxx/v_noabi/bsoncxx/vector.cpp
+++ src/bsoncxx/lib/bsoncxx/v_noabi/bsoncxx/vector.cpp
@@ -12,12 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <bsoncxx/vector/detail.hpp>
+#include <bsoncxx/vector/formats.hpp>
+
+//
+
 #include <cstdint>
 
 #include <bsoncxx/exception/error_code.hpp>
 #include <bsoncxx/exception/exception.hpp>
-#include <bsoncxx/vector/detail.hpp>
-#include <bsoncxx/vector/formats.hpp>
 
 #include <bson/bson.h>
 
