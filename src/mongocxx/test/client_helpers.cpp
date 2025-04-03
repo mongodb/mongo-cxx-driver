db009f1274b3a3d1016cff4f6fb8cd8589fd0b66
Kevin Albertson <kevin.albertson@mongodb.com>
2025-04-03 14:03:12 +0000

    add missing server api version

--- src/mongocxx/test/client_helpers.cpp
+++ src/mongocxx/test/client_helpers.cpp
@@ -57,7 +57,7 @@ namespace {
 // These frequently used network calls are cached to avoid bottlenecks during tests.
 document::value get_is_master() {
     static auto reply = []() {
-        auto client = mongocxx::client(mongocxx::uri());
+        auto client = mongocxx::client{mongocxx::uri{}, test_util::add_test_server_api()};
         return client["admin"].run_command(make_document(kvp("isMaster", 1)));
     }();
     return reply;
@@ -65,7 +65,7 @@ document::value get_is_master() {
 
 document::value get_server_status() {
     static auto status = []() {
-        auto client = mongocxx::client(mongocxx::uri());
+        auto client = mongocxx::client{mongocxx::uri{}, test_util::add_test_server_api()};
         return client["admin"].run_command(make_document(kvp("serverStatus", 1)));
     }();
     return status;
@@ -73,7 +73,7 @@ document::value get_server_status() {
 
 bsoncxx::stdx::optional<document::value> get_shards() {
     static auto shards = []() {
-        auto client = mongocxx::client(mongocxx::uri());
+        auto client = mongocxx::client{mongocxx::uri{}, test_util::add_test_server_api()};
         return client["config"]["shards"].find_one({});
     }();
     return (shards) ? shards.value() : bsoncxx::stdx::optional<document::value>{};
