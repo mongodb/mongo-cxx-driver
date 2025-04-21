d58a8841240132e88e343a012c932fed617ea9fc
Kevin Albertson <kevin.albertson@mongodb.com>
2025-04-21 14:24:12 -0400

    format

--- src/mongocxx/include/mongocxx/v_noabi/mongocxx/read_preference.hpp
+++ src/mongocxx/include/mongocxx/v_noabi/mongocxx/read_preference.hpp
@@ -289,7 +289,8 @@ class read_preference {
     ///
     /// @return A hedge document if one was set.
     ///
-    MONGOCXX_DEPRECATED MONGOCXX_ABI_EXPORT_CDECL(bsoncxx::v_noabi::stdx::optional<bsoncxx::v_noabi::document::view> const)
+    MONGOCXX_DEPRECATED MONGOCXX_ABI_EXPORT_CDECL(bsoncxx::v_noabi::stdx::optional<
+                                                  bsoncxx::v_noabi::document::view> const)
     hedge() const;
 
     ///
