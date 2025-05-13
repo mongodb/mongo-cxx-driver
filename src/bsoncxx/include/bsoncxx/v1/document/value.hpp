5edf0963b9dee8cc1d5c77f080c9467f87f26e63
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-05-13 09:33:23 -0500

    CXX-3231 add v1 forward headers and component files (#1401)

---
+++ src/bsoncxx/include/bsoncxx/v1/document/value.hpp
@@ -0,0 +1,43 @@
+// Copyright 2009-present MongoDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <bsoncxx/v1/document/value-fwd.hpp>
+
+//
+
+#include <bsoncxx/v1/detail/prelude.hpp>
+
+namespace bsoncxx {
+namespace v1 {
+namespace document {
+
+///
+/// A BSON document.
+///
+/// @attention This feature is experimental! It is not ready for use!
+///
+class value {};
+
+} // namespace document
+} // namespace v1
+} // namespace bsoncxx
+
+#include <bsoncxx/v1/detail/postlude.hpp>
+
+///
+/// @file
+/// Provides @ref bsoncxx::v1::document::value.
+///
