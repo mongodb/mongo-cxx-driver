073e69639eb45da811ec68239da2555725543ebd
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-05-21 15:17:57 -0500

    CXX-3294 Relocate all remaining Manual Doc Page examples into the examples directory (#1407)
    * Remove assumptions about the name of replica sets

---
+++ examples/mongocxx/mongodb.com/transactions_examples.cpp
@@ -0,0 +1,290 @@
+// Copyright 2009-present MongoDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+#include <mongocxx/client.hpp>
+#include <mongocxx/client_session.hpp>
+#include <mongocxx/exception/operation_exception.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/options/transaction.hpp>
+#include <mongocxx/read_concern.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/write_concern.hpp>
+
+#include <examples/macros.hh>
+
+namespace {
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
+
+using namespace mongocxx;
+
+void transactions_intro_example_1(mongocxx::client& client) {
+    // Start Transactions Intro Example 1
+    auto update_employee_info = [](client_session& session) {
+        auto& client = session.client();
+        auto employees = client["hr"]["employees"];
+        auto events = client["reporting"]["events"];
+
+        options::transaction txn_opts;
+        read_concern rc;
+        rc.acknowledge_level(read_concern::level::k_snapshot);
+        txn_opts.read_concern(rc);
+        write_concern wc;
+        wc.acknowledge_level(write_concern::level::k_majority);
+        txn_opts.write_concern(wc);
+        session.start_transaction(txn_opts);
+
+        try {
+            employees.update_one(
+                make_document(kvp("employee", 3)),
+                make_document(kvp("$set", make_document(kvp("status", "Inactive")))));
+            events.insert_one(make_document(
+                kvp("employee", 3), kvp("status", make_document(kvp("new", "Inactive"), kvp("old", "Active")))));
+        } catch (operation_exception const& oe) {
+            std::cout << "Caught exception during transaction, aborting." << std::endl;
+            session.abort_transaction();
+            throw oe;
+        }
+
+        while (true) {
+            try {
+                session.commit_transaction(); // Uses write concern set at transaction start.
+                std::cout << "Transaction committed." << std::endl;
+                break;
+            } catch (operation_exception const& oe) {
+                // Can retry commit.
+                if (oe.has_error_label("UnknownTransactionCommitResult")) {
+                    std::cout << "UnknownTransactionCommitResult, retrying commit operation ..." << std::endl;
+                    continue;
+                } else {
+                    std::cout << "Error during commit ..." << std::endl;
+                    throw oe;
+                }
+            }
+        }
+    };
+    // End Transactions Intro Example 1
+
+    auto session = client.start_session();
+    update_employee_info(session);
+}
+
+void transactions_retry_example_1(mongocxx::client& client) {
+    // Start Transactions Retry Example 1
+    using transaction_func = std::function<void(client_session & session)>;
+    auto run_transaction_with_retry = [](transaction_func txn_func, client_session& session) {
+        while (true) {
+            try {
+                txn_func(session); // performs transaction.
+                break;
+            } catch (operation_exception const& oe) {
+                std::cout << "Transaction aborted. Caught exception during transaction." << std::endl;
+                // If transient error, retry the whole transaction.
+                if (oe.has_error_label("TransientTransactionError")) {
+                    std::cout << "TransientTransactionError, retrying transaction ..." << std::endl;
+                    continue;
+                } else {
+                    throw oe;
+                }
+            }
+        }
+    };
+    // End Transactions Retry Example 1
+
+    auto session = client.start_session();
+    run_transaction_with_retry(
+        [&client](client_session& session) {
+            session.start_transaction();
+            auto coll = client["test"]["coll"];
+            coll.insert_one(make_document(kvp("x", 1)));
+            session.commit_transaction();
+        },
+        session);
+}
+
+void transactions_retry_example_2(mongocxx::client& client) {
+    // Start Transactions Retry Example 2
+    auto commit_with_retry = [](client_session& session) {
+        while (true) {
+            try {
+                session.commit_transaction(); // Uses write concern set at transaction start.
+                std::cout << "Transaction committed." << std::endl;
+                break;
+            } catch (operation_exception const& oe) {
+                // Can retry commit
+                if (oe.has_error_label("UnknownTransactionCommitResult")) {
+                    std::cout << "UnknownTransactionCommitResult, retrying commit operation ..." << std::endl;
+                    continue;
+                } else {
+                    std::cout << "Error during commit ..." << std::endl;
+                    throw oe;
+                }
+            }
+        }
+    };
+    // End Transactions Retry Example 2
+
+    auto session = client.start_session();
+    session.start_transaction();
+    auto coll = client["test"]["coll"];
+    coll.insert_one(make_document(kvp("x", 1)));
+    commit_with_retry(session);
+}
+
+void transactions_retry_example_3(mongocxx::client& client) {
+    // Start Transactions Retry Example 3
+    using transaction_func = std::function<void(client_session & session)>;
+    auto run_transaction_with_retry = [](transaction_func txn_func, client_session& session) {
+        while (true) {
+            try {
+                txn_func(session); // performs transaction.
+                break;
+            } catch (operation_exception const& oe) {
+                std::cout << "Transaction aborted. Caught exception during transaction." << std::endl;
+                // If transient error, retry the whole transaction.
+                if (oe.has_error_label("TransientTransactionError")) {
+                    std::cout << "TransientTransactionError, retrying transaction ..." << std::endl;
+                    continue;
+                } else {
+                    throw oe;
+                }
+            }
+        }
+    };
+
+    auto commit_with_retry = [](client_session& session) {
+        while (true) {
+            try {
+                session.commit_transaction(); // Uses write concern set at transaction start.
+                std::cout << "Transaction committed." << std::endl;
+                break;
+            } catch (operation_exception const& oe) {
+                // Can retry commit
+                if (oe.has_error_label("UnknownTransactionCommitResult")) {
+                    std::cout << "UnknownTransactionCommitResult, retrying commit operation ..." << std::endl;
+                    continue;
+                } else {
+                    std::cout << "Error during commit ..." << std::endl;
+                    throw oe;
+                }
+            }
+        }
+    };
+
+    // Updates two collections in a transaction
+    auto update_employee_info = [&](client_session& session) {
+        auto& client = session.client();
+        auto employees = client["hr"]["employees"];
+        auto events = client["reporting"]["events"];
+
+        options::transaction txn_opts;
+        read_concern rc;
+        rc.acknowledge_level(read_concern::level::k_snapshot);
+        txn_opts.read_concern(rc);
+        write_concern wc;
+        wc.acknowledge_level(write_concern::level::k_majority);
+        txn_opts.write_concern(wc);
+
+        session.start_transaction(txn_opts);
+
+        try {
+            employees.update_one(
+                make_document(kvp("employee", 3)),
+                make_document(kvp("$set", make_document(kvp("status", "Inactive")))));
+            events.insert_one(make_document(
+                kvp("employee", 3), kvp("status", make_document(kvp("new", "Inactive"), kvp("old", "Active")))));
+        } catch (operation_exception const& oe) {
+            std::cout << "Caught exception during transaction, aborting." << std::endl;
+            session.abort_transaction();
+            throw oe;
+        }
+
+        commit_with_retry(session);
+    };
+
+    auto session = client.start_session();
+    try {
+        run_transaction_with_retry(update_employee_info, session);
+    } catch (operation_exception const& oe) {
+        // Do something with error.
+        throw oe;
+    }
+    // End Transactions Retry Example 3
+}
+
+} // namespace
+
+int EXAMPLES_CDECL main() {
+    if (char const* const topology_env = std::getenv("MONGOCXX_TEST_TOPOLOGY")) {
+        auto const topology = std::string(topology_env);
+        if (topology != "replica") {
+            std::cerr << "Skipping: transactions example requires a replica set" << std::endl;
+            return 0;
+        }
+    }
+
+    // The mongocxx::instance constructor and destructor initialize and shut down the driver,
+    // respectively. Therefore, a mongocxx::instance must be created before using the driver and
+    // must remain alive for as long as the driver is in use.
+    mongocxx::instance const inst{};
+
+    std::function<void(mongocxx::client&)> const examples[] = {
+        transactions_intro_example_1,
+        transactions_retry_example_1,
+        transactions_retry_example_2,
+        transactions_retry_example_3,
+    };
+
+    client client{uri{}};
+
+    {
+        auto const reply = client["admin"].run_command(make_document(kvp("isMaster", 1)));
+        auto const max_wire_version = reply.view()["maxWireVersion"];
+        if (!max_wire_version) {
+            std::cerr << "Skipping: could not determine max wire version" << std::endl;
+            return EXIT_SUCCESS;
+        }
+        if (max_wire_version.type() != bsoncxx::type::k_int32) {
+            throw std::logic_error{"max wire version is not int32"};
+        }
+        if (max_wire_version.get_int32().value < 7) {
+            std::cerr << "Skipping: transactions example requires max wire version is >= 7" << std::endl;
+            return EXIT_SUCCESS;
+        }
+    }
+
+    for (auto const& example : examples) {
+        /* Create necessary collections. */
+        client["hr"]["employees"].drop();
+        client["hr"]["employees"].insert_one(make_document(kvp("employee", 3), kvp("status", "Active")));
+        client["reporting"]["events"].drop();
+        client["reporting"]["events"].insert_one(make_document(kvp("employee", 3), kvp("status", "Active")));
+
+        try {
+            example(client);
+        } catch (std::logic_error const& e) {
+            std::cerr << e.what() << std::endl;
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
--- examples/mongocxx/mongodb.com/versioned_api_examples.cpp
+++ examples/mongocxx/mongodb.com/versioned_api_examples.cpp
@@ -12,8 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <mongocxx/test/v_noabi/client_helpers.hh>
+#include <functional>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/string/to_string.hpp>
 
 #include <mongocxx/client.hpp>
@@ -23,46 +30,104 @@
 #include <mongocxx/options/server_api.hpp>
 #include <mongocxx/uri.hpp>
 
-#include <bsoncxx/test/catch.hh>
+#include <examples/macros.hh>
 
-#include <catch2/matchers/catch_matchers_string.hpp>
+namespace {
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
 
 using namespace mongocxx;
 
-static bool has_api_version_1() {
-    // API Version 1 was introduced in 5.0.
-    return test_util::get_max_wire_version() >= 13;
+// API Version 1 was introduced in 5.0.
+bool has_api_version_1() {
+    mongocxx::client client{mongocxx::uri{}};
+
+    auto const reply = client["admin"].run_command(make_document(kvp("isMaster", 1)));
+    auto const max_wire_version = reply.view()["maxWireVersion"];
+
+    if (!max_wire_version) {
+        return false;
+    }
+
+    if (max_wire_version.type() != bsoncxx::type::k_int32) {
+        throw std::logic_error{"max wire version is not int32"};
+    }
+
+    if (max_wire_version.get_int32().value < 13) {
+        return false;
+    }
+
+    return true;
 }
 
-static bool has_api_version_1_with_count() {
+bool has_api_version_1_with_count() {
     if (!has_api_version_1()) {
         return false;
     }
 
-    auto const version = test_util::get_server_version();
+    mongocxx::client client{mongocxx::uri{}};
+
+    auto const status = client["admin"].run_command(make_document(kvp("serverStatus", 1)));
+    auto const version = bsoncxx::string::to_string(status.view()["version"].get_string().value);
+
+    // mongocxx::test_util::compare_versions
+    auto compare_versions = [](std::string version1, std::string version2) -> std::int32_t {
+        // mongocxx::test_util::parse_version
+        auto const parse_version = [](std::string version) -> std::vector<std::int32_t> {
+            std::vector<std::int32_t> elements;
+            std::stringstream ss{version};
+            std::string element;
+
+            while (std::getline(ss, element, '.')) {
+                elements.push_back(std::stoi(element));
+            }
+
+            return elements;
+        };
+
+        std::vector<std::int32_t> v1 = parse_version(version1);
+        std::vector<std::int32_t> v2 = parse_version(version2);
+
+        for (std::size_t i = 0; i < std::min(v1.size(), v2.size()); ++i) {
+            std::int32_t difference = v1[i] - v2[i];
+
+            if (difference != 0) {
+                return difference;
+            }
+        }
+
+        return 0;
+    };
 
     // BACKPORT-12171: count command was backported to 5.0.9.
-    if (test_util::compare_versions(version, "5.0") == 0 && test_util::compare_versions(version, "5.0.9") >= 0) {
+    if (compare_versions(version, "5.0") == 0 && compare_versions(version, "5.0.9") >= 0) {
         return true;
     }
 
     // BACKPORT-12170: count command was backported to 5.3.2.
-    if (test_util::compare_versions(version, "5.3") == 0 && test_util::compare_versions(version, "5.3.2") >= 0) {
+    if (compare_versions(version, "5.3") == 0 && compare_versions(version, "5.3.2") >= 0) {
         return true;
     }
 
+    // mongocxx::test_util::get_server_version
+    auto const get_server_version = [&status]() {
+        return bsoncxx::string::to_string(status.view()["version"].get_string().value);
+    };
+
     // SERVER-63850: count command was added in 6.0.
-    return test_util::newer_than("6.0");
+    return (compare_versions(get_server_version(), version) >= 0);
 }
 
 // We'll format many of these examples by hand
 // clang-format off
 
-TEST_CASE("Versioned API, non-strict") {
-    instance::current();
+void versioned_api_example_1() {
     if (!has_api_version_1()) {
+        std::cerr << "Skipping: Versioned API Example 1 requires MongoDB 5.0 or newer" << std::endl;
         return;
     }
+
     // Start Versioned API Example 1
     using namespace mongocxx;
     uri client_uri{"mongodb://localhost"};
@@ -78,11 +143,12 @@ TEST_CASE("Versioned API, non-strict") {
     // End Versioned API Example 1
 }
 
-TEST_CASE("Versioned API, strict") {
-    instance::current();
+void versioned_api_example_2() {
     if (!has_api_version_1()) {
+        std::cerr << "Skipping: Versioned API Example 2 requires MongoDB 5.0 or newer" << std::endl;
         return;
     }
+
     // Start Versioned API Example 2
     using namespace mongocxx;
     uri client_uri{"mongodb://localhost"};
@@ -99,11 +165,12 @@ TEST_CASE("Versioned API, strict") {
     // End Versioned API Example 2
 }
 
-TEST_CASE("Versioned API, non-strict, for commands/features outside versioned API") {
-    instance::current();
+void versioned_api_example_3() {
     if (!has_api_version_1()) {
+        std::cerr << "Skipping: Versioned API Example 3 requires MongoDB 5.0 or newer" << std::endl;
         return;
     }
+
     // Start Versioned API Example 3
     using namespace mongocxx;
     uri client_uri{"mongodb://localhost"};
@@ -120,11 +187,12 @@ TEST_CASE("Versioned API, non-strict, for commands/features outside versioned AP
     // End Versioned API Example 3
 }
 
-TEST_CASE("Versioned API, non-strict with deprecation errors") {
-    instance::current();
+void versioned_api_example_4() {
     if (!has_api_version_1()) {
+        std::cerr << "Skipping: Versioned API Example 4 requires MongoDB 5.0 or newer" << std::endl;
         return;
     }
+
     // Start Versioned API Example 4
     using namespace mongocxx;
     uri client_uri{"mongodb://localhost"};
@@ -144,19 +212,19 @@ TEST_CASE("Versioned API, non-strict with deprecation errors") {
 
 /// Not actually a version function. Just used to appear in the documentation examples "as if" we
 /// were creating a date from a timestamp string
-static bsoncxx::types::b_date iso_string_to_bson_datetime(std::string const&) {
+bsoncxx::types::b_date iso_string_to_bson_datetime(std::string const&) {
     return bsoncxx::types::b_date(std::chrono::milliseconds{0});
 }
 
-TEST_CASE("Versioned API, with insert-many for 'count' migration") {
-    instance::current();
-
+void versioned_api_example_5() {
     if (!has_api_version_1()) {
+        std::cerr << "Skipping: Versioned API Example 5 requires MongoDB 5.0 or newer" << std::endl;
         return;
     }
 
     // Do *not* run this test if count command is included in API Version 1.
     if (has_api_version_1_with_count()) {
+        std::cerr << "Skipping: Versioned API Example 5 requires the 'count' command is NOT supported" << std::endl;
         return;
     }
 
@@ -199,13 +267,20 @@ TEST_CASE("Versioned API, with insert-many for 'count' migration") {
 
     try {
         db.run_command(make_document(kvp("count", "sales")));
-        FAIL_CHECK("Did not throw for apiStrict:true usage of old command");
+        throw std::logic_error{"Did not throw for apiStrict:true usage of old command"};
     } catch (mongocxx::operation_exception const& error) {
-        INFO(error.what());
-        CHECK(error.code().value() == 323);
-        CHECK_THAT(
-            error.what(),
-            Catch::Matchers::StartsWith("Provided apiStrict:true, but the command count is not in API Version 1."));
+        auto const what = std::string(error.what());
+        auto const value = error.code().value();
+
+        std::cout << what << std::endl;
+
+        if (value != 323) {
+            throw std::logic_error{"expected error code to be 323, got: " + std::to_string(value)};
+        }
+
+        if (what.find("Provided apiStrict:true, but the command count is not in API Version 1.") != 0) {
+            throw std::logic_error{"expected error message to start with 'Provided apiStrict:true'"};
+        }
     }
 
 #if 0
@@ -225,7 +300,32 @@ TEST_CASE("Versioned API, with insert-many for 'count' migration") {
     // Start Versioned API Example 7
     std::int64_t num_documents = sales.count_documents(make_document());
     // End Versioned API Example 7
+
     // Start Versioned API Example 8
-    CHECK(num_documents == 8);
+    if (num_documents != 8) {
+        throw std::logic_error{"expected the number of documents to equal 8"};
+    }
     // End Versioned API Example 8
+}
+
+} // namespace
+
+int EXAMPLES_CDECL main() {
+    // The mongocxx::instance constructor and destructor initialize and shut down the driver,
+    // respectively. Therefore, a mongocxx::instance must be created before using the driver and
+    // must remain alive for as long as the driver is in use.
+    mongocxx::instance const inst{};
+
+    try {
+        versioned_api_example_1();
+        versioned_api_example_2();
+        versioned_api_example_3();
+        versioned_api_example_4();
+        versioned_api_example_5();
+    } catch (std::logic_error const& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
