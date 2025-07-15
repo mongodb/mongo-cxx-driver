0d350d469158b745e4aed40a0bcc7a48ff52c6d5
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-07-15 14:55:45 -0500

    CXX-3311 Remove Debian 10 EVG task coverage (#1423)

--- .evergreen/config_generator/etc/distros.py
+++ .evergreen/config_generator/etc/distros.py
@@ -47,8 +47,7 @@ def ls_distro(name, **kwargs):
 
 
 DEBIAN_DISTROS = [
-    *ls_distro(name='debian10', os='debian', os_type='linux', os_ver='10'),
-    *ls_distro(name='debian11', os='debian', os_type='linux', os_ver='10'),
+    *ls_distro(name='debian11', os='debian', os_type='linux', os_ver='11'),
     *ls_distro(name='debian12-latest', os='debian', os_type='linux', os_ver='latest'),
 ]
 
