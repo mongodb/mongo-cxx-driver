3c03e3940522af9066528e5b27bdae25c54fced5
Ezra Chung <ezra.chung@mongodb.com>
2025-08-25 14:07:54 -0500

    Format source files with etc/format.py

--- src/mongocxx/test/spec/unified_tests/operations.cpp
+++ src/mongocxx/test/spec/unified_tests/operations.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "operations.hh"
+#include "./operations.hh"
 
 #include <sstream>
 #include <vector>
