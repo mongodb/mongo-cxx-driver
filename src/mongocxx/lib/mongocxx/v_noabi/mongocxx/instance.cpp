ba058a46f5a74451c6ddbe87d0ca0a66173094d4
Ezra Chung <ezra.chung@mongodb.com>
2025-10-02 09:19:47 -0500

    Simplify v_noabi implementation (capture pre-init logs)

--- src/mongocxx/lib/mongocxx/v_noabi/mongocxx/instance.cpp
+++ src/mongocxx/lib/mongocxx/v_noabi/mongocxx/instance.cpp
@@ -46,41 +46,13 @@ instance* sentinel() {
     return reinterpret_cast<instance*>(value);
 }
 
-void custom_log_handler(
-    mongoc_log_level_t log_level,
-    char const* domain,
-    char const* message,
-    void* user_data) noexcept {
-    (*static_cast<v_noabi::logger*>(user_data))(
-        static_cast<v_noabi::log_level>(log_level),
-        bsoncxx::v1::stdx::string_view(domain),
-        bsoncxx::v1::stdx::string_view(message));
-}
-
 } // namespace
 
 class instance::impl {
    public:
     v1::instance _instance;
-    std::unique_ptr<logger> _handler;
 
-    impl(impl&&) = delete;
-    impl& operator=(impl&&) = delete;
-    impl(impl const&) = delete;
-    impl& operator=(impl const&) = delete;
-
-    impl(std::unique_ptr<logger> handler) try : _instance{v1::default_logger{}}, _handler{std::move(handler)} {
-        // For backward compatibility, allow v1::instance to initialize mongoc with the default log handler, then set
-        // the custom logger AFTER initialization has already completed.
-        if (_handler) {
-            libmongoc::log_set_handler(&custom_log_handler, _handler.get());
-
-            // Inform the user that a custom log handler has been registered.
-            // Cannot use mocked `libmongoc::log()` due to varargs.
-            mongoc_log(MONGOC_LOG_LEVEL_INFO, "mongocxx", "libmongoc logging callback enabled");
-        } else {
-            libmongoc::log_set_handler(nullptr, nullptr);
-        }
+    impl(std::unique_ptr<logger> handler) try : _instance{std::move(handler)} {
     } catch (v1::exception const&) {
         throw v_noabi::logic_error{error_code::k_cannot_recreate_instance};
     }
