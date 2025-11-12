2f728ecbdd888d3d01bd803dcc7074efd2b7dde3
Ezra Chung <ezra.chung@mongodb.com>
2025-11-12 09:14:05 -0600

    Filter clang-tidy task to library sources only

--- etc/run-clang-tidy.sh
+++ etc/run-clang-tidy.sh
@@ -19,7 +19,8 @@ install_build_tools
 export CMAKE_GENERATOR="Ninja"
 
 uv tool install -q clang-tidy
-clang-tidy --version
+version="$(clang-tidy --version | perl -lne 'print $1 if m|LLVM version (\d+\.\d+\.\d+)|')"
+echo "clang-tidy version: ${version:?}"
 
 # Use ccache if available.
 if [[ -f "../mongoc/.evergreen/scripts/find-ccache.sh" ]]; then
@@ -36,41 +37,14 @@ cmake_config_flags=(
 )
 
 # Generate the compilation database file.
-cmake -S . -B build "${cmake_config_flags[@]}"
+cmake "${cmake_config_flags[@]}" -B build
 
-# Some files (i.e. headers) may need to be generated during the build step.
-cmake --build build
+mapfile -t sources < <(find src -type f \( -name *.cc -o -name *.cpp \) | perl -lne 'print if m$.*/(?:bsoncxx|mongocxx)/lib/.*$')
 
-#
-# Each check has a name and the checks to run can be chosen using the -checks= option, which specifies a comma-separated
-# list of positive and negative (prefixed with -) globs. For example:
-#
-#    $ clang-tidy test.cpp -checks=-*,clang-analyzer-*,-clang-analyzer-cplusplus*
-#
-# will disable all default checks (-*) and enable all clang-analyzer-* checks except for clang-analyzer-cplusplus* ones.
-#
-# The -list-checks option lists all the enabled checks. When used without -checks=, it shows checks enabled by default.
-# Use -checks=* to see all available checks or with any other value of -checks= to see which checks are enabled by this
-# value.
-#
-# see https://clang.llvm.org/extra/clang-tidy
-#
-
-echo "Running clang-tidy with configuration:"
-clang-tidy -p=build -dump-config
-
-find_args=(
-  -type f
-  \( -name *.hh -o -name *.hpp -o -name *.cpp \)    # All sources including headers.
-  -not -path "*/third_party/*"                      # Excluding third party headers.
-  -not -path "*/config/*.hpp"                       # Excluding config headers.
-  -not -path "*bsoncxx/v_noabi/bsoncxx/enums/*.hpp" # Excluding X macro headers.
+clang_tidy_flags=(
+  --quiet
+  -p build
+  --header-filter '.*/(?:bsoncxx|mongocxx)/(?:include|lib)/.*'
 )
 
-echo "Scanning the following files:"
-find src "${find_args[@]}"
-
-# TODO: update clang-tidy config and address warnings.
-{
-  find src "${find_args[@]}" | parallel clang-tidy --quiet -p=build {} 2>/dev/null
-} || true
+printf "%s\n" "${sources[@]}" | parallel -q "${parallel_flags[@]}" clang-tidy "${clang_tidy_flags[@]:?}" {} 2>/dev/null
