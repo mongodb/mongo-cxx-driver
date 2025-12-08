036c35789078e107c89debeedbf7c0df81803aa9
Ezra Chung <ezra.chung@mongodb.com>
2025-12-08 15:37:33 -0600

    v1: find_one_and_replace_options (CXX-3237, CXX-3238)

--- src/mongocxx/include/mongocxx/v_noabi/mongocxx/options/find_one_and_replace-fwd.hpp
+++ src/mongocxx/include/mongocxx/v_noabi/mongocxx/options/find_one_and_replace-fwd.hpp
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <mongocxx/v1/find_one_and_replace_options-fwd.hpp> // IWYU pragma: export
+
 #include <mongocxx/config/prelude.hpp>
 
 namespace mongocxx {
@@ -29,7 +31,7 @@ class find_one_and_replace;
 namespace mongocxx {
 namespace options {
 
-using ::mongocxx::v_noabi::options::find_one_and_replace;
+using v_noabi::options::find_one_and_replace;
 
 } // namespace options
 } // namespace mongocxx
@@ -39,4 +41,7 @@ using ::mongocxx::v_noabi::options::find_one_and_replace;
 ///
 /// @file
 /// Declares @ref mongocxx::v_noabi::options::find_one_and_replace.
+///
+/// @par Includes
+/// - @ref mongocxx/v1/find_one_and_replace_options-fwd.hpp
 ///
