ec658232fd1fc626f5e0e4179bf7552269f9c9d2
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-12-09 11:09:53 -0600

    v1: find_one_and_replace_options (CXX-3237, CXX-3238) (#1537)

---
+++ src/mongocxx/lib/mongocxx/v1/find_one_and_replace_options.hh
@@ -0,0 +1,44 @@
+// Copyright 2009-present MongoDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <mongocxx/v1/find_one_and_replace_options.hpp> // IWYU pragma: export
+
+//
+
+#include <bsoncxx/v1/document/value-fwd.hpp>
+#include <bsoncxx/v1/types/value-fwd.hpp>
+
+#include <mongocxx/v1/hint-fwd.hpp>
+#include <mongocxx/v1/write_concern-fwd.hpp>
+
+#include <bsoncxx/v1/stdx/optional.hpp>
+
+namespace mongocxx {
+namespace v1 {
+
+class find_one_and_replace_options::internal {
+   public:
+    static bsoncxx::v1::stdx::optional<bsoncxx::v1::document::value>& collation(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<v1::hint>& hint(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<bsoncxx::v1::document::value>& let(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<bsoncxx::v1::types::value>& comment(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<bsoncxx::v1::document::value>& projection(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<bsoncxx::v1::document::value>& sort(find_one_and_replace_options& self);
+    static bsoncxx::v1::stdx::optional<v1::write_concern>& write_concern(find_one_and_replace_options& self);
+};
+
+} // namespace v1
+} // namespace mongocxx
