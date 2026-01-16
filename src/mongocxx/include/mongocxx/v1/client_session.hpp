c77593d16d490869b448ac1603e077acce41f83d
Ezra Chung <ezra.chung@mongodb.com>
2026-01-16 09:16:43 -0600

    CXX-3236 Avoid copying transaction_options parameters

--- src/mongocxx/include/mongocxx/v1/client_session.hpp
+++ src/mongocxx/include/mongocxx/v1/client_session.hpp
@@ -183,7 +183,7 @@ class client_session {
     ///
     /// @throws mongocxx::v1::exception when a client-side error is encountered.
     ///
-    MONGOCXX_ABI_EXPORT_CDECL(void) start_transaction(v1::transaction_options opts);
+    MONGOCXX_ABI_EXPORT_CDECL(void) start_transaction(v1::transaction_options const& opts);
 
     ///
     /// Start a new transaction.
@@ -346,7 +346,7 @@ class client_session::options {
     ///
     /// Set the "defaultTransactionOptions" field.
     ///
-    MONGOCXX_ABI_EXPORT_CDECL(options&) default_transaction_opts(v1::transaction_options v);
+    MONGOCXX_ABI_EXPORT_CDECL(options&) default_transaction_opts(v1::transaction_options const& v);
 
     ///
     /// Return the current "defaultTransactionOptions" field.
