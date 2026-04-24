c46237e7ad661247e2a8ca4b2af9bae6c0e605b2
Edan Stasiuk <68456290+EdanStasiuk@users.noreply.github.com>
2026-04-24 12:15:41 -0700

    fix: use camelCase for field name strings in insert_many_options comments

--- src/mongocxx/include/mongocxx/v1/insert_many_options.hpp
+++ src/mongocxx/include/mongocxx/v1/insert_many_options.hpp
@@ -96,32 +96,32 @@ class insert_many_options {
     MONGOCXX_ABI_EXPORT_CDECL() insert_many_options();
 
     ///
-    /// Set the "bypass_document_validation" field.
+    /// Set the "bypassDocumentValidation" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(insert_many_options&) bypass_document_validation(bool bypass_document_validation);
 
     ///
-    /// Return the current "bypass_document_validation" field.
+    /// Return the current "bypassDocumentValidation" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(bsoncxx::v1::stdx::optional<bool>) bypass_document_validation() const;
 
     ///
-    /// Set the "read_concern" field.
+    /// Set the "readConcern" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(insert_many_options&) read_concern(v1::read_concern rc);
 
     ///
-    /// Return the current "read_concern" field.
+    /// Return the current "readConcern" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(bsoncxx::v1::stdx::optional<v1::read_concern>) read_concern() const;
 
     ///
-    /// Set the "write_concern" field.
+    /// Set the "writeConcern" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(insert_many_options&) write_concern(v1::write_concern wc);
 
     ///
-    /// Return the current "write_concern" field.
+    /// Return the current "writeConcern" field.
     ///
     MONGOCXX_ABI_EXPORT_CDECL(bsoncxx::v1::stdx::optional<v1::write_concern>) write_concern() const;
 
