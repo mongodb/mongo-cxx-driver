16dedaa53fb50d651fbd1593e0c94baacb7a43b1
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2026-04-29 14:30:46 -0500

    Update etc/test_make_release.py with pytest and extended test coverage (#1625)

--- etc/test_make_release.py
+++ etc/test_make_release.py
@@ -1,11 +1,13 @@
+import sys
 import textwrap
-import unittest
+import types
 
 import click
+import pytest
 
 import make_release
 
-# Changelog entries for the 3.9.0 release.
+# Changelog entries for the 1.2.3 release.
 CHANGELOG_ENTRIES = textwrap.dedent("""\
 - Add CMake option `MONGOCXX_OVERRIDE_DEFAULT_INSTALL_PREFIX` (default is `TRUE`
     for backwards-compatibility).
@@ -13,12 +15,12 @@ CHANGELOG_ENTRIES = textwrap.dedent("""\
 """)
 
 
-# The expected release notes generated for the 3.9.0 release.
+# The expected release notes generated for the 1.2.3 release.
 EXPECTED_RELEASE_NOTES = f"""\
 ## Added
 
 {CHANGELOG_ENTRIES}
-See the [full list of changes in Jira](https://jira.mongodb.org/issues/?jql=project%20%3D%20CXX%20AND%20fixVersion%20%3D%203.9.0).
+See the [full list of changes in Jira](https://jira.mongodb.org/issues/?jql=project%20%3D%20CXX%20AND%20fixVersion%20%3D%201.2.3).
 
 ## Feedback
 To report a bug or request a feature, please open a ticket in the MongoDB issue management tool Jira:
@@ -32,7 +34,7 @@ Release artifacts may be verified by using the accompanying detached signature (
 """
 
 
-# The CHANGELOG.md file containing multiple release entries, including the 3.9.0 release.
+# The CHANGELOG.md file containing multiple release entries, including the 1.2.3 release.
 def make_changelog(version_suffix: str) -> str:
     return f"""\
 # Changelog
@@ -42,7 +44,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## 3.9.0{version_suffix}
+## 1.2.3{version_suffix}
 
 ### Added
 
@@ -55,21 +57,21 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 """
 
 
-# The official 3.9.0 changelog.
+# The official 1.2.3 changelog.
 CHANGELOG = make_changelog('')
 
-# The unreleased 3.9.0 changelog.
+# The unreleased 1.2.3 changelog.
 CHANGELOG_UNRELEASED = make_changelog(' [Unreleased]')
 
 # Duplicate release entry check.
 CHANGELOG_DUPLICATE_VERSION = textwrap.dedent("""
-## 3.9.0
+## 1.2.3
 
 ### Added
 
 - Add something.
 
-## 3.9.0
+## 1.2.3
 
 ### Fixed
 
@@ -83,37 +85,187 @@ CHANGELOG_DUPLICATE_VERSION = textwrap.dedent("""
 """).lstrip()
 
 
-class TestMakeRelease(unittest.TestCase):
+class TestGenerateReleaseNotes:
+    fn = staticmethod(make_release.generate_release_notes)
+
     def test_success(self):
-        got = make_release.generate_release_notes('3.9.0', CHANGELOG)
-        self.assertEqual(got, EXPECTED_RELEASE_NOTES)
+        got = self.fn('1.2.3', CHANGELOG)
+        assert got == EXPECTED_RELEASE_NOTES
 
-    def test_raises_when_version_not_found(self):
-        with self.assertRaises(click.ClickException) as ctx:
-            make_release.generate_release_notes('4.0.0', CHANGELOG)
-        self.assertIn('Failed to find', str(ctx.exception.message))
+    def test_missing(self):
+        with pytest.raises(click.ClickException, match='Failed to find'):
+            self.fn('2.0.0', CHANGELOG)
 
-    def test_raises_on_unexpected_extra_characters(self):
-        with self.assertRaises(click.ClickException) as ctx:
-            make_release.generate_release_notes('3.9.0', CHANGELOG_UNRELEASED)
-        self.assertIn('Unexpected extra characters', str(ctx.exception.message))
+    def test_extra(self):
+        with pytest.raises(click.ClickException, match='Unexpected extra characters'):
+            self.fn('1.2.3', CHANGELOG_UNRELEASED)
 
-    def test_allows_unreleased_tag(self):
-        got = make_release.generate_release_notes('3.9.0', CHANGELOG_UNRELEASED, allow_unreleased_changelog_entry=True)
-        self.assertEqual(got, EXPECTED_RELEASE_NOTES)
+    def test_allow_unreleased_changelog_entry(self):
+        got = self.fn('1.2.3', CHANGELOG_UNRELEASED, allow_unreleased_changelog_entry=True)
+        assert got == EXPECTED_RELEASE_NOTES
 
-    def test_allows_unreleased_tag_only(self):
-        with self.assertRaises(click.ClickException) as ctx:
-            make_release.generate_release_notes(
-                '3.9.0', make_changelog(' extra'), allow_unreleased_changelog_entry=True
-            )
-        self.assertIn('Unexpected extra characters', str(ctx.exception.message))
+    def test_allows_unreleased_changelog_entry_only(self):
+        with pytest.raises(click.ClickException, match='Unexpected extra characters'):
+            self.fn('1.2.3', make_changelog(' extra'), allow_unreleased_changelog_entry=True)
 
-    def test_raises_on_duplicate_version_entry(self):
-        with self.assertRaises(click.ClickException) as ctx:
-            make_release.generate_release_notes('3.9.0', CHANGELOG_DUPLICATE_VERSION)
-        self.assertIn('Unexpected second changelog entry', str(ctx.exception.message))
+    def test_duplicate(self):
+        with pytest.raises(click.ClickException, match='Unexpected second changelog entry'):
+            self.fn('1.2.3', CHANGELOG_DUPLICATE_VERSION)
+
+
+class TestIsPreReleaseTag:
+    fn = staticmethod(make_release.is_pre_release_tag)
+
+    @pytest.mark.parametrize(
+        'tag',
+        [
+            'r1.2.3-rc0',
+            'r1.2.3-alpha',
+            'r1.2.3-extra',
+            'r1.2.3-a.b.c',
+        ],
+    )
+    def test_is_pre_release(self, tag):
+        assert self.fn(tag)
+
+    @pytest.mark.parametrize(
+        'tag',
+        [
+            'r0.0.0',
+            'r1.2.3',
+            'r1.2.4',
+            'r2.0.0',
+        ],
+    )
+    def test_normal(self, tag):
+        assert not self.fn(tag)
+
+    @pytest.mark.parametrize(
+        'tag',
+        [
+            'invalid',
+            'r1.2.3-',
+            'r1.2.3.invalid',
+            'r1.2.3invalid',
+        ],
+    )
+    def test_invalid(self, tag):
+        assert not self.fn(tag)
+
+
+class TestPrintBanner:
+    fn = staticmethod(make_release.print_banner)
+
+    @pytest.mark.parametrize(
+        'tag',
+        [
+            'invalid',
+            'r1.2.3',
+            'r2.0.0',
+        ],
+    )
+    def test_contains_git_revision(self, tag, capsys):
+        self.fn(tag)
+        captured = capsys.readouterr()
+        assert f'This build is for Git revision {tag}' in captured.out
+
+
+def _make_issue(key, status_name):
+    ret = types.SimpleNamespace()
+
+    ret.key = key
+
+    ret.fields = types.SimpleNamespace()
+    ret.fields.status = types.SimpleNamespace()
+    ret.fields.status.name = status_name
+
+    return ret
+
+
+class TestAllIssuesClosed:
+    fn = staticmethod(make_release.all_issues_closed)
+
+    @pytest.mark.parametrize(
+        'issues',
+        [
+            [],
+            [_make_issue('CXX-1234', 'Closed')],
+            [
+                _make_issue('CXX-1234', 'Closed'),
+                _make_issue('CXX-2345', 'Development Complete'),
+            ],
+            [
+                _make_issue('CXX-2345', 'Development Complete'),
+                _make_issue('CXX-1234', 'Closed'),
+            ],
+            [
+                _make_issue('CXX-1234', 'Closed'),
+                _make_issue('CXX-2345', 'Development Complete'),
+                _make_issue('CXX-3456', 'Closed'),
+            ],
+        ],
+    )
+    def test_all_closed(self, issues, capsys):
+        result = self.fn(issues)
+        assert result
+
+        captured = capsys.readouterr()
+        assert captured.out == ''
+        assert captured.err == ''
+
+    @pytest.mark.parametrize(
+        'issues, expected',
+        [
+            (
+                [_make_issue('CXX-1234', 'Open')],
+                ['CXX-1234'],
+            ),
+            (
+                [
+                    _make_issue('CXX-1234', 'Backlog'),
+                    _make_issue('CXX-2345', 'Closed'),
+                ],
+                ['CXX-1234'],
+            ),
+            (
+                [
+                    _make_issue('CXX-2345', 'Closed'),
+                    _make_issue('CXX-1234', 'In Progress'),
+                ],
+                ['CXX-1234'],
+            ),
+            (
+                [
+                    _make_issue('CXX-1234', 'In Code Review'),
+                    _make_issue('CXX-2345', 'Closed'),
+                    _make_issue('CXX-3456', 'Needs Triage'),
+                    _make_issue('CXX-4567', 'Closed'),
+                    _make_issue('CXX-5678', 'Ready for Work'),
+                ],
+                ['CXX-1234', 'CXX-3456', 'CXX-5678'],
+            ),
+        ],
+    )
+    def test_any_open(self, issues, expected, capsys):
+        result = self.fn(issues)
+        assert not result
+
+        capture = capsys.readouterr()
+        assert capture.out == ''
+
+        err = capture.err
+        assert 'The following open tickets were found:' in err
+
+        for issue in expected:
+            assert issue in err
+
+        for issue in {i.key for i in issues} - set(expected):
+            assert issue not in err
+
+
+def main(argv=()):
+    sys.exit(pytest.main([__file__, *argv]))
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main(sys.argv[1:])
