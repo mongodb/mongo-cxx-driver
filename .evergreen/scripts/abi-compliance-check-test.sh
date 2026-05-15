281803c4609b7a87e76651a424e8ec74c6701a7a
Ezra Chung <ezra.chung@mongodb.com>
2026-05-15 14:03:29 -0500

    Use abi-dumper for compliance checks

--- .evergreen/scripts/abi-compliance-check-test.sh
+++ .evergreen/scripts/abi-compliance-check-test.sh
@@ -19,157 +19,121 @@ old_ver="${base:1}-base"
 new_ver="${current:1}-current"
 
 command -V abi-compliance-checker >/dev/null
+command -V abi-dumper >/dev/null
+command -V parallel >/dev/null
 
 mkdir cxx-abi cxx-noabi
 
-# Generate XML descriptors with sections used by both old and new tests.
-if true; then
-  cat >old.xml <<DOC
-<version>
-  ${old_ver:?}
-</version>
+declare bsoncxx_old bsoncxx_new mongocxx_old mongocxx_new
+bsoncxx_old="${working_dir:?}/install/old/lib/libbsoncxx1.so"
+bsoncxx_new="${working_dir:?}/install/new/lib/libbsoncxx1.so"
+mongocxx_old="${working_dir:?}/install/old/lib/libmongocxx1.so"
+mongocxx_new="${working_dir:?}/install/new/lib/libmongocxx1.so"
 
-<libs>
-  ../install/old/lib
-</libs>
+# Forward compatibility with upcoming stable ABI library filenames.
+[[ -f "${bsoncxx_old:?}" ]] || bsoncxx_old="${working_dir:?}/install/old/lib/libbsoncxx.so"
+[[ -f "${bsoncxx_new:?}" ]] || bsoncxx_new="${working_dir:?}/install/new/lib/libbsoncxx.so"
+[[ -f "${mongocxx_old:?}" ]] || mongocxx_old="${working_dir:?}/install/old/lib/libmongocxx.so"
+[[ -f "${mongocxx_new:?}" ]] || mongocxx_new="${working_dir:?}/install/new/lib/libmongocxx.so"
 
-<add_include_paths>
-  ../install/old/include/
-</add_include_paths>
+# Usage: generate_abi_report <label> <dir> <abi> <can_fail_task>
+#   label: "stable" or "unstable".
+#   dir: "cxx-abi" or "cxx-noabi"."
+#   abi: "v1" or "v_noabi"."
+#   can_fail_task: "true" or "false".
+generate_abi_report() (
+  declare label="${1:?}" dir="${2:?}" abi="${3:?}" can_fail_task="${4:?}"
 
-<skip_including>
-  /v_noabi/bsoncxx/enums/
-  /v_noabi/bsoncxx/config/
-  /v_noabi/mongocxx/config/
-  /v1/detail/prelude.hpp
-  /v1/detail/postlude.hpp
-</skip_including>
+  cd "${dir:?}"
+  echo "Generating ${label:?} ABI report..."
 
-DOC
+  # Use "new" usage pattern based on binary debug-info analysis.
+  # -keep-registers-and-offsets: abi-dumper doesn't detect `-Og` with Clang.
+  # "WARNING: a "Struct" type with no attributes detected in the DWARF dump" is caused by missing mongoc debug-info.
+  # "ERROR: missed type id <N>" is false-positive caused by otherwise-unused forward-decls in a template parameter.
+  parallel --halt now,fail=1 --tagstring '[{#}]' ::: \
+    "abi-dumper '${bsoncxx_old:?}' \
+      -o bsoncxx-old.dump \
+      -lver '${old_ver:?}' \
+      -keep-registers-and-offsets \
+      -public-headers '${working_dir:?}/install/old/include/bsoncxx/${abi:?}' \
+      -include-paths '${working_dir:?}/install/old/include/bsoncxx/v_noabi/bsoncxx' \
+      -include-paths '${working_dir:?}/install/old/include/bsoncxx'" \
+    "abi-dumper '${bsoncxx_new:?}' \
+      -o bsoncxx-new.dump \
+      -lver '${new_ver:?}' \
+      -keep-registers-and-offsets \
+      -public-headers '${working_dir:?}/install/new/include/bsoncxx/${abi:?}' \
+      -include-paths '${working_dir:?}/install/new/include/bsoncxx/v_noabi/bsoncxx' \
+      -include-paths '${working_dir:?}/install/new/include/bsoncxx'" \
+    "abi-dumper '${mongocxx_old:?}' \
+      -o mongocxx-old.dump \
+      -lver '${old_ver:?}' \
+      -keep-registers-and-offsets \
+      -public-headers '${working_dir:?}/install/old/include/mongocxx/${abi:?}' \
+      -include-paths '${working_dir:?}/install/old/include/mongocxx/v_noabi/mongocxx' \
+      -include-paths '${working_dir:?}/install/old/include/mongocxx'" \
+    "abi-dumper '${mongocxx_new:?}' \
+      -o mongocxx-new.dump \
+      -lver '${new_ver:?}' \
+      -keep-registers-and-offsets \
+      -public-headers '${working_dir:?}/install/new/include/mongocxx/${abi:?}' \
+      -include-paths '${working_dir:?}/install/new/include/mongocxx/v_noabi/mongocxx' \
+      -include-paths '${working_dir:?}/install/new/include/mongocxx'" \
+    &>abi-dumper.log || {
+    cat abi-dumper.log 1>&2
+    exit 1
+  }
 
-  cat >new.xml <<DOC
-<version>
-  ${new_ver:?}
-</version>
+  # Exclude internal detail namespaces, matching <skip_namespaces> in the legacy XML descriptors.
+  declare -a skip_args=(-skip-internal-types "::detail::")
 
-<libs>
-  ../install/new/lib
-</libs>
+  if [[ "${abi:?}" == "v1" ]]; then
+    skip_args+=(
+      -skip-internal-symbols "\dv_noabi\d"
+      -skip-internal-types "::v_noabi::"
+    )
+  elif [[ "${abi:?}" == "v_noabi" ]]; then
+    skip_args+=(
+      -skip-internal-symbols "\dv1\d"
+      -skip-internal-types "::v1::"
+    )
+  fi
 
-<add_include_paths>
-  ../install/new/include/
-</add_include_paths>
+  declare bsoncxx_ret mongocxx_ret bsoncxx_pid mongocxx_pid
+  abi-compliance-checker \
+    -report-path bsoncxx.html \
+    -lib bsoncxx \
+    -old bsoncxx-old.dump \
+    -new bsoncxx-new.dump \
+    "${skip_args[@]}" &
+  bsoncxx_pid=$!
 
-<skip_including>
-  /v_noabi/bsoncxx/enums/
-  /v_noabi/bsoncxx/config/
-  /v_noabi/mongocxx/config/
-  /v1/detail/prelude.hpp
-  /v1/detail/postlude.hpp
-</skip_including>
+  abi-compliance-checker \
+    -report-path mongocxx.html \
+    -lib mongocxx \
+    -old mongocxx-old.dump \
+    -new mongocxx-new.dump \
+    "${skip_args[@]}" &
+  mongocxx_pid=$!
+  wait "${bsoncxx_pid:?}" && bsoncxx_ret=$? || bsoncxx_ret=$?
+  wait "${mongocxx_pid:?}" && mongocxx_ret=$? || mongocxx_ret=$?
 
-DOC
+  # Allow task to upload logs and HTML reports despite failed status by using the EVG task HTTP endpoint.
+  if [[ "${bsoncxx_ret:?}" -gt 1 || "${mongocxx_ret:?}" -gt 1 ]]; then
+    declare status='{"status":"failed","type":"test","should_continue":true,"desc":"abi-compliance-checker emitted one or more errors"}'
+    curl -sS -d "${status:?}" -H "Content-Type: application/json" -X POST localhost:2285/task_status || true
+  elif [[ "${can_fail_task:?}" == "true" ]] && [[ "${bsoncxx_ret:?}" -gt 0 || "${mongocxx_ret:?}" -gt 0 ]]; then
+    declare status='{"status":"failed","type":"test","should_continue":true,"desc":"detected stable ABI incompatibility"}'
+    curl -sS -d "${status:?}" -H "Content-Type: application/json" -X POST localhost:2285/task_status || true
+  fi
 
-  tee cxx-abi/old.xml cxx-noabi/old.xml <old.xml >/dev/null
-  tee cxx-abi/new.xml cxx-noabi/new.xml <new.xml >/dev/null
-  rm old.xml new.xml
-fi
-
-# Append sections specific to each ABI test.
-if true; then
-  cat >>cxx-abi/old.xml <<DOC
-<headers>
-  ../install/old/include/bsoncxx/
-  ../install/old/include/mongocxx/
-</headers>
-
-<skip_headers>
-  /v_noabi/
-</skip_headers>
-
-<skip_namespaces>
-  bsoncxx::detail
-  mongocxx::detail
-</skip_namespaces>
-DOC
-
-  cat >>cxx-abi/new.xml <<DOC
-<headers>
-  ../install/new/include/mongocxx/
-  ../install/new/include/bsoncxx/
-</headers>
-
-<skip_headers>
-  /v_noabi/
-</skip_headers>
-
-<skip_namespaces>
-  bsoncxx::detail
-  mongocxx::detail
-</skip_namespaces>
-DOC
-
-  cat >>cxx-noabi/old.xml <<DOC
-<headers>
-  ../install/old/include/bsoncxx/v_noabi
-  ../install/old/include/mongocxx/v_noabi
-</headers>
-
-<skip_namespaces>
-  bsoncxx::detail
-  mongocxx::detail
-  bsoncxx::v1
-  mongocxx::v1
-</skip_namespaces>
-DOC
-
-  cat >>cxx-noabi/new.xml <<DOC
-<headers>
-  ../install/new/include/bsoncxx/v_noabi
-  ../install/new/include/mongocxx/v_noabi
-</headers>
-
-<skip_namespaces>
-  bsoncxx::detail
-  mongocxx::detail
-  bsoncxx::v1
-  mongocxx::v1
-</skip_namespaces>
-DOC
-fi
-
-args=(
-  -lib mongo-cxx-driver
-  -old old.xml
-  -new new.xml
+  echo "Generating ${label:?} ABI report... done."
 )
 
-# Allow task to upload the HTML report despite failed status.
-echo "Generating stable ABI report..."
-pushd cxx-abi
-declare ret
-abi-compliance-checker "${args[@]}" 2>&1 && ret="$?" || ret="$?"
-if [[ "${ret:?}" -gt 1 ]]; then
-  declare status
-  status='{"status":"failed", "type":"test", "should_continue":true, "desc":"abi-compliance-checker emitted one or more errors"}'
-  curl -sS -d "${status:?}" -H "Content-Type: application/json" -X POST localhost:2285/task_status || true
-elif [[ "${ret:?}" -gt 0 ]]; then
-  declare status
-  status='{"status":"failed", "type":"test", "should_continue":true, "desc":"detected stable ABI incompatibility"}'
-  curl -sS -d "${status:?}" -H "Content-Type: application/json" -X POST localhost:2285/task_status || true
-fi
-popd # cxx-abi
-echo "Generating stable ABI report... done."
-
-# Also create a report for the unstable ABI. Errors are expected and OK.
-echo "Generating unstable ABI report..."
-pushd cxx-noabi
-declare ret
-abi-compliance-checker "${args[@]}" 2>&1 && ret="$?" || ret="$?"
-if [[ "${ret:?}" -gt 1 ]]; then
-  declare status
-  status='{"status":"failed", "type":"test", "should_continue":true, "desc":"abi-compliance-checker emitted one or more errors"}'
-  curl -sS -d "${status:?}" -H "Content-Type: application/json" -X POST localhost:2285/task_status || true
-fi
-popd # cxx-noabi
-echo "Generating unstable ABI report... done."
+declare stable_pid unstable_pid
+generate_abi_report "stable" "cxx-abi" "v1" "true" &
+stable_pid="$!"
+generate_abi_report "unstable" "cxx-noabi" "v_noabi" "false" &
+unstable_pid="$!"
+wait "${stable_pid:?}" "${unstable_pid:?}"
