6219d5fdfb476134adcf19accbec0a5cea6be690
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2026-08-10 13:37:54 -0500

    CXX-3544 Fix Clang 22 compat by including missing `<cstdlib>` header (#1691) (#1693)

--- .evergreen/scripts/abi-stability-setup.sh
+++ .evergreen/scripts/abi-stability-setup.sh
@@ -41,7 +41,7 @@ working_dir="$(pwd)"
 
 # TODO: remove "UV_CONSTRAINT" of CMake after the base release is r4.4.1+ to include fix of CXX-3529.
 constraint_file="$(mktemp)"
-echo "cmake<4.4" > "${constraint_file:?}"
+echo "cmake<4.4" >"${constraint_file:?}"
 export UV_CONSTRAINT="${constraint_file:?}"
 
 . mongo-cxx-driver/.evergreen/scripts/install-build-tools.sh
