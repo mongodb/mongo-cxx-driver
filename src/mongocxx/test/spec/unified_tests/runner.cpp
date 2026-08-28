5fb5a3ff0120430365ddb1598c41afcb784fa7ce
Kevin Albertson <kevin.albertson@mongodb.com>
2026-08-28 08:57:14 -0400

    migrate retryable reads GridFS tests to unified format
    Sync the GridFS retryable reads tests to mongodb/specifications commit
    1de749a00036a673a3d7e863e5313f9d8ebbe174.
    
    This does not add changes from the in-review spec changes.

--- src/mongocxx/test/spec/unified_tests/runner.cpp
+++ src/mongocxx/test/spec/unified_tests/runner.cpp
@@ -1610,7 +1610,14 @@ TEST_CASE("change streams unified format spec automated tests", "[unified_format
 }
 
 TEST_CASE("retryable reads unified format spec automated tests", "[unified_format_specs]") {
-    run_unified_format_tests_in_env_dir("RETRYABLE_READS_UNIFIED_TESTS_PATH");
+    std::set<bsoncxx::stdx::string_view> const unsupported_tests = {
+        // mongocxx::gridfs::bucket does not provide a download-by-filename API.
+        "gridfs-downloadByName.json",
+        // mongocxx::gridfs::bucket does not provide a download-by-filename API.
+        "gridfs-downloadByName-serverErrors.json",
+    };
+
+    run_unified_format_tests_in_env_dir("RETRYABLE_READS_UNIFIED_TESTS_PATH", unsupported_tests);
 }
 
 TEST_CASE("retryable writes unified format spec automated tests", "[unified_format_specs]") {
