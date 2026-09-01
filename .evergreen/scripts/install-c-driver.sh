01c027780f4c27c9ccb075a9c049810ef8d5c344
Kevin Albertson <kevin.albertson@mongodb.com>
2026-09-01 09:34:58 -0400

    remove no-longer-needed `AVOID_COMPILE_LIBMONGOCRYPT`

--- .evergreen/scripts/install-c-driver.sh
+++ .evergreen/scripts/install-c-driver.sh
@@ -77,9 +77,6 @@ if [[ "${SKIP_INSTALL_LIBMONGOCRYPT:-}" != "1" ]]; then
   # references to the USE_NINJA environment variable).
   AVOID_COMPILE_LIBMONGOCRYPT=$([[ "${OSTYPE:?}" == cygwin && "${CMAKE_GENERATOR:?}" != Visual\ Studio\ * ]] && echo 1 || echo 0)
 
-  # Avoid using compile-libmongocrypt.sh (gets libmongocrypt 1.21.0-dev) to avoid test failures due to 1.19.0 dropping "textPreview".
-  AVOID_COMPILE_LIBMONGOCRYPT=1 # TODO: remove this line once CXX-3467 is addressed.
-
   if [[ "${AVOID_COMPILE_LIBMONGOCRYPT:?}" == "1" ]]; then
     (
       git clone -q https://github.com/mongodb/libmongocrypt --branch 1.20.2 --depth 1
