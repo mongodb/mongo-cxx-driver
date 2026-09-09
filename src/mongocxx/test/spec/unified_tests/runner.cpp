f8830ebaa5a88caee1df9f3ea022c6bae31c517e
Kevin Albertson <kevin.albertson@mongodb.com>
2026-09-09 08:36:22 -0400

    support "createEntities"
    This is a small backport from  ef96492a46 to fix GridFS tests.

--- src/mongocxx/test/spec/unified_tests/runner.cpp
+++ src/mongocxx/test/spec/unified_tests/runner.cpp
@@ -1286,6 +1286,14 @@ void run_tests(bsoncxx::stdx::string_view test_description, document::view test)
             operations::state state;
 
             for (auto const& ops : ele["operations"].get_array().value) {
+                // The "createEntities" test runner operation creates additional entities during a
+                // test without clearing the existing entity map (unlike top-level "createEntities").
+                if (string::to_string(ops["name"].get_string().value) == "createEntities") {
+                    auto const entities = ops["arguments"]["entities"].get_array().value;
+                    REQUIRE(std::all_of(std::begin(entities), std::end(entities), add_to_map));
+                    continue;
+                }
+
                 auto const ignore_result_and_error = [&]() -> bool {
                     auto const elem = ops["ignoreResultAndError"];
                     return elem && elem.get_bool().value;
