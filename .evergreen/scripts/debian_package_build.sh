4b4201350f0452b58cbe3dd77070f4a995ab9a74
Roberto C. Sánchez <roberto@connexer.com>
2026-09-10 13:35:12 -0400

    fetch packages from new location

--- .evergreen/scripts/debian_package_build.sh
+++ .evergreen/scripts/debian_package_build.sh
@@ -59,7 +59,7 @@ sudo DEB_BUILD_PROFILES="${DEB_BUILD_PROFILES}" chroot ./unstable-chroot /bin/ba
   (apt-get install -y ca-certificates cmake debhelper doxygen git libsasl2-dev libsnappy-dev libssl-dev libutf8proc-dev pkgconf zlib1g-dev build-essential curl fakeroot furo git-buildpackage python3-sphinx python3-sphinx-design python3-packaging && \
   mkdir /tmp/mongo-c-driver && \
   cd /tmp/mongo-c-driver && \
-  curl -o deb.tar.gz -L https://s3.amazonaws.com/mciuploads/mongo-c-driver/master/mongo-c-driver-debian-packages-latest.tar.gz && \
+  curl -o deb.tar.gz -L https://mciuploads.s3.amazonaws.com/mongo-c-driver/master/mongo-c-driver-debian-packages-amd64-latest.tar.gz && \
   tar zxvf deb.tar.gz && \
   apt-get install -y ./*.deb && \
   chown -R root:root /tmp/mongo-cxx-driver && \
